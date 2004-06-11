946020a32fa322ace1a9b45b3a671fd7fa06393d
Marc Mulcahy <marcm@src.gnome.org>
2004-06-11 01:36:27 +0000

    Added default settings file.

---
+++ src/orca/settings.py
@@ -0,0 +1,9 @@
+# Default settings.py -- this is used when the user has no settings.py
+# or when the user has no home directory as in the case of gdm
+
+# We don't know what engines are installed, so voices is empty
+
+voices = {}
+keyEcho = False
+useSpeech = True
+useBraille = False
