859511bb4da5e48f42485cca43c055364cfc7eb7
Marc Mulcahy <marcm@src.gnome.org>
2004-06-25 09:07:12 +0000

    (init): Register event listeners for tracking window activation/deactivation for activating/deactivating scripts. (processEvent): Changed event processing to filter all at-spi events through one function which decides which script should receive each event.

--- src/orca/orca.py
+++ src/orca/orca.py
@@ -46,34 +46,70 @@ import kbd
 
 from orca_i18n import _
 
+# This dictionary contains references to all the loded scripts
+
+scripts = {}
+
 # List of running apps
 
 apps = []
-appnames = []
 
 # Build the list of currently runing apps
 
 def buildAppList ():
     global apps
-    global appnames
 
     apps = []
-    appnames = []
 
     i = core.desktop.childCount-1
     while i >= 0:
-        app = core.desktop.getChildAtIndex (i)
-        if app:
+        acc = core.desktop.getChildAtIndex (i)
+        try:
+            app = a11y.Accessible.cache[acc]
+        except:
+            if acc == None:
+                app = None
+            else:
+                app = a11y.Accessible (acc)
+        if app != None:
             apps.insert (0, app)
-            appnames.insert (0, app.name)
         i = i-1
+
+# This function is called when a toplevel window is activated -- it
+# reloads the script associated with the toplevel window's
+# application, and activates the script's keybindings
+
+def activateApp (app):
+    global scripts
+
+    # Stop speech when a new window is activated
+
+    speech.stop ("default")
+
+    # Find the script for this app
+
+    try:
+        s = scripts[app]
+    except:
+        print "no script found for " + app.name
+        return
+
+    # Reload the script
+
+    s.reload ()
+
+    # Make this scripts keybindings active
+
+    kbd.keybindings = s.keybindings
+    brl.onBrlKey = s.onBrlKey
+    
 
 # Track children-changed events on the desktop to determine when apps
 # start and stop
 
-def onChildrenChanged (event):
+def onChildrenChanged (e):
 
-    if event.source == core.desktop:
+    if e.source == core.desktop:
 
         # If the desktop is empty, the user has logged out-- shutdown Orca
         
@@ -82,29 +118,88 @@ def onChildrenChanged (event):
             shutdown ()
             return
 
-        if event.type == "object:children-changed:add":
-            app = core.desktop.getChildAtIndex (event.detail1)
-            script.load (app.name)
-        if event.type == "object:children-changed:remove":
-            if script.isLoaded (appnames[event.detail1]):
-                script.unload (appnames[event.detail1])
+        if e.type == "object:children-changed:add":
+            obj = core.desktop.getChildAtIndex (e.detail1)
+            app = a11y.makeAccessible (obj)
+            s = script.script (app)
+            s.load ()
+            scripts[app] = s
+            speech.say ("default", "Script for " + app.name + " loaded")
+        elif e.type == "object:children-changed:remove":
+            speech.say ("default", "unloading")
+            app = apps[e.detail1]
+            del scripts[app]
         buildAppList ()
     
-# Activate scripts when their apps' toplevel windows are activated
+# When toplevel windows are activated, reload the script associated
+# with the window's application and set the script's keybidings as the
+# active bindings
 
-def onWindowActivated (event):
+def onWindowActivated (e):
+    activateApp (a11y.makeAccessible(e.source).app)
 
-    # Clear the Braille display on Window Activations
+# Dummy class for converting the source of an event to an Accessible
+# object -- we need this since the core.event object we get from the
+# core is read-only-- so we create this dummy event object to contain
+# a copy of all the event members with the source converted to an
+# Accessible
 
-    brl.clear ()
-    app = event.source.parent
-    script.activate (app.name)
-    try:
-        func = script.active.onWindowActivated
-    except:
-        func = None
-    if func:
-        func (event)
+class Event:
+    pass
+
+# This function process *all* events destined for scripts
+
+def processEvent (e):
+    global scripts
+    
+    # Create an Accessible for the source
+
+    source = a11y.makeAccessible (e.source)
+
+    # Convert the event source to an Accessible
+
+    event = Event ()
+    event.type = e.type
+    event.detail1 = e.detail1
+    event.detail2 = e.detail2
+    event.any_data = e.any_data
+    event.source = source
+
+    # See if we have a script for this event
+
+    if source.app is None:
+        print "app not found"
+    else:
+        try:
+            s = scripts[source.app]
+        except:
+            s = None
+        if s is not None:
+            try:
+                func = s.listeners[e.type]
+            except:
+                func = None
+            if func is not None:
+                func (event)
+    
+# Find the currently active toplevel window
+
+def findActiveWindow ():
+    global apps
+    
+    # Travers through the list of known apps
+
+    for app in apps:
+        
+        # Traverse through all the toplevels of each app, looking for one with state active
+
+        i = 0
+        while i < app.childCount:
+            state = app.child(i).state
+            if state.count (core.Accessibility.STATE_ACTIVE) > 0:
+                return app.child(i)
+            i = i+1
+    return None
 
 # Orca initialized
 
@@ -112,14 +207,10 @@ initialized = False
 
 def init ():
     global initialized
-    global appnames
+    global apps
     
     if initialized:
         return False
-
-    # Initialize the core
-
-    core.init ()
 
     # Initialize a11y support
 
@@ -135,27 +226,38 @@ def init ():
 
     # Initialize Braille support
 
-    brl.init ()
+    if settings.useBraille:
+        brl.init ()
 
     # Build list of accessible apps
 
     buildAppList ()
 
+    # Create and load an app's script when it is added to the desktop
+
     core.registerEventListener (onChildrenChanged, "object:children-changed:")
+
+    # Reload a script's modules and activate it's keybindings when a toplevel of the application is activated
+
     core.registerEventListener (onWindowActivated, "window:activate")
 
-    # Load the default script
+    # a11y.dispatcher has a list of all the listeners we should register
 
-    script.load ("default")
-
-    # Activate the default script
-
-    script.activate ("default")
+    for type in a11y.dispatcher.values():
+        core.registerEventListener (processEvent, type)
 
     # Attempt to load scripts for all currently running apps
 
-    for name in appnames:
-        script.load (name)
+    for app in apps:
+
+        # Create a script for this application and add it to our list of scripts
+
+        s = script.script (app)
+        scripts[app] = s
+
+        # Load the script
+
+        s.load ()
 
     initialized = True
     return True
@@ -164,9 +266,48 @@ def init ():
 
 def start ():
     global initialized
+    global scripts
+    
 
     if not initialized:
         return False
+
+    speech.say ("default", _("Welcome to Orca."))
+
+    # Find the currently active toplevel window
+
+    win = findActiveWindow ()
+
+    if win:
+
+        speech.say ("default", "Found active application")
+
+        # Active the script for this window
+
+        activateApp (win.app)
+
+        # Find the script for this app
+
+        s = scripts[win.app]
+
+        # Generate a fake window activation event so we get some feedback
+        # as to what window is active
+
+        e = Event ()
+        e.source = win
+        e.type = "window:activate"
+        e.detail1 = 0
+        e.detail2 = 0
+        e.any_data = None
+    
+        # Send our fake event to the script -- this may fail if for some
+        # reason the script doesn't have an onWindowActivated function
+
+        try:
+            s.listeners["window:activate"](e)
+        except:
+            pass
+
     core.bonobo.main ()
 
 
@@ -175,20 +316,20 @@ def start ():
 def shutdown ():
     global initialized
     global apps
-    global appnames
 
     if not initialized:
         return False
 
     speech.say ("default", _("goodbye."))
 
-    del apps
-    del appnames
-
     # Deregister our event listeners
 
     core.unregisterEventListener (onChildrenChanged, "object:children-changed:")
-    core.unregisterEventListener (onWindowActivated, "window:activate")
+
+    # a11y.dispatcher has a list of all the listeners we should unregister
+
+    for key in a11y.dispatcher.keys():
+        core.unregisterEventListener (processEvent, key)
 
     # Shutdown the keyboard support
 
@@ -209,10 +350,6 @@ def shutdown ():
     # Shutdown Bonobo
 
     core.bonobo.main_quit ()
-
-    # Shutdown the core
-
-    core.shutdown ()
 
     initialized = False
     return True
