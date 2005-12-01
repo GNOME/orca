032a5a8a28268d7d1b667ce3a279aaf6db933bba
Rich Burridge <richb@src.gnome.org>
2005-12-01 17:05:43 +0000

    src/orca/scripts/Evolution.py Better handling of reading the details of
            * src/orca/scripts/Evolution.py
              Better handling of reading the details of the current message
              header that has focus. See comments in the Evolution.py script
              for further details.

--- src/orca/scripts/Evolution.py
+++ src/orca/scripts/Evolution.py
@@ -19,8 +19,10 @@
 
 import orca.debug as debug
 import orca.default as default
+import orca.atspi as atspi
 import orca.rolenames as rolenames
 import orca.orca as orca
+import orca.speech as speech
 
 ########################################################################
 #                                                                      #
@@ -46,8 +48,31 @@ class Script(default.Script):
         - event: the Event
         """
 
-        debug.printObjectEvent(debug.LEVEL_OFF,
-                               event,
-                               event.source.toString())
+        # debug.printObjectEvent(debug.LEVEL_OFF,
+        #                       event,
+        #                       event.source.toString())
+
+        # Pass the focus event onto the parent class to be handled in the
+        # default way.
 
         default.Script.onFocus(self, event)
+
+        # If the focus is in the message header list, then we want to speak
+        # the remainder of the tables cells in the current highlighted 
+        # message (the default.py onFocus() method will have handled the
+        # first one above).
+        #
+        # Note that the Evolution user can adjust which colums appear in 
+        # the message list and the order in which they appear, so that 
+        # Orca will just speak the ones that they are interested in.
+
+        if event.source.role == rolenames.ROLE_TABLE_CELL:
+            parent = event.source.parent
+            if parent.name == "Message List":
+                row = parent.table.getRowAtIndex(event.source.index)
+                for i in range(1, parent.table.nColumns):
+                    obj = parent.table.getAccessibleAt(row, i)
+                    cell = atspi.Accessible.makeAccessible(obj)
+                    utterances = self.speechGenerator._getSpeechForTableCell(cell, False)
+                    speech.speakUtterances(utterances)
+                    orca.setLocusOfFocus(event, event.source, False)
