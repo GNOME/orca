bc361c2383d158bcaf03708235c5d98f345463e0
William Walker <wwalker@src.gnome.org>
2006-01-17 19:33:43 +0000

    Workaround for bug 325917.

--- src/orca/scripts/gaim.py
+++ src/orca/scripts/gaim.py
@@ -1,6 +1,6 @@
 # Orca
 #
-# Copyright 2004 Sun Microsystems Inc.
+# Copyright 2004-2006 Sun Microsystems Inc.
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Library General Public
@@ -49,103 +49,15 @@ class Script(default.Script):
 
         default.Script.__init__(self, app)
 
-        self._output = None
-        self._input = None
-        
-
-    def _setChat(self, obj):
-        """If we've found a chat window, set a reference to it and read it.
-
-        Arguments:
-        - obj: the gaim frame
-        """
-    
-        # The chat window has three text boxes in it (the chat topic, the
-        # message log, and the text entry field).  find them all - they're
-        # always returned in the same order
-        #
-        entries = atspi.findByRole(obj, rolenames.ROLE_TEXT)
-        self._output = entries[1]
-        self._input = entries[2]
-
-        # Speak the title of the chat
-        #
-        label = obj.label
-        text = label + _(" chat")
-        braille.displayMessage(text)
-        speech.speak(text)
-
-        
-    def _setIm(self, obj):
-        """If we've found an IM window, set a reference to it and read it.
-
-        Arguments:
-        - obj: the gaim frame
-        """
-
-        # There are two text boxes in each IM window (the message log, and
-        # the text entry field).  They are always returned in the same
-        # order.   
-        #
-        entries = atspi.findByRole(obj, rolenames.ROLE_TEXT)
-        self._output = entries[0]
-        self._input = entries[1]
-
-        # Speak the title of the IM
-        #
-        label = obj.label
-        text = label + _(" instant message")
-        braille.displayMessage(text)
-        speech.speak(text)
-
-
-    def onWindowActivated(self, event):
-        """Called whenever one of Gaim's toplevel windows is activated.
-
-        Arguments:
-        - event: the window activated Event
-        """
-    
-        # If it's not a standard window, do the normal behavior since it
-        # won't have an IM or chat in it
-        #
-        if event.source.role != rolenames.ROLE_FRAME:
-            self._output = None
-            self._input = None
-            return default.Script.onWindowActivated(self, event)
-
-        # Frames with two text boxes are considered to be instant message
-        # windows, and those with three are considered chats.  This works for
-        # now, but we need a more robust way of doing this sort of thing for
-        # the general case
-        #
-        entries = atspi.findByRole(event.source, rolenames.ROLE_TEXT)
-    
-        if len(entries) == 2:
-            self._setIm(event.source)
-        elif len(entries) == 3:
-            self._setChat(event.source)
-        else:
-            self._output = None
-            self._input = None
-
-        return default.Script.onWindowActivated(self, event)
-
-
     def onTextInserted(self, event):
-        """Called whenever text is inserted into one of Gaim's text objects.
-        If the object is an instant message or chat, speak the text If we're
-        not watching anything, do the default behavior.
+        """Called whenever text is inserted into one of Gaim's text
+        objects.  If the object is an instant message or chat, speak
+        the text. If we're not watching anything, do the default
+        behavior.
 
         Arguments:
         - event: the text inserted Event
         """
-
-        # [[[TODO: WDW - HACK we seem to be getting object ids for the
-        # same text area.  So, we do something a bit more creative here.]]]
-        #
-        #if (self._output == None) or (event.source != self._output):
-        #    return default.Script.onTextInserted(self, event)
 
         # Do the default action for everything except the display that
         # is showing the chat.
@@ -169,30 +81,21 @@ class Script(default.Script):
             #
             if text[0] == "\n":
                 text = text[1:]
-                
-            braille.displayMessage(text)
-            speech.speak(text)
+                braille.displayMessage(text)
+                speech.speak(text)
+                return
+            else:
+                # [[[TODO: WDW - HACK to handle the case where the
+                # area where the user types loses focus and never
+                # regains it with respect to the AT-SPI regardless if
+                # it really gets it with respect to the toolkit.  The
+                # way we are guessing we are really in the area where
+                # you type is because the text does not end in a
+                # "\n".  This is related to bug
+                # http://bugzilla.gnome.org/show_bug.cgi?id=325917]]]
+                #
+                orca.setLocusOfFocus(event, event.source, False)
+                return default.Script.onTextInserted(self, event)
         else:
             orca.setLocusOfFocus(event, event.source, False)
             return default.Script.onTextInserted(self, event)
-            
-        
-    def onFocus(self, event):
-        """Called when any object in Gaim gets the focus.
-
-        Arguments:
-        - event: the focus Event
-        """
-
-        # The text boxes in chat and IM windows have no names - so we give
-        # them some here
-        #
-        if event.source == self._input:
-            text = _("Input")
-        elif event.source == self._output:
-            text = _("Message Log")
-        else:
-            return default.Script.onFocus(self, event)
-
-        text = text + " " + getRoleName(event.source)
-        speech.speak(text)
