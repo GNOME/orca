1ea836635360df187a7e1b0291e0f7eafcc51afd
William Walker <wwalker@src.gnome.org>
2006-03-15 01:21:06 +0000

    Add simple script to set locus of focus to username/password field of gdmlogin when Orca starts up as part of accessible login.

---
+++ src/orca/scripts/gdmlogin.py
@@ -0,0 +1,64 @@
+# Orca
+#
+# Copyright 2006 Sun Microsystems Inc.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General Public
+# License along with this library; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 02111-1307, USA.
+
+"""Custom script for gdmlogin - this makes sure the focused item is spoken
+when Orca starts.
+"""
+
+import orca.atspi as atspi
+import orca.default as default
+import orca.orca as orca
+
+########################################################################
+#                                                                      #
+# The Gaim script class.                                               #
+#                                                                      #
+########################################################################
+
+class Script(default.Script):
+
+    def __init__(self, app):
+        """Creates a new script for the given application.
+        
+        Arguments:
+        - app: the application to create a script for.
+        """
+        default.Script.__init__(self, app)
+
+    def findFocusedObject(self, obj):
+	for i in range(0, obj.childCount):
+	    try:
+		child = obj.child(i)
+		if child.state.count(atspi.Accessibility.STATE_FOCUSED):
+		    return child
+		else:
+		    candidate = self.findFocusedObject(child)
+		    if candidate:
+			return candidate
+	    except:
+		pass
+	return None		
+
+    def onWindowActivated(self, event):
+	default.Script.onWindowActivated(self, event)
+	obj = self.findFocusedObject(self.app)
+	if obj:
+	    orca.setLocusOfFocus(event, obj)
+        else:
+	    default.Script.onWindowActivated(self, event)
