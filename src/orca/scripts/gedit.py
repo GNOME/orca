745b03f27c9c7308109483e65708179cbdef7b03
Rich Burridge <richb@src.gnome.org>
2006-03-30 16:30:18 +0000

    src/orca/scripts/gedit.py Added in spell checking support. The misspelt
            * src/orca/scripts/gedit.py
              Added in spell checking support. The misspelt word and its
              context are now spoken in a similar way to the StarOffice and
              Evolution scripts.
    
              NOTE: this will currently only work correctly if you turn:
                    CACHE_VALUES = False
                    in atspi.py. See Orca bug #334924 for more details
                    (http://bugzilla.gnome.org/show_bug.cgi?id=334924).

--- src/orca/scripts/gedit.py
+++ src/orca/scripts/gedit.py
@@ -29,12 +29,15 @@ technologies to just leave it alone or it will pull the trigger on the
 application.]]]
 """
 
+import orca.debug as debug
 import orca.atspi as atspi
 import orca.braille as braille
 import orca.default as default
+import orca.orca as orca
 import orca.rolenames as rolenames
 import orca.speech as speech
 import orca.speechgenerator as speechgenerator
+import orca.util as util
 
 from orca.orca_i18n import _
 from orca.rolenames import getRoleName
@@ -78,7 +81,283 @@ class SpeechGenerator(speechgenerator.SpeechGenerator):
 
 class Script(default.Script):
 
+    def __init__(self, app):
+        """Creates a new script for the given application.
+
+        Arguments:
+        - app: the application to create a script for.
+        """
+
+        default.Script.__init__(self, app)
+
+        # This will be used to cache a handle to the gedit text area for
+        # spell checking purposes.
+
+        self.textArea = None
+
+        # The following variables will be used to try to determine if we've
+        # already handled this misspelt word (see readMisspeltWord() for
+        # more details.
+
+        self.lastCaretPosition = -1
+        self.lastBadWord = ''
+        self.lastEventType = ''
+
+
     def getSpeechGenerator(self):
         """Returns the speech generator for this script.
         """
         return SpeechGenerator()
+
+
+    def readMisspeltWord(self, event, panel):
+        """Speak/braille the current misspelt word plus its context.
+           The spell check dialog contains a "paragraph" which shows the
+           context for the current spelling mistake. After speaking/brailling
+           the default action for this component, that a selection of the
+           surronding text from that paragraph with the misspelt word is also
+           spoken.
+
+        Arguments:
+        - event: the event.
+        - panel: the panel in the check spelling dialog containing the label
+                 with the misspelt word.
+        """
+
+        # Braille the default action for this component.
+        #
+        orca.setLocusOfFocus(event, event.source, False)
+        self.updateBraille(orca.locusOfFocus)
+
+        # Look for the label containing the misspelled word.
+        # There will be three labels in the top panel in the Check
+        # Spelling dialog. Look for the one that isn't a label to
+        # another component.
+        #
+        allLabels = atspi.findByRole(panel, rolenames.ROLE_LABEL)
+        for i in range(0, len(allLabels)):
+            if allLabels[i].name.startswith(_("Change to:")) or \
+               allLabels[i].name.startswith(_("Misspelled word:")):
+                continue
+            else:
+                badWord = allLabels[i].name
+                break
+
+        # Note that we often get two or more of these focus or property-change
+        # events each time there is a new misspelt word. We extract the
+        # current text caret position and the misspelt word and compare 
+        # them against the values saved from the last time this routine 
+        # was called. If they are the same then we ignore it.
+
+        if self.textArea != None:
+            allText = atspi.findByRole(self.textArea, rolenames.ROLE_TEXT)
+            caretPosition = allText[0].text.caretOffset
+
+            debug.println(debug.LEVEL_FINEST, \
+                "gedit.readMisspeltWord: type=%s  word=%s caret position=%d" % \
+                (event.type, badWord, caretPosition))
+
+            if (caretPosition == self.lastCaretPosition) and \
+               (badWord == self.lastBadWord) and \
+               (event.type == self.lastEventType):
+                return
+
+            # The indication that spell checking is complete is when the
+            # "misspelt" word is set to "Completed spell checking". Ugh!
+            # Try to detect this and let the user know.
+
+            if badWord == _("Completed spell checking"):
+                utterance = _("Spell checking is complete.")
+                speech.speak(utterance)
+                utterance = _("Press Tab and Return to terminate.")
+                speech.speak(utterance)
+                return
+
+            # If we have a handle to the gedit text area, then extract out
+            # all the text objects, and create a list of all the words found
+            # in them.
+            #
+            allTokens = []
+            for i in range(0, len(allText)):
+                text = allText[i].text.getText(0, -1)
+                tokens = text.split()
+                allTokens += tokens
+
+            # Create an utterance to speak consisting of the misspelt
+            # word plus the context where it is used (upto five words
+            # to either side of it).
+            #
+            for i in range(0, len(allTokens)):
+                if allTokens[i].startswith(badWord):
+                    min = i - 5
+                    if min < 0:
+                        min = 0
+                    max = i + 5
+                    if max > (len(allTokens) - 1):
+                        max = len(allTokens) - 1
+
+                    utterances = [_("Misspelled word: "), badWord, \
+                                  _(" Context is ")] + allTokens[min:max+1]
+
+                    # Turn the list of utterances into a string.
+                    text = " ".join(utterances)
+                    speech.speak(text)
+
+                    # Save misspelt word information for comparison purposes 
+                    # next time around.
+                    #
+                    self.lastCaretPosition = caretPosition
+                    self.lastBadWord = badWord
+                    self.lastEventType = event.type
+
+                    return
+
+
+    def walkComponentHierarchy(self, obj):
+        """Debug routine to print out the hierarchy of components for the
+           given object.
+
+        Arguments:
+        - obj: the component to start from
+        """
+
+        print "<<<<---- Component Hierachy ---->>>>"
+        print "START: Obj:", obj.name, obj.role
+        parent = obj
+        while parent:
+            if parent != obj:
+                if not parent.parent:
+                    print "TOP: Parent:", parent.name, parent.role
+                else:
+                    print "Parent:", parent.name, parent.role
+            parent = parent.parent
+        print "<<<<============================>>>>"
+
+
+    # This method tries to detect and handle the following cases:
+    # 1) Text area (for caching handle for spell checking purposes).
+    # 2) Check Spelling Dialog.
+
+    def onFocus(self, event):
+        """Called whenever an object gets focus.
+
+        Arguments:
+        - event: the Event
+        """
+
+        brailleGen = self.brailleGenerator
+        speechGen = self.speechGenerator
+
+        debug.printObjectEvent(debug.LEVEL_FINEST,
+                               event,
+                               event.source.toString())
+
+        # self.walkComponentHierarchy(event.source)
+
+        # 1) Text area (for caching handle for spell checking purposes).
+        #
+        # This works in conjunction with code in section 2). Check to see if
+        # focus is currently in the gedit text area. If it is, then, if this 
+        # is the first time, save a pointer to the scroll pane which contains
+        # the text being editted.
+        #
+        # Note that this drops through to then use the default event
+        # processing in the parent class for this "focus:" event.
+
+        rolesList = [rolenames.ROLE_TEXT, \
+                     rolenames.ROLE_SCROLL_PANE, \
+                     rolenames.ROLE_FILLER, \
+                     rolenames.ROLE_PAGE_TAB, \
+                     rolenames.ROLE_PAGE_TAB_LIST, \
+                     rolenames.ROLE_SPLIT_PANE]
+        if util.isDesiredFocusedItem(event.source, rolesList):
+            debug.println(debug.LEVEL_FINEST,
+                      "gedit.onFocus - text area.")
+
+            self.textArea = event.source.parent
+            # Fall-thru to process the event with the default handler.
+
+
+        # 2) check spelling dialog.
+        #
+        # Check to see if the Spell Check dialog has just appeared and got
+        # focus. If it has, then speak/braille the current misspelt word
+        # plus its context.
+        #
+        # Note that in order to make sure that this focus event is for the
+        # check spelling dialog, a check is made of the localized name of the
+        # option pane. Translators for other locales will need to ensure that
+        # their translation of this string matches what gedit uses in
+        # that locale.
+
+        rolesList = [rolenames.ROLE_TEXT, \
+                     rolenames.ROLE_FILLER, \
+                     rolenames.ROLE_PANEL, \
+                     rolenames.ROLE_FILLER, \
+                     rolenames.ROLE_FRAME]
+        if util.isDesiredFocusedItem(event.source, rolesList):
+            frame = event.source.parent.parent.parent.parent
+            if frame.name.startswith(_("Check Spelling")):
+                debug.println(debug.LEVEL_FINEST,
+                      "gedit.onFocus - check spelling dialog.")
+
+                self.readMisspeltWord(event, event.source.parent.parent)
+                # Fall-thru to process the event with the default handler.
+
+
+        # For everything else, pass the focus event onto the parent class
+        # to be handled in the default way.
+
+        default.Script.onFocus(self, event)
+
+
+    # This method tries to detect and handle the following cases:
+    # 1) check spelling dialog.
+
+    def onNameChanged(self, event):
+        """Called whenever a property on an object changes.
+
+        Arguments:
+        - event: the Event
+        """
+
+        brailleGen = self.brailleGenerator
+        speechGen = self.speechGenerator
+
+        debug.printObjectEvent(debug.LEVEL_FINEST,
+                               event,
+                               event.source.toString())
+
+        # self.walkComponentHierarchy(event.source)
+
+        # 1) check spelling dialog.
+        #
+        # Check to see if if we've had a property-change event for the
+        # accessible name for the label containing the current misspelt
+        # word in the check spelling dialog.
+        # This (hopefully) means that the user has just corrected a
+        # spelling mistake, in which case, speak/braille the current
+        # misspelt word plus its context.
+        #
+        # Note that in order to make sure that this event is for the
+        # check spelling dialog, a check is made of the localized name of the
+        # frame. Translators for other locales will need to ensure that
+        # their translation of this string matches what gedit uses in
+        # that locale.
+
+        rolesList = [rolenames.ROLE_LABEL, \
+                     rolenames.ROLE_PANEL, \
+                     rolenames.ROLE_FILLER, \
+                     rolenames.ROLE_FRAME]
+        if util.isDesiredFocusedItem(event.source, rolesList):
+            frame = event.source.parent.parent.parent
+            if frame.name.startswith(_("Check Spelling")):
+                debug.println(debug.LEVEL_FINEST,
+                      "gedit.onNameChanged - check spelling dialog.")
+
+                self.readMisspeltWord(event, event.source.parent)
+                # Fall-thru to process the event with the default handler.
+
+        # Pass the event onto the parent class to be handled in the default way.
+        default.Script.onNameChanged(self, event)
+
