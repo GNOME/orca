4578629fbaf365e8f18b4a3f3fd6e62b6ff01213
William Walker <wwalker@src.gnome.org>
2006-05-12 19:11:26 +0000

    Add onTextInserted to gnome-terminal

--- src/orca/scripts/gnome-terminal.py
+++ src/orca/scripts/gnome-terminal.py
@@ -17,9 +17,15 @@
 # Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 # Boston, MA 02111-1307, USA.
 
+import orca.atspi as atspi
+import orca.debug as debug
 import orca.default as default
+import orca.input_event as input_event
 import orca.util as util
 import orca.orca as orca
+import orca.rolenames as rolenames
+import orca.settings as settings
+import orca.speech as speech
 
 ########################################################################
 #                                                                      #
@@ -51,3 +57,69 @@ class Script(default.Script):
     #        orca.setLocusOfFocus(event, obj)
     #    else:
     #        default.Script.onWindowActivated(self, event)
+
+    def onTextInserted(self, event):
+        """Called whenever text is inserted into an object.
+
+        Arguments:
+        - event: the Event
+        """
+
+        # Ignore text insertions from non-focused objects, unless the
+        # currently focused object is the parent of the object from which
+        # text was inserted.
+        #
+        if (event.source != orca.locusOfFocus) \
+            and (event.source.parent != orca.locusOfFocus):
+            return
+
+        # We'll also ignore sliders because we get their output via
+        # their values changing.
+        #
+        if event.source.role == rolenames.ROLE_SLIDER:
+            return
+
+        self.updateBraille(event.source)
+
+        text = event.any_data.value()
+
+        debug.println(debug.eventDebugLevel, "text='%s'" % text)
+
+        # If the last input event was a keyboard event, check to see if
+        # the text for this event matches what the user typed. If it does,
+        # then don't speak it.
+        #
+        # Note that the text widgets sometimes compress their events,
+        # thus we might get a longer string from a single text inserted
+        # event, while we also get individual keyboard events for the
+        # characters used to type the string.  This is ugly.  We attempt
+        # to handle it here by only echoing text if we think it was the
+        # result of a command (e.g., a paste operation).
+        #
+        # Note that we have to special case the space character as it
+        # comes across as "space" in the keyboard event and " " in the
+        # text event.
+        #
+        if isinstance(orca.lastInputEvent, input_event.KeyboardEvent):
+            keyString = orca.lastInputEvent.event_string
+            wasCommand = orca.lastInputEvent.modifiers \
+                         & (atspi.Accessibility.MODIFIER_CONTROL \
+                            | atspi.Accessibility.MODIFIER_ALT \
+                            | atspi.Accessibility.MODIFIER_META \
+                            | atspi.Accessibility.MODIFIER_META2 \
+                            | atspi.Accessibility.MODIFIER_META3)
+            wasCommand = wasCommand or (keyString == "Return")
+            if (text == " " and keyString == "space") \
+                or (text == keyString):
+                pass
+            elif wasCommand:
+                if text.isupper():
+                    speech.speak(text, self.voices[settings.UPPERCASE_VOICE])
+                else:
+                    speech.speak(text)
+
+        if settings.enableEchoByWord \
+               and ((keyString == "Return") \
+                    or util.isWordDelimiter(text[-1:])):
+            self.echoPreviousWord(event.source)
+
