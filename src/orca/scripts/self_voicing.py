05ae5bf893f6b833bad9837500ff590f9605129b
William Walker <wwalker@src.gnome.org>
2006-05-18 20:42:00 +0000

    Add support for self_voicing script.

---
+++ src/orca/scripts/self_voicing.py
@@ -0,0 +1,74 @@
+# Orca
+#
+# Copyright 2006 Sun Microsystems Inc.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General Public
+# License along with this library; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 02111-1307, USA.
+
+"""A script to do nothing.  This is for self-voicing apps."""
+
+import orca.script
+
+class Script(orca.script.Script):
+    """A script to do nothing.  This is for self-voicing apps."""
+    
+    def __init__(self, app):
+        """Creates a script for the given application, if necessary.
+        This method should not be called by anyone except the
+        focus_tracking_presenter.
+
+        Arguments:
+        - app: the Python Accessible application to create a script for
+        """
+
+        orca.script.Script.__init__(self, app)
+
+    def getBrailleGenerator(self):
+        """Returns the braille generator for this script.
+        """
+        return None
+
+    def getSpeechGenerator(self):
+        """Returns the speech generator for this script.
+        """
+        return None
+
+    def processObjectEvent(self, event):
+        """Does nothing.
+
+        Arguments:
+        - event: the Event
+        """
+        pass
+
+    def processKeyboardEvent(self, keyboardEvent):
+        """Does nothing.
+
+        Arguments:
+        - keyboardEvent: an instance of input_event.KeyboardEvent
+
+        Returns False to indicate the event was not consumed.
+        """
+        return False
+
+    def processBrailleEvent(self, brailleEvent):
+        """Does nothing.
+
+        Arguments:
+        - brailleEvent: an instance of input_event.BrailleEvent
+
+        Returns False to indicate the event was not consumed.
+        """
+        return False
