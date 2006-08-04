6a32a00fda9967da529f20409826566b1e6b3f0c
William Walker <wwalker@src.gnome.org>
2006-08-04 15:37:25 +0000

    Added new voice parameters for EMACSpeak support for DECtalk (thanks Joanie Diggs!)

--- src/orca/dectalk.py
+++ src/orca/dectalk.py
@@ -32,6 +32,7 @@ __date__ = "$Date$"
 __copyright__ = "Copyright (c) 2005 Google Inc."
 __license__ = "LGPL"
 
+
 _defined_voices = {}
 
 # Map from ACSS dimensions to Dectalk settings:
@@ -43,7 +44,7 @@ _table['family'] = {
     'male' : ' :np ',
     'paul' :  ':np',
     'man' :  ':nh',
-    'man' : ' :nh ',
+    'harry' : ' :nh ',
     'dennis' :  ':nd',
     'frank' :  ':nf',
     'betty' :  ':nb',
@@ -76,14 +77,17 @@ _male_ap = [
     (1, 101, 112),
     (2, 108, 109),
     (3, 112, 106),
-    (4, 118, 103, ),
+    (4, 118, 103),
     (5, 122, 100),
     (6, 128, 98),
     (7, 134, 96),
     (8, 140, 94),
-    (9, 147, 91)]
+    (9, 147, 91)
+    ]
 
 _update_map(_table, ('male', 'average-pitch'),
+            " ap %s hs %s ",  _male_ap)
+_update_map(_table, ('paul', 'average-pitch'),
             " ap %s hs %s ",  _male_ap)
 
 #Man  has a big head --and a lower pitch for the middle setting
@@ -92,7 +96,7 @@ _man_ap = [
     (1, 59, 123),
     (2, 68, 121),
     (3, 77, 120),
-    (4, 83, 118, ),
+    (4, 83, 118),
     (5, 89, 115),
     (6, 95, 112),
     (7, 110, 105),
@@ -102,13 +106,15 @@ _man_ap = [
 
 _update_map(_table,('man', 'average-pitch'),
             " ap %s hs %s ",_man_ap)
+_update_map(_table,('harry', 'average-pitch'),
+            " ap %s hs %s ",_man_ap)
 
 _female_ap = [
     (0, 160, 115),
     (1, 170, 112),
     (2, 181, 109),
     (3, 192, 106),
-    (4, 200, 103, ),
+    (4, 200, 103),
     (5, 208, 100),
     (6, 219, 98),
     (7, 225, 96),
@@ -118,6 +124,122 @@ _female_ap = [
 
 _update_map(_table, ('female', 'average-pitch'),
             " ap %s hs %s ",_female_ap)
+_update_map(_table, ('betty', 'average-pitch'),
+            " ap %s hs %s ",_female_ap)
+
+# The default DECtalk values for the pitch of the other voices seem
+# to be as follows:
+# Frank = 155, Dennis = 110, Ursula = 240, Rita = 106, Wendy = 200
+# Kit = Child = 306
+# Therefore, follow TV Raman's lead:
+
+_frank_ap = [
+    (0, 129, 115),
+    (1, 134, 112),
+    (2, 141, 109),
+    (3, 145, 106),
+    (4, 151, 103),
+    (5, 155, 100),
+    (6, 159, 98),
+    (7, 165, 96),
+    (8, 171, 94),
+    (9, 178, 91)
+    ]
+
+_update_map(_table, ('frank', 'average-pitch'),
+            " ap %s hs %s ",  _frank_ap)
+
+_dennis_ap = [
+    (0, 84, 115),
+    (1, 89, 112),
+    (2, 96, 109),
+    (3, 100, 106),
+    (4, 106, 103),
+    (5, 110, 100),
+    (6, 116, 98),
+    (7, 122, 96),
+    (8, 128, 94),
+    (9, 135, 91)
+    ]
+
+_update_map(_table, ('dennis', 'average-pitch'),
+            " ap %s hs %s ",  _dennis_ap)
+
+_ursula_ap = [
+    (0, 196, 115),
+    (1, 206, 112),
+    (2, 215, 109),
+    (3, 224, 106),
+    (4, 232, 103),
+    (5, 240, 100),
+    (6, 251, 98),
+    (7, 265, 96),
+    (8, 280, 94),
+    (9, 300, 91)
+    ]
+
+_update_map(_table, ('ursula', 'average-pitch'),
+            " ap %s hs %s ",  _ursula_ap)
+
+_rita_ap = [
+    (0, 62, 115),
+    (1, 72, 112),
+    (2, 81, 109),
+    (3, 90, 106),
+    (4, 98, 103),
+    (5, 106, 100),
+    (6, 117, 98),
+    (7, 131, 96),
+    (8, 146, 94),
+    (9, 166, 91)
+    ]
+
+_update_map(_table, ('rita', 'average-pitch'),
+            " ap %s hs %s ",  _rita_ap)
+
+# For some reason, Wendy at a high pitch causes the
+# synthesizer to click and eventually make a feedback sound!
+# It doesn't seem to be the result of the pitch.
+# Keeping head size constant for higher pitch seems to eliminate
+# the problem.
+
+_wendy_ap = [
+    (0, 156, 115),
+    (1, 166, 112),
+    (2, 175, 109),
+    (3, 184, 106),
+    (4, 192, 103),
+    (5, 200, 100),
+    (6, 211, 100),
+    (7, 225, 100),
+    (8, 240, 100),
+    (9, 260, 100)
+    ]
+
+_update_map(_table, ('wendy', 'average-pitch'),
+            " ap %s hs %s ",  _wendy_ap)
+
+# Kit/Child can't have the traditional adult head size
+# Setting the largest head size is the smallest adult
+# female head size.
+
+_child_ap = [
+    (0, 256, 91),
+    (1, 266, 89),
+    (2, 276, 87),
+    (3, 286, 85),
+    (4, 296, 83),
+    (5, 306, 81),
+    (6, 316, 79),
+    (7, 326, 77),
+    (8, 336, 75),
+    (9, 346, 73)
+    ]
+
+_update_map(_table, ('child', 'average-pitch'),
+            " ap %s hs %s ",  _child_ap)
+_update_map(_table, ('kit', 'average-pitch'),
+            " ap %s hs %s ",  _child_ap)
 
 # pitch-range for male:
 
@@ -143,6 +265,17 @@ _male_pr = [
 
 _update_map(_table, ('male', 'pitch-range'),
             " pr %s as %s ", _male_pr)
+_update_map(_table, ('paul', 'pitch-range'),
+            " pr %s as %s ", _male_pr)
+
+# For now, assume that standard pitch range is reasonably
+# consistent for all male voices with the execption of harry
+
+_update_map(_table, ('frank', 'pitch-range'),
+            " pr %s as %s ", _male_pr)
+_update_map(_table, ('dennis', 'pitch-range'),
+            " pr %s as %s ", _male_pr)
+
 
 _man_pr = [
     (0, 0, 0),
@@ -159,6 +292,8 @@ _man_pr = [
 
 _update_map(_table, ('man', 'pitch-range'),
             " pr %s as %s ", _man_pr)
+_update_map(_table, ('harry', 'pitch-range'),
+            " pr %s as %s ", _man_pr)
 
 _female_pr = [
     (0, 0, 0),
@@ -174,6 +309,22 @@ _female_pr = [
     ]
 
 _update_map(_table, ('female', 'pitch-range'),
+            " pr %s as %s ", _female_pr)
+_update_map(_table, ('betty', 'pitch-range'),
+            " pr %s as %s ", _female_pr)
+
+# For now, assume that standard pitch range is reasonably
+# consistent for all female voices, including kit
+
+_update_map(_table, ('ursula', 'pitch-range'),
+            " pr %s as %s ", _female_pr)
+_update_map(_table, ('rita', 'pitch-range'),
+            " pr %s as %s ", _female_pr)
+_update_map(_table, ('wendy', 'pitch-range'),
+            " pr %s as %s ", _female_pr)
+_update_map(_table, ('kit', 'pitch-range'),
+            " pr %s as %s ", _female_pr)
+_update_map(_table, ('child', 'pitch-range'),
             " pr %s as %s ", _female_pr)
 
 # Stress:
@@ -202,13 +353,23 @@ _male_stress =[
 
 _update_map(_table, ('male', 'stress'),
             " hr %s sr %s qu %s bf %s ", _male_stress)
+_update_map(_table, ('paul', 'stress'),
+            " hr %s sr %s qu %s bf %s ", _male_stress)
+
+# For now, grabbing these values for all males but Harry
+
+_update_map(_table, ('frank', 'stress'),
+            " hr %s sr %s qu %s bf %s ", _male_stress)
+_update_map(_table, ('dennis', 'stress'),
+            " hr %s sr %s qu %s bf %s ", _male_stress)
+
 
 _man_stress = [
     (0, 0, 0, 0, 0),
-    (1, 4, 6, 2, 2, ),
-    (2, 8, 12, 4, 4, ),
-    (3, 12, 18, 6, 6, ),
-    (4, 16, 24, 8, 8, ),
+    (1, 4, 6, 2, 2),
+    (2, 8, 12, 4, 4),
+    (3, 12, 18, 6, 6),
+    (4, 16, 24, 8, 8),
     (5, 20, 30, 10, 9),
     (6, 40, 48, 32, 16),
     (7, 60, 66, 54, 22),
@@ -218,22 +379,38 @@ _man_stress = [
 
 _update_map(_table, ('man', 'stress'),
             " hr %s sr %s qu %s bf %s ", _man_stress)
+_update_map(_table, ('harry', 'stress'),
+            " hr %s sr %s qu %s bf %s ", _man_stress)
 
 _female_stress = [
     (0, 1, 1, 0, 0),
     (1, 3, 4, 11, 0),
     (2, 5, 8, 22, 0),
-    (3, 8, 12, 33, 0, ),
+    (3, 8, 12, 33, 0),
     (4, 11, 16, 44, 0),
     (5, 14, 20, 55, 0),
     (6, 35, 40, 65, 10),
     (7, 56, 80, 75, 20),
     (8, 77, 90, 85, 30),
     (9, 100, 100, 100, 40)
-
     ]
 
 _update_map(_table, ('female', 'stress'),
+            " hr %s sr %s qu %s bf %s ", _female_stress)
+_update_map(_table, ('betty', 'stress'),
+            " hr %s sr %s qu %s bf %s ", _female_stress)
+
+# For now, grabbing these values for all females including kit
+
+_update_map(_table, ('ursula', 'stress'),
+            " hr %s sr %s qu %s bf %s ", _female_stress)
+_update_map(_table, ('rita', 'stress'),
+            " hr %s sr %s qu %s bf %s ", _female_stress)
+_update_map(_table, ('wendy', 'stress'),
+            " hr %s sr %s qu %s bf %s ", _female_stress)
+_update_map(_table, ('kit', 'stress'),
+            " hr %s sr %s qu %s bf %s ", _female_stress)
+_update_map(_table, ('child', 'stress'),
             " hr %s sr %s qu %s bf %s ", _female_stress)
 
 #richness
@@ -257,6 +434,15 @@ _male_richness = [
 
 _update_map(_table, ('male', 'richness'),
             " ri %s sm %s " ,_male_richness)
+_update_map(_table, ('paul', 'richness'),
+            " ri %s sm %s " ,_male_richness)
+
+# For now, grabbing these values for all males but Harry
+
+_update_map(_table, ('frank', 'richness'),
+            " ri %s sm %s " ,_male_richness)
+_update_map(_table, ('dennis', 'richness'),
+            " ri %s sm %s " ,_male_richness)
 
 _man_richness = [
     (0, 100, 0),
@@ -272,6 +458,8 @@ _man_richness = [
     ]
 
 _update_map(_table, ('man', 'richness'),
+            " ri %s sm %s " , _man_richness)
+_update_map(_table, ('harry', 'richness'),
             " ri %s sm %s " , _man_richness)
 
 _female_richness = [
@@ -289,7 +477,26 @@ _female_richness = [
 
 _update_map(_table, ('female', 'richness'),
             " ri %s sm %s ", _female_richness)
+_update_map(_table, ('betty', 'richness'),
+            " ri %s sm %s ", _female_richness)
+
+# For now, grabbing these values for all females including kit
+
+_update_map(_table, ('ursula', 'richness'),
+            " ri %s sm %s ", _female_richness)
+_update_map(_table, ('rita', 'richness'),
+            " ri %s sm %s ", _female_richness)
+_update_map(_table, ('wendy', 'richness'),
+            " ri %s sm %s ", _female_richness)
+_update_map(_table, ('kit', 'richness'),
+            " ri %s sm %s ", _female_richness)
+_update_map(_table, ('child', 'richness'),
+            " ri %s sm %s ", _female_richness)
+
+
 def getrate(r):    return int(180 + 4*r)
+
+def getvolume(v):  return int(10*v)
 
 def getvoicelist(): return _table['family'].keys()
 
@@ -313,6 +520,7 @@ def acss2voice(acss):
             code += _table['family'][familyName]
     if 'rate' in acss: code += " :ra %s" % getrate(acss['rate'])
     if 'punctuations' in acss: code += " :punc %s" %acss['punctuations']
+    if 'gain' in acss: code += " :volume set %s" % getvolume(acss['gain'])
     voice = ""
     dv = ""
     for d in ['average-pitch', 'pitch-range',
