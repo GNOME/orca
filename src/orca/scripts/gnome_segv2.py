7d230c1f795dfefd9d2819d1592fbcb55a59e499
Rich Burridge <richb@src.gnome.org>
2006-10-01 02:38:15 +0000

    src/orca/scripts/gnome_segv2.py: Fixed bug #344192 - Orca doesn't report
            * src/orca/scripts/Makefile.am:
              src/orca/scripts/gnome_segv2.py:
              Fixed bug #344192 - Orca doesn't report on "Application has
              unexpected crashed" dialogs. Added in a new gnome_segv2.py
              script and overrode the locusOfFocusChanged() method. If the
              gnome_segv2 application is running it's probably because
              some application has crashed unexpectedly. If we just called
              locusOfFocusChanged() in default.py we would hang. To
              circumvent this, we set the old locus of focus to None, then
              we can safely call the locusOfFocusChanged() method in default.py.

---
+++ src/orca/scripts/gnome_segv2.py
@@ -0,0 +1,73 @@
+# Orca
+#
+# Copyright 2006 Sun Microsystems Inc.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General Public
+# License along with this library; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 02111-1307, USA.
+
+"""Custom script for gnome_segv2."""
+
+__id__        = "$Id$"
+__version__   = "$Revision$"
+__date__      = "$Date$"
+__copyright__ = "Copyright (c) 2005-2006 Sun Microsystems Inc."
+__license__   = "LGPL"
+
+import orca.debug as debug
+import orca.default as default
+import orca.atspi as atspi
+import orca.rolenames as rolenames
+import orca.orca as orca
+import orca.braille as braille
+import orca.speech as speech
+import orca.settings as settings
+import orca.util as util
+
+from orca.orca_i18n import _ # for gettext support
+
+
+########################################################################
+#                                                                      #
+# The gnome_segv2 script class.                                            #
+#                                                                      #
+########################################################################
+
+class Script(default.Script):
+
+    def __init__(self, app):
+        """Creates a new script for the given application.
+
+        Arguments:
+        - app: the application to create a script for.
+        """
+
+        default.Script.__init__(self, app)
+
+    def locusOfFocusChanged(self, event, oldLocusOfFocus, newLocusOfFocus):
+        """Called when the visual object with focus changes.
+
+        Arguments:
+        - event: if not None, the Event that caused the change
+        - oldLocusOfFocus: Accessible that is the old locus of focus
+        - newLocusOfFocus: Accessible that is the new locus of focus
+        """
+
+        # If the gnome_segv2 application is running it's probably because
+        # some application has crashed unexpectedly. If we just called
+        # locusOfFocusChanged() in default.py we would hang (see bug #344192).
+        # To circumvent this, we set the old locus of focus to None, then
+        # we can safely call the locusOfFocusChanged() method in default.py.
+        #
+        default.Script.locusOfFocusChanged(self, event, None, newLocusOfFocus)
