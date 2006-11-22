e7fb66be00303f5f687b2a7eff1054d0c5cc748c
Rich Burridge <richb@src.gnome.org>
2006-11-22 15:56:10 +0000

    Fixed bug #377955 - Suggested additions to pronunciation dictionary.
            * src/orca/pronunciation_dict.py:
              Fixed bug #377955 - Suggested additions to pronunciation
              dictionary.

--- src/orca/pronunciation_dict.py
+++ src/orca/pronunciation_dict.py
@@ -36,9 +36,13 @@ from orca_i18n import _ # for gettext support
 #
 pronunciation_dict = {}
 pronunciation_dict[_("ASAP")]    = _("as soon as possible")
+pronunciation_dict[_("GHz")]     = _("gigahertz")
 pronunciation_dict[_("IMAP")]    = _("eye map")
+pronunciation_dict[_("LDAP")]    = _("ell dap")
 pronunciation_dict[_("LOL")]     = _("laughing out loud")
+pronunciation_dict[_("MHz")]     = _("megahertz")
 pronunciation_dict[_("strikethrough")] = _("strike through")
+pronunciation_dict[_("SELinux")] = _("ess ee linux")
 
 def getPronunciation(word):
     """Given a word, return a string that represents what this word
