e27c44b1ded916367fcfcc7f5c48d3943a704d74
Lynn Monsanto <lmonsanto@src.gnome.org>
2007-01-17 20:27:12 +0000

    Filters out unrelated text in the Account Preferences Dialog
    svn path=/trunk/; revision=1934

--- src/orca/scripts/Thunderbird.py
+++ src/orca/scripts/Thunderbird.py
@@ -31,11 +31,194 @@ import orca.debug as debug
 import orca.default as default
 import orca.rolenames as rolenames
 import orca.settings as settings
+import orca.speechgenerator as speechgenerator
 import orca.speech as speech
 import orca.util as util
 import orca.Gecko as Gecko
 
 from orca.orca_i18n import _
+
+
+########################################################################
+#                                                                      #
+# Custom SpeechGenerator for Thunderbird                               #
+#                                                                      #
+########################################################################
+
+class SpeechGenerator(speechgenerator.SpeechGenerator):
+    """Provides a speech generator specific to Thunderbird.
+    """
+
+    def __init__(self, script):
+        # Set the debug level for all the methods in this script.
+        #
+        self.debugLevel = debug.LEVEL_FINEST
+
+        self._debug("__init__")
+        speechgenerator.SpeechGenerator.__init__(self, script)
+        
+
+    def _debug(self, msg):
+        """ Convenience method for printing debug messages
+        """
+        debug.println(self.debugLevel, "Thunderbird.SpeechGenerator: "+msg)
+
+
+    def _getSpeechForAlert(self, obj, already_focused):
+        """Gets the title of the dialog and the contents of labels inside the
+        dialog that are not associated with any other objects.
+
+        Arguments:
+        - obj: the Accessible dialog
+        - already_focused: False if object just received focus
+
+        Returns a list of utterances be spoken.
+        """
+
+        utterances = []
+        label = self._getSpeechForObjectLabel(obj)
+        utterances.extend(label)
+        name = self._getSpeechForObjectName(obj)
+        if name != label:
+            utterances.extend(name)
+
+        # Find all the unrelated labels in the dialog and speak them.
+        #
+        labels = util.findUnrelatedLabels(obj)
+
+        for label in labels:
+            utterances.append(label.name)
+
+        # Remove all static text except for text at the beginning
+        # of the dialog. This is what the user needs to hear
+        # when the dialog is first presented.
+        if utterances[0] == 'Account Settings':
+
+            if utterances[1] == 'Server Settings':
+                utterances = utterances[0:4]
+                
+            elif utterances[1] == 'Copies & Folders':
+                utterances = utterances[0:2]
+        
+            elif utterances[1] == 'Composition & Addressing':
+                utterances = utterances[0:2]
+        
+            elif utterances[1] == 'Offline & Disk Space':
+                utterances = utterances[0:3]
+        
+            elif utterances[1] == 'Junk Settings':
+                utterances = utterances[0:3]
+        
+            elif utterances[1] == 'Return Receipts':
+                utterances = utterances[0:2]
+        
+            elif utterances[1] == 'Security':
+                utterances = utterances[0:3]
+        
+            elif utterances[1] == 'Account Settings':
+                utterances = utterances[1:3]
+        
+            elif utterances[1] == 'Disk Space':
+                utterances = utterances[0:2]
+        
+            elif utterances[1] == 'Junk Settings':
+                utterances = utterances[0:3]
+        
+            elif utterances[1] == 'Outgoing Server (SMTP) Settings':
+                utterances = utterances[0:2]
+        
+        self._debug("unrelated labels='%s'" % utterances)
+        return utterances
+
+
+    def _getSpeechForDialog(self, obj, already_focused):
+        """Get the speech for a dialog box.
+
+        Arguments:
+        - obj: the dialog box
+        - already_focused: False if object just received focus
+
+        Returns a list of utterances to be spoken for the object.
+        """
+
+        return self._getSpeechForAlert(obj, already_focused)
+
+
+    def getSpeechContext(self, obj, stopAncestor=None):
+        """Get the speech that describes the names and role of
+        the container hierarchy of the object, stopping at and
+        not including the stopAncestor.
+
+        Arguments:
+        - obj: the object
+        - stopAncestor: the ancestor to stop at and not include (None
+          means include all ancestors)
+
+        Returns a list of utterances to be spoken.
+        """
+
+        utterances = []
+
+        if not obj:
+            return utterances
+
+        if obj is stopAncestor:
+            return utterances
+
+        parent = obj.parent
+        while parent and (parent.parent != parent):
+            if parent == stopAncestor:
+                break
+
+            # We try to omit things like fillers off the bat...
+            # Also, ignores panels.
+            #
+            if (parent.role == rolenames.ROLE_FILLER) \
+                or (parent.role == rolenames.ROLE_PANEL) \
+                or (parent.role == rolenames.ROLE_FORM) \
+                or (parent.role == rolenames.ROLE_LIST_ITEM) \
+                or (parent.role == rolenames.ROLE_LIST) \
+                or (parent.role == rolenames.ROLE_PARAGRAPH) \
+                or (parent.role == rolenames.ROLE_SECTION) \
+                or (parent.role == rolenames.ROLE_LAYERED_PANE) \
+                or (parent.role == rolenames.ROLE_SPLIT_PANE) \
+                or (parent.role == rolenames.ROLE_SCROLL_PANE) \
+                or (parent.role == rolenames.ROLE_UNKNOWN) \
+                or (self._script.isLayoutOnly(parent)):
+                parent = parent.parent
+                continue
+
+            # Well...if we made it this far, we will now append the
+            # role, then the text, and then the label.
+            #
+            if parent.role != rolenames.ROLE_TABLE_CELL:
+                self._debug("gettingSpeechForRole: '%s'" % parent.role)
+                utterances.append(rolenames.getSpeechForRoleName(parent))
+
+            # Now...autocompletes are wierd.  We'll let the handling of
+            # the entry give us the name.
+            #
+            if parent.role == rolenames.ROLE_AUTOCOMPLETE:
+                parent = parent.parent
+                continue
+
+            # Finally, put in the text and label (if they exist)
+            # Skip displayed text that starts with "chrome://"
+            #
+            text = util.getDisplayedText(parent)
+            label = util.getDisplayedLabel(parent)
+            if text and (text != label) and len(text) and \
+                   (not text.startswith("chrome://")):
+                utterances.append(text)
+            if label and len(label):
+                utterances.append(label)
+
+            parent = parent.parent
+
+        utterances.reverse()
+        self._debug("'%s'" % utterances)
+
+        return utterances
 
 
 ########################################################################
@@ -62,6 +245,12 @@ class Script(Gecko.Script):
         Gecko.Script.__init__(self, app)
 
 
+    def getSpeechGenerator(self):
+        """Returns the speech generator for this script.
+        """
+        return SpeechGenerator(self)
+
+
     def _debug(self, msg):
         """ Convenience method for printing debug messages
         """
@@ -79,12 +268,12 @@ class Script(Gecko.Script):
         top = util.getTopLevel(obj)
         consume = False
 
-        self._debug("onFocus: name='%s', role='%s', top name='%s', top role='%s'" \
-                    % (obj.name, obj.role, top.name, top.role))
+        # self._debug("onFocus: name='%s', role='%s', top name='%s', top role='%s'" \
+        #             % (obj.name, obj.role, top.name, top.role))
 
         # Don't speak a chrome URL.
-        if obj.name.startswith("_(chrome://"):
-            return
+        # if obj.name.startswith(_("chrome://")):
+        #    return
 
         # Handle dialogs.
         if top.role == rolenames.ROLE_DIALOG:
