8c1f69f57ed78aeb67c940fb1b23e0749374e795
Rich Burridge <richb@src.gnome.org>
2007-05-08 16:58:45 +0000

    Fix for bug #435201 – Orca is too chatty when navigating by paragraph in
            * src/orca/scripts/StarOffice.py:
              Fix for bug #435201 – Orca is too chatty when navigating 
              by paragraph in OOo Writer.
              Ignore "object:text-caret-moved" events when moving my paragraph 
              in OOo Writer using Control-Up/Down, if event.detail1 is -1.
    
    svn path=/trunk/; revision=2356

--- src/orca/scripts/StarOffice.py
+++ src/orca/scripts/StarOffice.py
@@ -2108,6 +2108,12 @@ class Script(default.Script):
                     speech.speak(_("blank"), None, False)
                 return
 
+        # If detail1 from the "object:text-caret-moved" event is -1, 
+        # then just ignore it. See Orca bug #435201 for more details.
+        #
+        if event.detail1 == -1:
+            return
+
         # Speak a newline, if appropriate.
         if self.speakNewLine(event.source):
             speech.speak(chnames.getCharacterName("\n"), None, False)
