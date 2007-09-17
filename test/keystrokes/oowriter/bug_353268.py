ae2c913f7c63084a70d378b6b30ece4c7694f316
Rich Burridge <richb@src.gnome.org>
2007-09-17 16:43:31 +0000

    Macaroon test file for bug #3353268 - Orca is double reading lines in
            * test/keystrokes/oowriter/bug_353268.py:  (new)
              Macaroon test file for bug #3353268 - Orca is double reading
              lines in openoffice with latest Ubuntu live CD.
    
    svn path=/trunk/; revision=2867

---
+++ test/keystrokes/oowriter/bug_353268.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python
+
+"""Test to verify bug #353268 is still fixed.
+   Orca is double reading lines in openoffice with latest Ubuntu live CD.
+"""
+
+from macaroon.playback.keypress_mimic import *
+
+sequence = MacroSequence()
+
+######################################################################
+# 1. Start oowriter.
+#
+sequence.append(WaitForWindowActivate("Untitled1 - OpenOffice.org Writer",None))
+sequence.append(WaitForFocus("", acc_role=pyatspi.ROLE_PARAGRAPH))
+
+######################################################################
+# 2. Enter Alt-f, right arrow and Return.  (File->New->Text Document).
+#
+sequence.append(KeyComboAction("<Alt>f"))
+sequence.append(WaitForFocus("New", acc_role=pyatspi.ROLE_MENU))
+
+sequence.append(KeyComboAction("Right"))
+sequence.append(WaitForFocus("Text Document", acc_role=pyatspi.ROLE_MENU_ITEM))
+
+sequence.append(KeyComboAction("Return"))
+sequence.append(WaitForFocus("", acc_role=pyatspi.ROLE_PARAGRAPH))
+
+######################################################################
+# 3. Enter a couple lines of text:
+#    "Line 1"
+#    "Line 2"
+#
+sequence.append(TypeAction("Line 1"))
+sequence.append(KeyComboAction("Return"))
+sequence.append(WaitForFocus("", acc_role=pyatspi.ROLE_PARAGRAPH))
+sequence.append(TypeAction("Line 2"))
+sequence.append(KeyComboAction("Return"))
+sequence.append(WaitForFocus("", acc_role=pyatspi.ROLE_PARAGRAPH))
+
+######################################################################
+# 4. Enter Control-Home to return to the top of the document.
+#
+sequence.append(KeyComboAction("<Control>Home"))
+sequence.append(WaitForFocus("", acc_role=pyatspi.ROLE_PARAGRAPH))
+
+######################################################################
+# 5. Arrow down over the first line of text.
+sequence.append(KeyComboAction("Down"))
+sequence.append(WaitForFocus("", acc_role=pyatspi.ROLE_PARAGRAPH))
+
+######################################################################
+# 6. Arrow down over the second line of text.
+#
+sequence.append(KeyComboAction("Down"))
+sequence.append(WaitForFocus("", acc_role=pyatspi.ROLE_PARAGRAPH))
+
+######################################################################
+# 7. Enter Alt-f, Alt-c to close the Writer application.
+#
+sequence.append(KeyComboAction("<Alt>f"))
+sequence.append(WaitForFocus("New", acc_role=pyatspi.ROLE_MENU))
+
+sequence.append(KeyComboAction("<Alt>c"))
+sequence.append(WaitForFocus("Save", acc_role=pyatspi.ROLE_PUSH_BUTTON))
+
+######################################################################
+# 8. Enter Tab and Return to discard the current changes.
+#
+sequence.append(KeyComboAction("Tab"))
+sequence.append(WaitForFocus("Discard", acc_role=pyatspi.ROLE_PUSH_BUTTON))
+
+sequence.append(KeyComboAction("Return"))
+
+sequence.start()
