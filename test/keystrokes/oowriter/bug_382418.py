144753dfd5d77e6d1ef9e7717b29ae1ce9d9fff9
Rich Burridge <richb@src.gnome.org>
2007-09-18 19:17:21 +0000

    Macaroon test file for bug #382418 – Orca should announce when you
            * test/keystrokes/oowriter/bug_382418.py:  (new)
              Macaroon test file for bug #382418 – Orca should announce when
              you enter/leave a table in OOo Writer documents.
    
    svn path=/trunk/; revision=2879

---
+++ test/keystrokes/oowriter/bug_382418.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python
+
+"""Test to verify bug #469367 is still fixed.
+   Orca StarOffice script not properly announcing (potential) indentation
+   in OOo Writer.
+"""
+
+from macaroon.playback.keypress_mimic import *
+
+sequence = MacroSequence()
+
+######################################################################
+# 1. Start oowriter.
+#
+sequence.append(WaitForWindowActivate("Untitled1 - OpenOffice.org Writer",None))
+sequence.append(WaitForFocus("", acc_role=pyatspi.ROLE_PARAGRAPH))
+
+######################################################################
+# 2. Enter Alt-f, right arrow and Return.  (File->New->Text Document).
+#
+sequence.append(KeyComboAction("<Alt>f"))
+sequence.append(WaitForFocus("New", acc_role=pyatspi.ROLE_MENU))
+
+sequence.append(KeyComboAction("Right"))
+sequence.append(WaitForFocus("Text Document", acc_role=pyatspi.ROLE_MENU_ITEM))
+
+sequence.append(KeyComboAction("Return"))
+sequence.append(WaitForWindowActivate("Untitled2 - OpenOffice.org Writer",None))
+sequence.append(WaitForFocus("", acc_role=pyatspi.ROLE_PARAGRAPH))
+
+######################################################################
+# 3. Enter the following line and press Return:
+#    Line 1
+#
+sequence.append(TypeAction("Line 1"))
+sequence.append(KeyComboAction("Return"))
+sequence.append(WaitForFocus("", acc_role=pyatspi.ROLE_PARAGRAPH))
+
+######################################################################
+# 4. Enter Alt-a, right arrow and Return.  (Table->Insert->Table...).
+#
+sequence.append(KeyComboAction("<Alt>a"))
+sequence.append(WaitForFocus("Insert", acc_role=pyatspi.ROLE_MENU))
+
+sequence.append(KeyComboAction("Right"))
+sequence.append(WaitForFocus("Table...", acc_role=pyatspi.ROLE_MENU_ITEM))
+
+sequence.append(KeyComboAction("Return"))
+sequence.append(WaitForFocus("Name", acc_role=pyatspi.ROLE_TEXT))
+
+######################################################################
+# 5. Enter Return (Insert a table with the default parameters - 2x2).
+#
+sequence.append(KeyComboAction("Return"))
+sequence.append(WaitForFocus("", acc_role=pyatspi.ROLE_PARAGRAPH))
+
+######################################################################
+# 6. Type Control-Home to move the text caret to the start of the
+#    document (and leave the table).
+#
+sequence.append(KeyComboAction("<Control>Home"))
+sequence.append(WaitForFocus("", acc_role=pyatspi.ROLE_PARAGRAPH))
+
+######################################################################
+# 7. Type a down arrow to enter the table.
+#
+sequence.append(KeyComboAction("Down"))
+sequence.append(WaitForFocus("", acc_role=pyatspi.ROLE_PARAGRAPH))
+
+######################################################################
+# 8. Enter Alt-f, Alt-c to close the Writer application.
+#    A save dialog will appear.
+#
+sequence.append(KeyComboAction("<Alt>f"))
+sequence.append(WaitForFocus("New", acc_role=pyatspi.ROLE_MENU))
+
+sequence.append(KeyComboAction("<Alt>c"))
+sequence.append(WaitForFocus("Save", acc_role=pyatspi.ROLE_PUSH_BUTTON))
+
+######################################################################
+# 9. Enter Tab and Return to discard the current changes.
+#
+sequence.append(KeyComboAction("Tab"))
+sequence.append(WaitForFocus("Discard", acc_role=pyatspi.ROLE_PUSH_BUTTON))
+
+sequence.append(KeyComboAction("Return"))
+
+######################################################################
+# 10. Wait for things to get back to normal.
+#
+sequence.append(WaitForWindowActivate("Untitled1 - OpenOffice.org Writer", None))
+sequence.append(WaitForFocus("", acc_role=pyatspi.ROLE_PARAGRAPH))
+sequence.append(WaitForFocus(acc_role=pyatspi.ROLE_INVALID, timeout=3000))
+
+sequence.start()
