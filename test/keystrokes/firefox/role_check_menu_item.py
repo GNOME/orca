3ed6851ba126dddd7c1acaf52f195696055549d9
Joanmarie Diggs <joanied@src.gnome.org>
2007-09-20 01:47:34 +0000

    Adjusted Macaroon Gecko tests to include comments with speech and braille
    * test/keystrokes/firefox/role_push_button.py,
      test/keystrokes/firefox/role_check_menu_item.py,
      test/keystrokes/firefox/role_check_box.py,
      test/keystrokes/firefox/role_radio_button.py,
      test/keystrokes/firefox/role_radio_menu_item.py,
      test/keystrokes/firefox/role_page_tab.py,
      test/keystrokes/firefox/role_accel_label.py:
    Adjusted Macaroon Gecko tests to include comments with speech and braille output.  Rolled the Where Am I tests into the role tests.  Switched the wait at the end to the new PauseAction().  And 
    finally: Caught a dumb mistake that was causing some timeouts.  Now we are only timing out when Firefox lies to us and says a menu is really a menu item when it's really not. :-)
    
    svn path=/trunk/; revision=2886

--- test/keystrokes/firefox/role_check_menu_item.py
+++ test/keystrokes/firefox/role_check_menu_item.py
@@ -2,7 +2,8 @@
 
 """Test of menu checkbox output using Firefox.
 """
-from macaroon.playback.keypress_mimic import *
+
+from macaroon.playback import *
 
 sequence = MacroSequence()
 
@@ -12,23 +13,52 @@ sequence = MacroSequence()
 sequence.append(WaitForWindowActivate("Minefield",None))
 
 ########################################################################
-# Open the "View" menu and Up Arrow to Full Screen.
+# Open the "View" menu.
 #
 sequence.append(KeyComboAction("<Alt>v"))
+sequence.append(WaitForFocus("Toolbars", acc_role=pyatspi.ROLE_MENU))
 
-sequence.append(WaitForFocus("Toolbars", pyatspi.ROLE_MENU))
+########################################################################
+# When focus is on Toolbars, Up Arrow to the "Full Screen" check menu
+# item. The following should be presented in speech and braille:
+#
+# BRAILLE LINE:  'Minefield Application Minefield Frame ToolBar Application MenuBar < > Full Screen CheckItem(F11)'
+#      VISIBLE:  '< > Full Screen CheckItem(F11)', cursor=1
+# SPEECH OUTPUT: ''
+# SPEECH OUTPUT: 'Full Screen check item not checked F11'
+#
 sequence.append(KeyComboAction("Up"))
-
 sequence.append(WaitForFocus("Full Screen", acc_role=pyatspi.ROLE_CHECK_MENU_ITEM))
 
 ########################################################################
-# Dismiss the menu.
+# Do a basic "Where Am I" via KP_Enter.  The following should be
+# presented in speech and braille:
+#
+# Note: Due to https://bugzilla.mozilla.org/show_bug.cgi?id=396799,
+# We might not speak the item count.
+#
+# BRAILLE LINE:  'Minefield Application Minefield Frame ToolBar Application MenuBar < > Full Screen CheckItem(F11)'
+#      VISIBLE:  '< > Full Screen CheckItem(F11)', cursor=1
+# SPEECH OUTPUT: 'View menu'
+# SPEECH OUTPUT: 'Full Screen'
+# SPEECH OUTPUT: 'check item'
+# SPEECH OUTPUT: 'not checked'
+# SPEECH OUTPUT: 'F11'
+# SPEECH OUTPUT: 'item 10 of 10'
+# SPEECH OUTPUT: ''
+#
+sequence.append(KeyComboAction("KP_Enter"))
+sequence.append(PauseAction(3000))
+
+########################################################################
+# Dismiss the menu by pressing Escape and wait for the location bar
+# to regain focus.
 #
 sequence.append(KeyComboAction("Escape"))
+sequence.append(WaitForFocus("Location", acc_role=pyatspi.ROLE_ENTRY))
 
 # Just a little extra wait to let some events get through.
 #
-sequence.append(WaitForFocus("Location", acc_role=pyatspi.ROLE_ENTRY))
-sequence.append(WaitForFocus(acc_role=pyatspi.ROLE_INVALID, timeout=3000))
+sequence.append(PauseAction(3000))
 
 sequence.start()
--- test/keystrokes/firefox/role_radio_button.py
+++ test/keystrokes/firefox/role_radio_button.py
@@ -3,7 +3,7 @@
 """Test of radio button output using Firefox.
 """
 
-from macaroon.playback.keypress_mimic import *
+from macaroon.playback import *
 
 sequence = MacroSequence()
 
@@ -17,29 +17,79 @@ sequence.append(WaitForWindowActivate("Minefield",None))
 #
 sequence.append(KeyComboAction("<Alt>f"))
 
-sequence.append(WaitForFocus("New Window", pyatspi.ROLE_MENU_ITEM))
+sequence.append(WaitForFocus("New Window", acc_role=pyatspi.ROLE_MENU_ITEM))
 sequence.append(TypeAction("u"))
+
+sequence.append(WaitForWindowActivate("Page Setup",None))
 
 ########################################################################
 # In the Page Setup dialog, focus should already be on the "Portrait"
-# radio button.  Right Arrow to "Landscape" and Left Arrow back.
+# radio button.  Right Arrow to the "Landscape" radio button. The 
+# following should be presented in speech and braille:
 #
-sequence.append(WaitForWindowActivate("Page Setup",None))
+# BRAILLE LINE:  'Minefield Application Page Setup Dialog ScrollPane Orientation: Panel &=y Landscape RadioButton'
+#     VISIBLE:  '&=y Landscape RadioButton', cursor=1
+# SPEECH OUTPUT: 'Orientation: panel'
+# SPEECH OUTPUT: 'Landscape selected radio button'
+#
+# Note that the Orientation panel was also announced because we currently
+# do not get a focus event for the Portrait radio button which initially
+# is focused when the dialog box appeared.  Thus it's new context as far
+# as we're concerned. https://bugzilla.mozilla.org/show_bug.cgi?id=388187
+#
 sequence.append(KeyComboAction("Right"))
-
-sequence.append(WaitForFocus("Landscape", pyatspi.ROLE_RADIO_BUTTON))
-sequence.append(KeyComboAction("Left"))
-
-sequence.append(WaitForFocus("Portrait", pyatspi.ROLE_RADIO_BUTTON))
+sequence.append(WaitForFocus("Landscape", acc_role=pyatspi.ROLE_RADIO_BUTTON))
 
 ########################################################################
-# Close the dialog
+# Do a basic "Where Am I" via KP_Enter.  The following should be
+# presented in speech and braille:
 #
+# BRAILLE LINE:  'Minefield Application Page Setup Dialog ScrollPane Orientation: Panel &=y Landscape RadioButton'
+#      VISIBLE:  '&=y Landscape RadioButton', cursor=1
+# SPEECH OUTPUT: 'Orientation:'
+# SPEECH OUTPUT: 'Landscape radio button'
+# SPEECH OUTPUT: 'selected'
+# SPEECH OUTPUT: ''
+# SPEECH OUTPUT: ''
+#
+sequence.append(KeyComboAction("KP_Enter"))
+sequence.append(PauseAction(3000))
+
+########################################################################
+# Left Arrow to return to the "Portrait" radio button.  The following 
+# should be presented in speech and braille:
+#
+# BRAILLE LINE:  'Minefield Application Page Setup Dialog ScrollPane Orientation: Panel &=y Portrait RadioButton'
+#      VISIBLE:  '&=y Portrait RadioButton', cursor=1
+# SPEECH OUTPUT: ''
+# SPEECH OUTPUT: 'Portrait selected radio button'
+#
+sequence.append(KeyComboAction("Left"))
+sequence.append(WaitForFocus("Portrait", acc_role=pyatspi.ROLE_RADIO_BUTTON))
+
+########################################################################
+# Do a basic "Where Am I" via KP_Enter.  The following should be
+# presented in speech and braille:
+#
+# BRAILLE LINE:  'Minefield Application Page Setup Dialog ScrollPane Orientation: Panel &=y Portrait RadioButton'
+#     VISIBLE:  '&=y Portrait RadioButton', cursor=1
+# SPEECH OUTPUT: 'Orientation:'
+# SPEECH OUTPUT: 'Portrait radio button'
+# SPEECH OUTPUT: 'selected'
+# SPEECH OUTPUT: ''
+# SPEECH OUTPUT: ''
+#
+sequence.append(KeyComboAction("KP_Enter"))
+sequence.append(PauseAction(3000))
+
+########################################################################
+# Dismiss the dialog by pressing Escape and wait for the location bar
+# to regain focus.
 sequence.append(KeyComboAction("Escape"))
+sequence.append(WaitForFocus("Location", acc_role=pyatspi.ROLE_ENTRY))
 
 # Just a little extra wait to let some events get through.
 #
-sequence.append(WaitForFocus("Location", acc_role=pyatspi.ROLE_ENTRY))
-sequence.append(WaitForFocus(acc_role=pyatspi.ROLE_INVALID, timeout=3000))
+sequence.append(PauseAction(3000))
 
 sequence.start()
