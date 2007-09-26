8c6dc28500e8390e2fa034ed562ef46a5dff156a
Rich Burridge <richb@src.gnome.org>
2007-09-26 16:35:05 +0000

    Macaroon test file for bug #364086 – Orca reports "paragraph 0
            * test/keystrokes/oocalc/bug_364086.py:      (new)
              Macaroon test file for bug #364086 – Orca reports "paragraph 0
              paragraph" <char> when you begin typing in a Calc cell.
    
    svn path=/trunk/; revision=2930

---
+++ test/keystrokes/oocalc/bug_364086.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+
+"""Test to verify bug #364086 is still fixed.
+   Orca reports "paragraph 0 paragraph" <char> when you begin typing 
+   in a Calc cell.
+"""
+
+from macaroon.playback import *
+
+sequence = MacroSequence()
+
+######################################################################
+# 1. Start oocalc.
+#
+sequence.append(WaitForWindowActivate("Untitled1 - OpenOffice.org Calc",None))
+sequence.append(WaitForFocus("Sheet Sheet1", acc_role=pyatspi.ROLE_TABLE))
+
+######################################################################
+# 2. Enter Alt-f, right arrow, down arrow and Return.
+#    (File->New->Spreadsheet).
+#
+# BRAILLE LINE:  'soffice Application Untitled2 - OpenOffice.org Calc Frame Untitled2 - OpenOffice.org Calc RootPane ScrollPane Document view3 Sheet Sheet1 Table Cell A1 '
+# VISIBLE:  'Cell A1 ', cursor=1
+# SPEECH OUTPUT: 'panel'
+# SPEECH OUTPUT: 'Sheet Sheet1 table grayed'
+# SPEECH OUTPUT: ' A1'
+#
+sequence.append(KeyComboAction("<Alt>f"))
+sequence.append(WaitForFocus("New", acc_role=pyatspi.ROLE_MENU))
+
+sequence.append(KeyComboAction("Right"))
+sequence.append(WaitForFocus("Text Document", acc_role=pyatspi.ROLE_MENU_ITEM))
+
+sequence.append(KeyComboAction("Down"))
+sequence.append(WaitForFocus("Spreadsheet", acc_role=pyatspi.ROLE_MENU_ITEM))
+
+sequence.append(KeyComboAction("Return"))
+sequence.append(WaitForFocus("Sheet Sheet1", acc_role=pyatspi.ROLE_TABLE))
+
+######################################################################
+# 3. Type "hello" (without the quotes), followed by Return.
+#
+# BRAILLE LINE:  'soffice Application Untitled2 - OpenOffice.org Calc Frame Untitled2 - OpenOffice.org Calc RootPane ScrollPane Document view3 Sheet Sheet1 Table Cell A2 '
+# VISIBLE:  'Cell A2 ', cursor=1
+# SPEECH OUTPUT: 'hello A1'
+# SPEECH OUTPUT: ' A2'
+#
+sequence.append(TypeAction("hello"))
+sequence.append(KeyComboAction("Return"))
+sequence.append(WaitForFocus("Sheet Sheet1", acc_role=pyatspi.ROLE_TABLE))
+
+######################################################################
+# 4. Enter Alt-f, Alt-c to close the Calc spreadsheet window.
+#
+sequence.append(KeyComboAction("<Alt>f"))
+sequence.append(WaitForFocus("New", acc_role=pyatspi.ROLE_MENU))
+
+sequence.append(KeyComboAction("<Alt>c"))
+sequence.append(WaitForFocus("Save", acc_role=pyatspi.ROLE_PUSH_BUTTON))
+
+######################################################################
+# 5. Enter Tab and Return to discard the current changes.
+#
+sequence.append(KeyComboAction("Tab"))
+sequence.append(WaitForFocus("Discard", acc_role=pyatspi.ROLE_PUSH_BUTTON))
+
+sequence.append(KeyComboAction("Return"))
+
+######################################################################
+# 6. Wait for things to get back to normal.
+#
+sequence.append(PauseAction(3000))
+
+sequence.start()
