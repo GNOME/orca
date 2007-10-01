cd3f4c585e782c505709c37059f31f80b0d88b79
Rich Burridge <richb@src.gnome.org>
2007-10-01 22:40:22 +0000

    src/orca/braillegenerator.py: Work on bug #465989 – Adding panel
            * src/orca/speechgenerator.py:
              src/orca/braillegenerator.py:
              Work on bug #465989 – Adding panel applets.
              Implemented the suggested fix in comment #25 of this bug.
              If there is no displayed text, check to see if this table cell
              contains an icon (image). If yes:
                1/ Try to get a description for it and speak that.
                2/ Treat the object of role type ROLE_IMAGE and speak/braille
                   the role name.
    
    svn path=/branches/gnome-2-20/; revision=2960

--- src/orca/speechgenerator.py
+++ src/orca/speechgenerator.py
@@ -1240,21 +1240,6 @@ class SpeechGenerator:
 
         utterances = []
 
-        # Check to see if this table cell contains an icon (image).
-        # If yes:
-        #   1/ Try to get a description for it and speak that. 
-        #   2/ Treat the object of role type ROLE_IMAGE and speak 
-        #      the role name.
-        # See bug #465989 for more details.
-        #
-        if obj.image:
-            if obj.image.imageDescription:
-                utterances.append(obj.image.imageDescription)
-            obj.role = rolenames.ROLE_IMAGE
-            utterances.extend(self._getSpeechForImage(obj, already_focused))
-            obj.role = rolenames.ROLE_TABLE_CELL
-            return utterances
-
         # If this table cell has 2 children and one of them has a 
         # 'toggle' action and the other does not, then present this 
         # as a checkbox where:
@@ -1308,8 +1293,23 @@ class SpeechGenerator:
                     obj.role = rolenames.ROLE_TABLE_CELL
                     break
 
-        utterances.append(self._script.getDisplayedText(\
-                          self._script.getRealActiveDescendant(obj)))
+        displayedText = self._script.getDisplayedText( \
+                          self._script.getRealActiveDescendant(obj))
+        utterances.append(displayedText)
+
+        # If there is no displayed text, check to see if this table cell
+        # contains an icon (image). If yes:
+        #   1/ Try to get a description for it and speak that.
+        #   2/ Treat the object of role type ROLE_IMAGE and speak
+        #      the role name.
+        # See bug #465989 for more details.
+        #
+        if (not displayedText or len(displayedText) == 0) and obj.image:
+            if obj.image.imageDescription:
+                utterances.append(obj.image.imageDescription)
+            obj.role = rolenames.ROLE_IMAGE
+            utterances.extend(self._getSpeechForImage(obj, already_focused))
+            obj.role = rolenames.ROLE_TABLE_CELL
 
         if obj.state.count(atspi.Accessibility.STATE_EXPANDABLE):
             if obj.state.count(atspi.Accessibility.STATE_EXPANDED):
