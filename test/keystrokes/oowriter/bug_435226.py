4f2ba4c83941a554615c6369bdd9fbec9948e3af
Rich Burridge <richb@src.gnome.org>
2007-10-09 16:55:40 +0000

    Now that bug #480278 has been fixed, the SPEECH OUTPUT line in step #8 for
            * test/keystrokes/oowriter/bug_435226.py:
              Now that bug #480278 has been fixed, the SPEECH OUTPUT line in 
              step #8 for this test has been adjusted to reflect the correct 
              output.
    
    svn path=/trunk/; revision=3001

--- test/keystrokes/oowriter/bug_435226.py
+++ test/keystrokes/oowriter/bug_435226.py
@@ -3,6 +3,10 @@
 """Test to verify bug #435226 is still fixed.
    Where-am-I doesn't correctly handle multiple selected paragraphs in 
    OOo Writer and Evolution.
+   Note that bug #480278 was opened, because the output was being smushed.
+   The SPEECH OUTPUT line in step #8 for this test has been adjusted to 
+   reflect the now correct output.
+
 """
 
 from macaroon.playback import *
@@ -77,7 +81,7 @@ sequence.append(PauseAction(3000))
 # BRAILLE LINE:  'soffice Application spanish - OpenOffice.org Writer Frame spanish - OpenOffice.org Writer RootPane ScrollPane Document view Hm! She is made of harder stuff! Cardinal Fang! Fetch the COMFY CHAIR! $l'
 # VISIBLE:  'Hm! She is made of harder stuff!', cursor=17
 # SPEECH OUTPUT: 'paragraph'
-# SPEECH OUTPUT: 'Spanish Inquisition! Our chief weapon is surprise. Surprise and fear. Fear and surprise. Our two weapons are fear and surprise. And ruthless efficiency. Our three weapons are fear, surprise, and ruthless efficiency. And an almost fanatical devotion to the Pope. Our four. No. Amongst our weapons. Amongst our weaponry, are such elements as fear, surprise. I'll come in again.NOBODY expects the Spanish Inquisition! Amongst our weaponry are such diverse elements as: fear, surprise, ruthless efficiency, an almost fanatical devotion to the Pope, and nice red uniforms - Oh damn!Now old lady, you have one last chance. Confess the heinous sin of heresy, reject the works of the ungodly. Two last chances. And you shall be free. Three last chances. You have three last chances, the nature of which I have divulged in my previous utterance.Hm! She is made '
+# SPEECH OUTPUT: 'Spanish Inquisition! Our chief weapon is surprise. Surprise and fear. Fear and surprise. Our two weapons are fear and surprise. And ruthless efficiency. Our three weapons are fear, surprise, and ruthless efficiency. And an almost fanatical devotion to the Pope. Our four. No. Amongst our weapons. Amongst our weaponry, are such elements as fear, surprise. I'll come in again. NOBODY expects the Spanish Inquisition! Amongst our weaponry are such diverse elements as: fear, surprise, ruthless efficiency, an almost fanatical devotion to the Pope, and nice red uniforms - Oh damn! Now old lady, you have one last chance. Confess the heinous sin of heresy, reject the works of the ungodly. Two last chances. And you shall be free. Three last chances. You have three last chances, the nature of which I have divulged in my previous utterance. Hm! She is made '
 # SPEECH OUTPUT: 'selected'
 #
 sequence.append(KeyComboAction("KP_Enter"))
