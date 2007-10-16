474e771a6b90f107709ae85b2cfaedb280a96855
William Walker <wwalker@src.gnome.org>
2007-10-16 00:22:51 +0000

    More work on test assertions.
    svn path=/trunk/; revision=3064

--- test/keystrokes/gtk-demo/role_window.py
+++ test/keystrokes/gtk-demo/role_window.py
@@ -4,6 +4,7 @@
 """
 
 from macaroon.playback import *
+import utils
 
 sequence = MacroSequence()
 
@@ -25,17 +26,18 @@ sequence.append(WaitForFocus(acc_role=pyatspi.ROLE_TREE_TABLE))
 
 ########################################################################
 # Once gtk-demo is running, do a "Where Am I" to get the title info via
-# KP_Insert+KP_Enter.  The following should be presented:
+# KP_Insert+KP_Enter.
 #
-# BRAILLE LINE:  'gtk-demo Application GTK+ Code Demos Frame TabList Widget (double click for demo) ScrollPane TreeTable Widget (double click for demo) ColumnHeader Application main window TREE LEVEL 1'
-#      VISIBLE:  'Application main window TREE LEV', cursor=1
-#
-# SPEECH OUTPUT: 'GTK+ Code Demos'
-#
+sequence.append(utils.StartRecordingAction())
 sequence.append(KeyPressAction(0, None, "KP_Insert"))
 sequence.append(KeyComboAction("KP_Enter"))
 sequence.append(KeyReleaseAction(0, None, "KP_Insert"))
 sequence.append(PauseAction(3000))
+sequence.append(utils.AssertPresentationAction(
+    "Window Where Am I",
+    ["BRAILLE LINE:  'gtk-demo Application GTK+ Code Demos Frame TabList Widget (double click for demo) ScrollPane TreeTable Widget (double click for demo) ColumnHeader Application main window TREE LEVEL 1'",
+     "     VISIBLE:  'Application main window TREE LEV', cursor=1",
+     "SPEECH OUTPUT: 'GTK+ Code Demos'"]))
 
 # Just a little extra wait to let some events get through.
 #
