8ba606d5bb9fd7605c1c95a5808a00aa53cb5b27
Rich Burridge <richb@src.gnome.org>
2007-11-30 17:59:36 +0000

    More work on bug #486726 - Eliminate pychecker warnings/errors.           Pylint now gives us a 10.00/10 for debug.py and espeechfactory.py.
    svn path=/trunk/; revision=3302

--- src/orca/debug.py
+++ src/orca/debug.py
@@ -28,7 +28,6 @@ __date__      = "$Date$"
 __copyright__ = "Copyright (c) 2005-2007 Sun Microsystems Inc."
 __license__   = "LGPL"
 
-import sys
 import traceback
 import pyatspi
 
@@ -144,14 +143,6 @@ def println(level, text = ""):
     - level: the accepted debug level
     - text: the text to print (default is a blank line)
     """
-
-    # Check to see if debugging is not enabled for the calling module.
-    #
-    frame = sys._getframe(1)
-    name = frame.f_globals["__name__"]
-    module = sys.modules[name]
-    if module.__dict__.has_key("_enableDebug") and not module._enableDebug:
-        return
 
     if level >= debugLevel:
         if debugFile:
