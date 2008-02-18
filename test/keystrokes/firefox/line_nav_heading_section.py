69c769ee3843e1b71ce24e8854613a236a231f74
Joanmarie Diggs <joanied@src.gnome.org>
2008-02-18 23:49:48 +0000

    New regression tests.
    svn path=/trunk/; revision=3597

---
+++ test/keystrokes/firefox/line_nav_heading_section.py
@@ -0,0 +1,207 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+
+"""Test of line navigation output of Firefox on a page with headings
+in sections.
+"""
+
+from macaroon.playback import *
+import utils
+
+sequence = MacroSequence()
+
+########################################################################
+# We wait for the focus to be on a blank Firefox window.
+#
+sequence.append(WaitForWindowActivate("Minefield",None))
+
+########################################################################
+# Load the local "simple form" test case.
+#
+sequence.append(KeyComboAction("<Control>l"))
+sequence.append(WaitForFocus("Location", acc_role=pyatspi.ROLE_ENTRY))
+
+sequence.append(TypeAction(utils.htmlURLPrefix + "heading-section.html"))
+sequence.append(KeyComboAction("Return"))
+
+sequence.append(WaitForDocLoad())
+
+sequence.append(WaitForFocus("HTML test page",
+                             acc_role=pyatspi.ROLE_DOCUMENT_FRAME))
+
+########################################################################
+# Press Control+Home to move to the top.
+#
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("<Control>Home"))
+sequence.append(utils.AssertPresentationAction(
+    "Top of file",
+    ["BRAILLE LINE:  'Heading 1. h1'",
+     "     VISIBLE:  'Heading 1. h1', cursor=1",
+     "SPEECH OUTPUT: 'Heading 1. heading  '",
+     "SPEECH OUTPUT: 'level 1'"]))
+
+########################################################################
+# Down Arrow.
+#
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("Down"))
+sequence.append(utils.AssertPresentationAction(
+    "Line Down",
+    ["BRAILLE LINE:  'Heading 2. h1'",
+     "     VISIBLE:  'Heading 2. h1', cursor=1",
+     "SPEECH OUTPUT: 'Heading 2. heading  '",
+     "SPEECH OUTPUT: 'level 1'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("Down"))
+sequence.append(utils.AssertPresentationAction(
+    "Line Down",
+    ["BRAILLE LINE:  'sect 1'",
+     "     VISIBLE:  'sect 1', cursor=1",
+     "SPEECH OUTPUT: 'sect 1'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("Down"))
+sequence.append(utils.AssertPresentationAction(
+    "Line Down",
+    ["BRAILLE LINE:  'Heading 3. h1'",
+     "     VISIBLE:  'Heading 3. h1', cursor=1",
+     "SPEECH OUTPUT: 'Heading 3. heading  '",
+     "SPEECH OUTPUT: 'level 1'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("Down"))
+sequence.append(utils.AssertPresentationAction(
+    "Line Down",
+    ["BRAILLE LINE:  'sect 2'",
+     "     VISIBLE:  'sect 2', cursor=1",
+     "SPEECH OUTPUT: 'sect 2'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("Down"))
+sequence.append(utils.AssertPresentationAction(
+    "Line Down",
+    ["BRAILLE LINE:  'Heading 4. h1'",
+     "     VISIBLE:  'Heading 4. h1', cursor=1",
+     "SPEECH OUTPUT: 'Heading 4. heading  '",
+     "SPEECH OUTPUT: 'level 1'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("Down"))
+sequence.append(utils.AssertPresentationAction(
+    "Line Down",
+    ["BRAILLE LINE:  'sect 3'",
+     "     VISIBLE:  'sect 3', cursor=1",
+     "SPEECH OUTPUT: 'sect 3'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("Down"))
+sequence.append(utils.AssertPresentationAction(
+    "Line Down",
+    ["BRAILLE LINE:  'Heading 5. h1'",
+     "     VISIBLE:  'Heading 5. h1', cursor=1",
+     "SPEECH OUTPUT: 'Heading 5. heading  '",
+     "SPEECH OUTPUT: 'level 1'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("Down"))
+sequence.append(utils.AssertPresentationAction(
+    "Line Down",
+    ["BRAILLE LINE:  'Heading 6. h1'",
+     "     VISIBLE:  'Heading 6. h1', cursor=1",
+     "SPEECH OUTPUT: 'Heading 6. heading  '",
+     "SPEECH OUTPUT: 'level 1'",]))
+
+########################################################################
+# Up Arrow.
+#
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("Up"))
+sequence.append(utils.AssertPresentationAction(
+    "Line Up",
+    ["BRAILLE LINE:  'Heading 5. h1'",
+     "     VISIBLE:  'Heading 5. h1', cursor=1",
+     "SPEECH OUTPUT: 'Heading 5. heading  '",
+     "SPEECH OUTPUT: 'level 1'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("Up"))
+sequence.append(utils.AssertPresentationAction(
+    "Line Up",
+    ["BRAILLE LINE:  'sect 3'",
+     "     VISIBLE:  'sect 3', cursor=1",
+     "SPEECH OUTPUT: 'sect 3'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("Up"))
+sequence.append(utils.AssertPresentationAction(
+    "Line Up",
+    ["BRAILLE LINE:  'Heading 4. h1'",
+     "     VISIBLE:  'Heading 4. h1', cursor=1",
+     "SPEECH OUTPUT: 'Heading 4. heading  '",
+     "SPEECH OUTPUT: 'level 1'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("Up"))
+sequence.append(utils.AssertPresentationAction(
+    "Line Up",
+    ["BRAILLE LINE:  'sect 2'",
+     "     VISIBLE:  'sect 2', cursor=1",
+     "SPEECH OUTPUT: 'sect 2'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("Up"))
+sequence.append(utils.AssertPresentationAction(
+    "Line Up",
+    ["BRAILLE LINE:  'Heading 3. h1'",
+     "     VISIBLE:  'Heading 3. h1', cursor=1",
+     "SPEECH OUTPUT: 'Heading 3. heading  '",
+     "SPEECH OUTPUT: 'level 1'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("Up"))
+sequence.append(utils.AssertPresentationAction(
+    "Line Up",
+    ["BRAILLE LINE:  'sect 1'",
+     "     VISIBLE:  'sect 1', cursor=1",
+     "SPEECH OUTPUT: 'sect 1'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("Up"))
+sequence.append(utils.AssertPresentationAction(
+    "Line Up",
+    ["BRAILLE LINE:  'Heading 2. h1'",
+     "     VISIBLE:  'Heading 2. h1', cursor=1",
+     "SPEECH OUTPUT: 'Heading 2. heading  '",
+     "SPEECH OUTPUT: 'level 1'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("Up"))
+sequence.append(utils.AssertPresentationAction(
+    "Line Up",
+    ["BRAILLE LINE:  'Heading 1. h1'",
+     "     VISIBLE:  'Heading 1. h1', cursor=1",
+     "SPEECH OUTPUT: 'Heading 1. heading  '",
+     "SPEECH OUTPUT: 'level 1'"]))
+
+########################################################################
+# Move to the location bar by pressing Control+L.  When it has focus
+# type "about:blank" and press Return to restore the browser to the
+# conditions at the test's start.
+#
+sequence.append(KeyComboAction("<Control>l"))
+sequence.append(WaitForFocus("Location", acc_role=pyatspi.ROLE_ENTRY))
+
+sequence.append(TypeAction("about:blank"))
+sequence.append(KeyComboAction("Return"))
+
+sequence.append(WaitForDocLoad())
+
+# Just a little extra wait to let some events get through.
+#
+sequence.append(PauseAction(3000))
+
+sequence.append(utils.AssertionSummaryAction())
+
+sequence.start()
---
+++ test/keystrokes/firefox/line_nav_table_cell_links.py
@@ -0,0 +1,168 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+
+"""Test of line navigation output of Firefox on a page with links
+in a table cell with line breaks.
+"""
+
+from macaroon.playback import *
+import utils
+
+sequence = MacroSequence()
+
+########################################################################
+# We wait for the focus to be on a blank Firefox window.
+#
+sequence.append(WaitForWindowActivate("Minefield",None))
+
+########################################################################
+# Load the local "simple form" test case.
+#
+sequence.append(KeyComboAction("<Control>l"))
+sequence.append(WaitForFocus("Location", acc_role=pyatspi.ROLE_ENTRY))
+
+sequence.append(TypeAction(utils.htmlURLPrefix + "table-cell-links.html"))
+sequence.append(KeyComboAction("Return"))
+
+sequence.append(WaitForDocLoad())
+
+sequence.append(WaitForFocus("Table Cell Links",
+                             acc_role=pyatspi.ROLE_DOCUMENT_FRAME))
+
+########################################################################
+# Press Control+Home to move to the top.
+#
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("<Control>Home"))
+sequence.append(utils.AssertPresentationAction(
+    "Top of file",
+    ["BRAILLE LINE:  'Here are some links'",
+     "     VISIBLE:  'Here are some links', cursor=1",
+     "SPEECH OUTPUT: 'Here are some links'"]))
+
+########################################################################
+# Down Arrow.
+#
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("Down"))
+sequence.append(utils.AssertPresentationAction(
+    "Line Down",
+    ["BRAILLE LINE:  'HTML Tags'",
+     "     VISIBLE:  'HTML Tags', cursor=1",
+     "SPEECH OUTPUT: 'HTML Tags",
+     "'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("Down"))
+sequence.append(utils.AssertPresentationAction(
+    "Line Down",
+    ["BRAILLE LINE:  '<!--> Link'",
+     "     VISIBLE:  '<!--> Link', cursor=1",
+     "SPEECH OUTPUT: '<!--> link'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("Down"))
+sequence.append(utils.AssertPresentationAction(
+    "Line Down",
+    ["BRAILLE LINE:  '<!DOCTYPE> Link'",
+     "     VISIBLE:  '<!DOCTYPE> Link', cursor=1",
+     "SPEECH OUTPUT: '<!DOCTYPE> link'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("Down"))
+sequence.append(utils.AssertPresentationAction(
+    "Line Down",
+    ["BRAILLE LINE:  '<a> Link '",
+     "     VISIBLE:  '<a> Link ', cursor=1",
+     "SPEECH OUTPUT: '<a> link ",
+     "'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("Down"))
+sequence.append(utils.AssertPresentationAction(
+    "Line Down",
+    ["BRAILLE LINE:  '<abbr> Link'",
+     "     VISIBLE:  '<abbr> Link', cursor=1",
+     "SPEECH OUTPUT: '<abbr> link'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("Down"))
+sequence.append(utils.AssertPresentationAction(
+    "Line Down",
+    ["BRAILLE LINE:  '<acronym> Link'",
+     "     VISIBLE:  '<acronym> Link', cursor=1",
+     "SPEECH OUTPUT: '<acronym> link'"]))
+
+########################################################################
+# Up Arrow.
+#
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("Up"))
+sequence.append(utils.AssertPresentationAction(
+    "Line Up",
+    ["BRAILLE LINE:  '<abbr> Link'",
+     "     VISIBLE:  '<abbr> Link', cursor=1",
+     "SPEECH OUTPUT: '<abbr> link'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("Up"))
+sequence.append(utils.AssertPresentationAction(
+    "Line Up",
+    ["BRAILLE LINE:  '<a> Link '",
+     "     VISIBLE:  '<a> Link ', cursor=1",
+     "SPEECH OUTPUT: '<a> link ",
+     "'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("Up"))
+sequence.append(utils.AssertPresentationAction(
+    "Line Up",
+    ["BRAILLE LINE:  '<!DOCTYPE> Link'",
+     "     VISIBLE:  '<!DOCTYPE> Link', cursor=1",
+     "SPEECH OUTPUT: '<!DOCTYPE> link'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("Up"))
+sequence.append(utils.AssertPresentationAction(
+    "Line Up",
+    ["BRAILLE LINE:  '<!--> Link'",
+     "     VISIBLE:  '<!--> Link', cursor=1",
+     "SPEECH OUTPUT: '<!--> link'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("Up"))
+sequence.append(utils.AssertPresentationAction(
+    "Line Up",
+    ["BRAILLE LINE:  'HTML Tags'",
+     "     VISIBLE:  'HTML Tags', cursor=1",
+     "SPEECH OUTPUT: 'HTML Tags",
+     "'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("Up"))
+sequence.append(utils.AssertPresentationAction(
+    "Line Up",
+    ["BRAILLE LINE:  'Here are some links'",
+     "     VISIBLE:  'Here are some links', cursor=1",
+     "SPEECH OUTPUT: 'Here are some links'"]))
+
+########################################################################
+# Move to the location bar by pressing Control+L.  When it has focus
+# type "about:blank" and press Return to restore the browser to the
+# conditions at the test's start.
+#
+sequence.append(KeyComboAction("<Control>l"))
+sequence.append(WaitForFocus("Location", acc_role=pyatspi.ROLE_ENTRY))
+
+sequence.append(TypeAction("about:blank"))
+sequence.append(KeyComboAction("Return"))
+
+sequence.append(WaitForDocLoad())
+
+# Just a little extra wait to let some events get through.
+#
+sequence.append(PauseAction(3000))
+
+sequence.append(utils.AssertionSummaryAction())
+
+sequence.start()
