902961c45c5519eaf7e20cc007c02be14b158be7
Willie Walker <william.walker@sun.com>
2009-05-14 18:56:32 -0400

    Treat tree tables like tables
    Without this, arrowing across a row would cause the whole row to speak.

--- src/orca/default.py
+++ src/orca/default.py
@@ -2778,7 +2778,8 @@ class Script(script.Script):
         # table-related points of reference.
         #
         if oldParent is not None and oldParent == newParent and \
-              newParent.getRole() == pyatspi.ROLE_TABLE:
+              newParent.getRole() in [pyatspi.ROLE_TABLE,
+                                      pyatspi.ROLE_TREE_TABLE]:
             for key in self.pointOfReference.keys():
                 if key not in ('lastRow', 'lastColumn'):
                     del self.pointOfReference[key]
