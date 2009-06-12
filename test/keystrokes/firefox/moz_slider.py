f729a140c40094cfb39ff73cc622cab4ad6501c5
Willie Walker <william.walker@sun.com>
2009-06-12 12:27:52 -0400

    Update regression test since Orca is speaking what is on the screen

--- test/keystrokes/firefox/moz_slider.py
+++ test/keystrokes/firefox/moz_slider.py
@@ -34,7 +34,7 @@ sequence.append(utils.AssertPresentationAction(
     "tab to slider", 
     ["BRAILLE LINE:  '10% Slider'",
      "     VISIBLE:  '10% Slider', cursor=1",
-     "SPEECH OUTPUT: 'My slider slider 10%'"]))
+     "SPEECH OUTPUT: 'slider 10%'"]))
 
 ########################################################################
 # Do a basic "Where Am I" via KP_Enter.  
