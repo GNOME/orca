2e0926662aaba0f22a53d3733df9df1c8e32f16b
Joanmarie Diggs <joanmarie.diggs@gmail.com>
2009-08-14 16:15:27 -0400

    New regression test illustrating bgo#591807 - Orca sometimes presents the previous line when Home/End is pressed in Gecko

---
+++ test/keystrokes/firefox/bug_591807.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+
+"""Test of navigation by Home/End on a blank line."""
+
+from macaroon.playback import *
+import utils
+
+sequence = MacroSequence()
+
+########################################################################
+# We wait for the focus to be on a blank Firefox window.
+#
+sequence.append(WaitForWindowActivate(utils.firefoxFrameNames, None))
+
+########################################################################
+# Load the local blockquote test case.
+#
+sequence.append(KeyComboAction("<Control>l"))
+sequence.append(WaitForFocus(acc_role=pyatspi.ROLE_ENTRY))
+
+sequence.append(TypeAction(utils.htmlURLPrefix + "bug-591807.html"))
+sequence.append(KeyComboAction("Return"))
+
+sequence.append(WaitForDocLoad())
+sequence.append(WaitForFocus("Test",
+                             acc_role=pyatspi.ROLE_DOCUMENT_FRAME))
+
+########################################################################
+# Press Control+Home to move to the top.
+#
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("<Control>Home"))
+sequence.append(utils.AssertPresentationAction(
+    "Top of file", 
+    ["BRAILLE LINE:  'This is a test.'",
+     "     VISIBLE:  'This is a test.', cursor=1",
+     "SPEECH OUTPUT: 'This is a test.",
+     "'"]))
+
+########################################################################
+# Press Down Arrow to move to the blank line.
+#
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("Down"))
+sequence.append(utils.AssertPresentationAction(
+    "Down",
+    ["BRAILLE LINE:  ''",
+     "     VISIBLE:  '', cursor=1",
+     "SPEECH OUTPUT: 'blank'"]))
+
+########################################################################
+# Press Home to move to the beginning of the blank line.
+#
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("Home"))
+sequence.append(utils.AssertPresentationAction(
+    "Home",
+    ["BUG? - We should remain on the blank line.",
+     "BRAILLE LINE:  'This is a test.'",
+     "     VISIBLE:  'This is a test.', cursor=1",
+     "SPEECH OUTPUT: 'T'"]))
+
+########################################################################
+# Press Control+Home to move to the top.
+#
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("<Control>Home"))
+sequence.append(utils.AssertPresentationAction(
+    "Top of file",
+    ["BRAILLE LINE:  'This is a test.'",
+     "     VISIBLE:  'This is a test.', cursor=1",
+     "SPEECH OUTPUT: 'This is a test.",
+     "'"]))
+
+########################################################################
+# Press Down Arrow to move to the blank line.
+#
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("Down"))
+sequence.append(utils.AssertPresentationAction(
+    "Down",
+    ["BRAILLE LINE:  ''",
+     "     VISIBLE:  '', cursor=1",
+     "SPEECH OUTPUT: 'blank'"]))
+
+########################################################################
+# Press End to move to the end of the blank line.
+#
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("End"))
+sequence.append(utils.AssertPresentationAction(
+    "End",
+    ["BUG? - We should remain on the blank line.",
+     "BRAILLE LINE:  'This is a test.'",
+     "     VISIBLE:  'This is a test.', cursor=16",
+     "SPEECH OUTPUT: 'newline'"]))
+
+########################################################################
+# Move to the location bar by pressing Control+L.  When it has focus
+# type "about:blank" and press Return to restore the browser to the
+# conditions at the test's start.
+#
+sequence.append(KeyComboAction("<Control>l"))
+sequence.append(WaitForFocus(acc_role=pyatspi.ROLE_ENTRY))
+
+sequence.append(TypeAction("about:blank"))
+sequence.append(KeyComboAction("Return"))
+
+sequence.append(WaitForDocLoad())
+
+# Just a little extra wait to let some events get through.
+#
+sequence.append(PauseAction(3000))
+
+sequence.append(utils.AssertionSummaryAction())
+
+sequence.start()
