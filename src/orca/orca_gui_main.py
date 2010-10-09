ef3057da93ae05ac7efc277e679c9a5fa19bf912
Joanmarie Diggs <joanmarie.diggs@gmail.com>
2010-10-09 19:51:41 +0200

    Upgrade from LGPLv2 to LGPLv2.1.

--- src/orca/orca_gui_main.py
+++ src/orca/orca_gui_main.py
@@ -3,16 +3,16 @@
 # Copyright 2005-2010 Sun Microsystems Inc.
 #
 # This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Library General Public
+# modify it under the terms of the GNU Lesser General Public
 # License as published by the Free Software Foundation; either
-# version 2 of the License, or (at your option) any later version.
+# version 2.1 of the License, or (at your option) any later version.
 #
 # This library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Library General Public License for more details.
+# Lesser General Public License for more details.
 #
-# You should have received a copy of the GNU Library General Public
+# You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, write to the
 # Free Software Foundation, Inc., Franklin Street, Fifth Floor,
 # Boston MA  02110-1301 USA.
@@ -114,17 +114,17 @@ class OrcaMainGUI(orca_gtkbuilder.GtkBuilderWrapper):
         #
         licenseText = \
             _("Orca is free software; you can redistribute it and/or\n" \
-              "modify it under the terms of the GNU Library General\n" \
+              "modify it under the terms of the GNU Lesser General\n" \
               "Public License as published by the Free Software Foundation;\n" \
-              "either version 2 of the License, or (at your option) any\n" \
+              "either version 2.1 of the License, or (at your option) any\n" \
               "later version.\n" \
               "\n" \
               "Orca is distributed in the hope that it will be useful, but\n" \
               "WITHOUT ANY WARRANTY; without even the implied warranty of\n" \
               "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See\n" \
-              "the GNU Library General Public License for more details.\n" \
+              "the GNU Lesser General Public License for more details.\n" \
               "\n" \
-              "You should have received a copy of the GNU Library General\n" \
+              "You should have received a copy of the GNU Lesser General\n" \
               "Public License along with Orca; if not, write to the\n" \
               "Free Software Foundation, Inc., Franklin Street, Fifth Floor," \
               "\nBoston MA  02110-1301 USA.")
