aa579a5bb6cda785882e90932bf161824a202d33
Michael Whapples <mwhapples@aim.com>
2011-01-05 17:32:26 +0000

    Fix for bug #636841 - Orca does not speak user which gets selected when the user first presses down cursor in gdm login screen

---
+++ src/orca/scripts/apps/gdm-simple-greeter/__init__.py
@@ -0,0 +1,3 @@
+"""Custom script for gdm-simple-greeter"""
+
+from script import Script
---
+++ src/orca/scripts/apps/gdm-simple-greeter/script.py
@@ -0,0 +1,14 @@
+"""Custom script for gdm-simple-greeter"""
+import orca.default as default
+
+class Script(default.Script):
+    def __init__(self, app):
+        """Creates a new script for the given application
+
+        Arguments:
+        - app: the application to create the script for
+        """
+        default.Script.__init__(self, app)
+
+    def stopSpeechOnActiveDescendantChanged(self, event):
+        return False
