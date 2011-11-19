335d9910c43a28642d51b362745bdfe7c5ccc7c3
Joanmarie Diggs <jdiggs@igalia.com>
2011-11-19 14:10:41 -0500

    Fix for bug #664386 - Orca does not always present image links in WebKitGtk content

--- src/orca/scripts/toolkits/WebKitGtk/script.py
+++ src/orca/scripts/toolkits/WebKitGtk/script.py
@@ -515,3 +515,27 @@ class Script(default.Script):
                 return
 
         text.setCaretOffset(offset)
+
+    def getTextLineAtCaret(self, obj, offset=None):
+        """Gets the line of text where the caret is.
+
+        Argument:
+        - obj: an Accessible object that implements the AccessibleText
+          interface
+        - offset: an optional caret offset to use. (Not used here at the
+          moment, but needed in the Gecko script)
+
+        Returns the [string, caretOffset, startOffset] for the line of text
+        where the caret is.
+        """
+
+        # TODO - JD: Piece together the full line for braille
+
+        textLine = default.Script.getTextLineAtCaret(self, obj, offset)
+        string = textLine[0].decode('UTF-8')
+        if string.find(self.EMBEDDED_OBJECT_CHARACTER) == -1:
+            return textLine
+
+        textLine[0] = self.utilities.displayedText(obj)
+
+        return textLine
