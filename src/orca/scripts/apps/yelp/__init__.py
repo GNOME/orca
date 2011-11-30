1e1dc3e36cc095de10f655ad9b05aa8039e263aa
Joanmarie Diggs <jdiggs@igalia.com>
2011-11-30 17:00:27 +0100

    Fix for bug 665216 - Remove Orca's support for Gecko-based Yelp v2

--- src/orca/scripts/apps/yelp/__init__.py
+++ src/orca/scripts/apps/yelp/__init__.py
@@ -17,7 +17,7 @@
 # Free Software Foundation, Inc., Franklin Street, Fifth Floor,
 # Boston MA  02110-1301 USA.
 
-""" Custom script for Yelp."""
+""" Custom script for Yelp v3."""
 
 __id__        = "$Id$"
 __version__   = "$Revision$"
@@ -25,28 +25,4 @@ __date__      = "$Date$"
 __copyright__ = "Copyright (c) 2011 The Orca Team."
 __license__   = "LGPL"
 
-import sys
-
-import pyatspi
-from orca.script_utilities import Utilities
-
-def unloadYelpScript(version):
-    if 'orca.scripts.apps.yelp.yelp_' + version in sys.modules:
-        del(sys.modules['orca.scripts.apps.yelp.yelp_' + version])
-
-
-def getScript(app):
-    """Returns the correct version of the Yelp script based on toolkit."""
-    docFrames = Utilities.descendantsWithRole(app, pyatspi.ROLE_DOCUMENT_FRAME)
-    toolkit = ""
-    if docFrames:
-        attrs = dict([a.split(':', 1) for a in docFrames[0].getAttributes()])
-        toolkit = attrs.get('toolkit', '')
-
-    if toolkit == 'WebKitGtk':
-        unloadYelpScript('v2')
-        from yelp_v3 import script
-        return script.Script(app)
-    else:
-        from yelp_v2 import script
-        return script.Script(app)
+from script import Script
