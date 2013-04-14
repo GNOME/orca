e5459b92cf71815f10f5cd2b12e5fe578b73295d
Joanmarie Diggs <jdiggs@igalia.com>
2013-04-14 15:02:34 -0400

    Fix the repeated line presentation in Thunderbird

--- src/orca/scripts/toolkits/Gecko/script_utilities.py
+++ src/orca/scripts/toolkits/Gecko/script_utilities.py
@@ -532,7 +532,7 @@ class Utilities(script_utilities.Utilities):
 
             if boundary:
                 [string, start, end] = \
-                    text.getTextAtOffset(offset + 1, boundary)
+                    text.getTextAtOffset(offset, boundary)
                 if end == -1:
                     end = text.characterCount
             else:
