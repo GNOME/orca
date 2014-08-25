2fbb388b4e0daf3ad6134f08e8ff993b38ce5734
Joanmarie Diggs <jdiggs@igalia.com>
2014-08-25 02:03:33 -0400

    New regression test

---
+++ test/keystrokes/firefox/aria_landmarks.py
@@ -0,0 +1,124 @@
+#!/usr/bin/python
+
+"""Test of structural navigation amongst landmarks."""
+
+from macaroon.playback import *
+import utils
+
+sequence = MacroSequence()
+
+sequence.append(KeyComboAction("<Control>Home"))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("m"))
+sequence.append(utils.AssertPresentationAction(
+    "1. m to next landmark",
+    ["KNOWN ISSUE: Braille should not treat these as a single line",
+     "BRAILLE LINE:  'navigation main complementary'",
+     "     VISIBLE:  'navigation main complementary', cursor=1",
+     "SPEECH OUTPUT: 'navigation'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("m"))
+sequence.append(utils.AssertPresentationAction(
+    "2. m to next landmark",
+    ["BRAILLE LINE:  'navigation main complementary'",
+     "     VISIBLE:  'navigation main complementary', cursor=12",
+     "SPEECH OUTPUT: 'main '",
+     "SPEECH OUTPUT: 'application'",
+     "SPEECH OUTPUT: 'embedded'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("m"))
+sequence.append(utils.AssertPresentationAction(
+    "3. m to next landmark",
+    ["BRAILLE LINE:  'application'",
+     "     VISIBLE:  'application', cursor=1",
+     "SPEECH OUTPUT: 'application'",
+     "SPEECH OUTPUT: 'embedded'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("m"))
+sequence.append(utils.AssertPresentationAction(
+    "4. m to next landmark",
+    ["BRAILLE LINE:  'complementary'",
+     "     VISIBLE:  'complementary', cursor=1",
+     "SPEECH OUTPUT: 'complementary '",
+     "SPEECH OUTPUT: 'form '",
+     "SPEECH OUTPUT: 'search'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("m"))
+sequence.append(utils.AssertPresentationAction(
+    "5. m to next landmark",
+    ["BRAILLE LINE:  'search'",
+     "     VISIBLE:  'search', cursor=1",
+     "SPEECH OUTPUT: 'search'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("m"))
+sequence.append(utils.AssertPresentationAction(
+    "6. m to next landmark",
+    ["BRAILLE LINE:  'contentinfo'",
+     "     VISIBLE:  'contentinfo', cursor=1",
+     "SPEECH OUTPUT: 'contentinfo'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("m"))
+sequence.append(utils.AssertPresentationAction(
+    "7. m to next landmark",
+    ["BRAILLE LINE:  'Wrapping to top.'",
+     "     VISIBLE:  'Wrapping to top.', cursor=0",
+     "BRAILLE LINE:  'banner'",
+     "     VISIBLE:  'banner', cursor=1",
+     "SPEECH OUTPUT: 'Wrapping to top.' voice=system",
+     "SPEECH OUTPUT: 'banner'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("<Shift>m"))
+sequence.append(utils.AssertPresentationAction(
+    "8. m to previous landmark",
+    ["BRAILLE LINE:  'Wrapping to bottom.'",
+     "     VISIBLE:  'Wrapping to bottom.', cursor=0",
+     "BRAILLE LINE:  'contentinfo'",
+     "     VISIBLE:  'contentinfo', cursor=1",
+     "SPEECH OUTPUT: 'Wrapping to bottom.' voice=system",
+     "SPEECH OUTPUT: 'contentinfo'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("<Shift>m"))
+sequence.append(utils.AssertPresentationAction(
+    "9. m to previous landmark",
+    ["BRAILLE LINE:  'search'",
+     "     VISIBLE:  'search', cursor=1",
+     "SPEECH OUTPUT: 'search'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("<Shift>m"))
+sequence.append(utils.AssertPresentationAction(
+    "10. m to previous landmark",
+    ["KNOWN ISSUE: We are skipping over complementary on the way back",
+     "BRAILLE LINE:  'application'",
+     "     VISIBLE:  'application', cursor=1",
+     "SPEECH OUTPUT: 'application'",
+     "SPEECH OUTPUT: 'embedded'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("<Shift>m"))
+sequence.append(utils.AssertPresentationAction(
+    "11. m to previous landmark",
+    ["KNOWN ISSUE: We are skipping over navigation on the way back",
+     "BRAILLE LINE:  'navigation main complementary'",
+     "     VISIBLE:  'navigation main complementary', cursor=1",
+     "SPEECH OUTPUT: 'navigation'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("<Shift>m"))
+sequence.append(utils.AssertPresentationAction(
+    "12. m to previous landmark",
+    ["BRAILLE LINE:  'banner'",
+     "     VISIBLE:  'banner', cursor=1",
+     "SPEECH OUTPUT: 'banner'"]))
+
+sequence.append(utils.AssertionSummaryAction())
+sequence.start()
