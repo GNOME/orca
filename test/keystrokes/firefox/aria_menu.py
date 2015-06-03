eefe1ff9305a2cd7008360b874bd0219fcdda633
Joanmarie Diggs <jdiggs@igalia.com>
2015-06-03 00:18:21 -0400

    Updated regression test

--- test/keystrokes/firefox/aria_menu.py
+++ test/keystrokes/firefox/aria_menu.py
@@ -15,9 +15,9 @@ sequence.append(utils.StartRecordingAction())
 sequence.append(KeyComboAction("<Control><Alt>m"))
 sequence.append(utils.AssertPresentationAction(
     "1. Move to the menu",
-    ["BRAILLE LINE:  'ARIA Spreadsheet and Menubar embedded Edit menu'",
-     "     VISIBLE:  'Edit menu', cursor=1",
-     "BRAILLE LINE:  'ARIA Spreadsheet and Menubar embedded Edit menu'",
+    ["BRAILLE LINE:  'table'",
+     "     VISIBLE:  'table', cursor=1",
+     "BRAILLE LINE:  'Edit menu'",
      "     VISIBLE:  'Edit menu', cursor=1",
      "SPEECH OUTPUT: 'Edit menu'"]))
 
@@ -26,18 +26,15 @@ sequence.append(KeyComboAction("KP_Enter"))
 sequence.append(PauseAction(3000))
 sequence.append(utils.AssertPresentationAction(
     "2. basic whereAmI",
-    ["BRAILLE LINE:  'ARIA Spreadsheet and Menubar embedded Edit menu'",
+    ["BRAILLE LINE:  'Edit menu'",
      "     VISIBLE:  'Edit menu', cursor=1",
-     "SPEECH OUTPUT: 'ARIA Spreadsheet and Menubar - Mozilla Firefox frame'",
      "SPEECH OUTPUT: 'Edit menu 1 of 1'"]))
 
 sequence.append(utils.StartRecordingAction())
 sequence.append(KeyComboAction("Right"))
 sequence.append(utils.AssertPresentationAction(
     "3. Move to View",
-    ["BRAILLE LINE:  'ARIA Spreadsheet and Menubar embedded View menu'",
-     "     VISIBLE:  'View menu', cursor=1",
-     "BRAILLE LINE:  'ARIA Spreadsheet and Menubar embedded View menu'",
+    ["BRAILLE LINE:  'View menu'",
      "     VISIBLE:  'View menu', cursor=1",
      "SPEECH OUTPUT: 'View menu'"]))
 
@@ -45,35 +42,23 @@ sequence.append(utils.StartRecordingAction())
 sequence.append(KeyComboAction("Down"))
 sequence.append(utils.AssertPresentationAction(
     "4. Move to Themes",
-    ["BRAILLE LINE:  'ARIA Spreadsheet and Menubar embedded menu'",
-     "     VISIBLE:  'menu', cursor=1",
-     "BRAILLE LINE:  'ARIA Spreadsheet and Menubar embedded Themes          > menu'",
+    ["BRAILLE LINE:  'Themes          > menu'",
      "     VISIBLE:  'Themes          > menu', cursor=1",
-     "BRAILLE LINE:  'ARIA Spreadsheet and Menubar embedded Themes          > menu'",
-     "     VISIBLE:  'Themes          > menu', cursor=1",
-     "SPEECH OUTPUT: 'menu'",
      "SPEECH OUTPUT: 'Themes          > menu'"]))
 
 sequence.append(utils.StartRecordingAction())
 sequence.append(KeyComboAction("Right"))
 sequence.append(utils.AssertPresentationAction(
     "5. Move to basic grey",
-    ["BRAILLE LINE:  'ARIA Spreadsheet and Menubar embedded menu'",
-     "     VISIBLE:  'menu', cursor=1",
-     "BRAILLE LINE:  'ARIA Spreadsheet and Menubar embedded Basic Grey'",
+    ["BRAILLE LINE:  'Basic Grey'",
      "     VISIBLE:  'Basic Grey', cursor=1",
-     "BRAILLE LINE:  'ARIA Spreadsheet and Menubar embedded Basic Grey'",
-     "     VISIBLE:  'Basic Grey', cursor=1",
-     "SPEECH OUTPUT: 'menu'",
      "SPEECH OUTPUT: 'Basic Grey'"]))
 
 sequence.append(utils.StartRecordingAction())
 sequence.append(KeyComboAction("Down"))
 sequence.append(utils.AssertPresentationAction(
     "6. Move to the blues",
-    ["BRAILLE LINE:  'ARIA Spreadsheet and Menubar embedded The Blues'",
-     "     VISIBLE:  'The Blues', cursor=1",
-     "BRAILLE LINE:  'ARIA Spreadsheet and Menubar embedded The Blues'",
+    ["BRAILLE LINE:  'The Blues'",
      "     VISIBLE:  'The Blues', cursor=1",
      "SPEECH OUTPUT: 'The Blues'"]))
 
@@ -81,9 +66,7 @@ sequence.append(utils.StartRecordingAction())
 sequence.append(KeyComboAction("Down"))
 sequence.append(utils.AssertPresentationAction(
     "7. Move to garden",
-    ["BRAILLE LINE:  'ARIA Spreadsheet and Menubar embedded Garden'",
-     "     VISIBLE:  'Garden', cursor=1",
-     "BRAILLE LINE:  'ARIA Spreadsheet and Menubar embedded Garden'",
+    ["BRAILLE LINE:  'Garden'",
      "     VISIBLE:  'Garden', cursor=1",
      "SPEECH OUTPUT: 'Garden'"]))
 
@@ -91,9 +74,7 @@ sequence.append(utils.StartRecordingAction())
 sequence.append(KeyComboAction("Down"))
 sequence.append(utils.AssertPresentationAction(
     "8. Move to in the pink",
-    ["BRAILLE LINE:  'ARIA Spreadsheet and Menubar embedded In the Pink grayed'",
-     "     VISIBLE:  'In the Pink grayed', cursor=1",
-     "BRAILLE LINE:  'ARIA Spreadsheet and Menubar embedded In the Pink grayed'",
+    ["BRAILLE LINE:  'In the Pink grayed'",
      "     VISIBLE:  'In the Pink grayed', cursor=1",
      "SPEECH OUTPUT: 'In the Pink grayed'"]))
 
@@ -101,9 +82,7 @@ sequence.append(utils.StartRecordingAction())
 sequence.append(KeyComboAction("Down"))
 sequence.append(utils.AssertPresentationAction(
     "9. Move to rose",
-    ["BRAILLE LINE:  'ARIA Spreadsheet and Menubar embedded Rose'",
-     "     VISIBLE:  'Rose', cursor=1",
-     "BRAILLE LINE:  'ARIA Spreadsheet and Menubar embedded Rose'",
+    ["BRAILLE LINE:  'Rose'",
      "     VISIBLE:  'Rose', cursor=1",
      "SPEECH OUTPUT: 'Rose'"]))
 
@@ -111,9 +90,7 @@ sequence.append(utils.StartRecordingAction())
 sequence.append(KeyComboAction("Left"))
 sequence.append(utils.AssertPresentationAction(
     "10. Move back to Themes",
-    ["BRAILLE LINE:  'ARIA Spreadsheet and Menubar embedded Themes          > menu'",
-     "     VISIBLE:  'Themes          > menu', cursor=1",
-     "BRAILLE LINE:  'ARIA Spreadsheet and Menubar embedded Themes          > menu'",
+    ["BRAILLE LINE:  'Themes          > menu'",
      "     VISIBLE:  'Themes          > menu', cursor=1",
      "SPEECH OUTPUT: 'Themes          > menu'"]))
 
@@ -121,9 +98,7 @@ sequence.append(utils.StartRecordingAction())
 sequence.append(KeyComboAction("Down"))
 sequence.append(utils.AssertPresentationAction(
     "11. Move to hide",
-    ["BRAILLE LINE:  'ARIA Spreadsheet and Menubar embedded Hide'",
-     "     VISIBLE:  'Hide', cursor=1",
-     "BRAILLE LINE:  'ARIA Spreadsheet and Menubar embedded Hide'",
+    ["BRAILLE LINE:  'Hide'",
      "     VISIBLE:  'Hide', cursor=1",
      "SPEECH OUTPUT: 'Hide'"]))
 
@@ -131,9 +106,7 @@ sequence.append(utils.StartRecordingAction())
 sequence.append(KeyComboAction("Down"))
 sequence.append(utils.AssertPresentationAction(
     "12. Move to show",
-    ["BRAILLE LINE:  'ARIA Spreadsheet and Menubar embedded Show'",
-     "     VISIBLE:  'Show', cursor=1",
-     "BRAILLE LINE:  'ARIA Spreadsheet and Menubar embedded Show'",
+    ["BRAILLE LINE:  'Show'",
      "     VISIBLE:  'Show', cursor=1",
      "SPEECH OUTPUT: 'Show'"]))
 
@@ -141,9 +114,7 @@ sequence.append(utils.StartRecordingAction())
 sequence.append(KeyComboAction("Down"))
 sequence.append(utils.AssertPresentationAction(
     "13. Move to more",
-    ["BRAILLE LINE:  'ARIA Spreadsheet and Menubar embedded More                > menu'",
-     "     VISIBLE:  'More                > menu', cursor=1",
-     "BRAILLE LINE:  'ARIA Spreadsheet and Menubar embedded More                > menu'",
+    ["BRAILLE LINE:  'More                > menu'",
      "     VISIBLE:  'More                > menu', cursor=1",
      "SPEECH OUTPUT: 'More                > menu'"]))
 
@@ -151,22 +122,15 @@ sequence.append(utils.StartRecordingAction())
 sequence.append(KeyComboAction("Right"))
 sequence.append(utils.AssertPresentationAction(
     "14. Move to one",
-    ["BRAILLE LINE:  'ARIA Spreadsheet and Menubar embedded menu'",
-     "     VISIBLE:  'menu', cursor=1",
-     "BRAILLE LINE:  'ARIA Spreadsheet and Menubar embedded one'",
+    ["BRAILLE LINE:  'one'",
      "     VISIBLE:  'one', cursor=1",
-     "BRAILLE LINE:  'ARIA Spreadsheet and Menubar embedded one'",
-     "     VISIBLE:  'one', cursor=1",
-     "SPEECH OUTPUT: 'menu'",
      "SPEECH OUTPUT: 'one'"]))
 
 sequence.append(utils.StartRecordingAction())
 sequence.append(KeyComboAction("Down"))
 sequence.append(utils.AssertPresentationAction(
     "15. Move to two",
-    ["BRAILLE LINE:  'ARIA Spreadsheet and Menubar embedded two'",
-     "     VISIBLE:  'two', cursor=1",
-     "BRAILLE LINE:  'ARIA Spreadsheet and Menubar embedded two'",
+    ["BRAILLE LINE:  'two'",
      "     VISIBLE:  'two', cursor=1",
      "SPEECH OUTPUT: 'two'"]))
 
