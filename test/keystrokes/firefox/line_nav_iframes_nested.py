d469dbbb146ba02a54a31b56dfb8dc27ba721f69
Joanmarie Diggs <jdiggs@igalia.com>
2015-09-03 16:33:09 -0400

    New test case

---
+++ test/keystrokes/firefox/line_nav_iframes_nested.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+"""Test of line navigation output of Firefox."""
+
+from macaroon.playback import *
+import utils
+
+sequence = MacroSequence()
+
+# Work around some new quirk in Gecko that causes this test to fail if
+# run via the test harness rather than manually.
+sequence.append(KeyComboAction("<Control>r"))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("<Control>Home"))
+sequence.append(utils.AssertPresentationAction(
+    "1. Top of file",
+    ["BRAILLE LINE:  'Click here to publicly \+1 this. push button internal frame'",
+     "     VISIBLE:  'Click here to publicly \+1 this. ', cursor=1",
+     "BRAILLE LINE:  'Click here to publicly \+1 this. push button [0-9]+'",
+     "     VISIBLE:  'Click here to publicly \+1 this. ', cursor=1",
+     "SPEECH OUTPUT: 'Click here to publicly \+1 this. push button'",
+     "SPEECH OUTPUT: '[0-9]+'",
+     "SPEECH OUTPUT: 'internal frame'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("Down"))
+sequence.append(utils.AssertPresentationAction(
+    "2. Line Down",
+    ["BRAILLE LINE:  'After the iframe'",
+     "     VISIBLE:  'After the iframe', cursor=1",
+     "SPEECH OUTPUT: 'After the iframe'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("<Control>Home"))
+sequence.append(utils.AssertPresentationAction(
+    "3. Top of file",
+    ["BRAILLE LINE:  'Click here to publicly \+1 this. push button [0-9]+ internal frame'",
+     "     VISIBLE:  'Click here to publicly \+1 this. ', cursor=1",
+     "SPEECH OUTPUT: 'Click here to publicly \+1 this. push button'",
+     "SPEECH OUTPUT: '[0-9]+'",
+     "SPEECH OUTPUT: 'internal frame'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("Down"))
+sequence.append(utils.AssertPresentationAction(
+    "4. Line Down",
+    ["KNOWN ISSUE: We are stuck on the internal iframe",
+     "BRAILLE LINE:  'Click here to publicly \+1 this. push button [0-9]+ internal frame'",
+     "     VISIBLE:  'Click here to publicly \+1 this. ', cursor=1",
+     "SPEECH OUTPUT: 'Click here to publicly \+1 this. push button'",
+     "SPEECH OUTPUT: '[0-9]+'",
+     "SPEECH OUTPUT: 'internal frame'"]))
+
+sequence.append(utils.AssertionSummaryAction())
+sequence.start()
