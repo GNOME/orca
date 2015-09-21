c746c42dba2f321d93c20b2a642549a2175546cf
Joanmarie Diggs <jdiggs@igalia.com>
2015-09-21 16:58:57 -0400

    Don't treat objects with ROLE_LABEL as an ancestor

--- src/orca/scripts/web/speech_generator.py
+++ src/orca/scripts/web/speech_generator.py
@@ -65,6 +65,7 @@ class SpeechGenerator(speech_generator.SpeechGenerator):
                                pyatspi.ROLE_MENU_BAR,
                                pyatspi.ROLE_TOOL_BAR]
         args['skipRoles'] = [pyatspi.ROLE_PARAGRAPH,
+                             pyatspi.ROLE_LABEL,
                              pyatspi.ROLE_LIST_ITEM,
                              pyatspi.ROLE_TEXT]
 
