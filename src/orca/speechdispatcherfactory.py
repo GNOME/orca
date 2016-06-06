GNOME/orca

commit 4934a5bd582c5872f8b955f232368b2ec353aae1

More efficient iteration.
There is no need to do for var in list(iterable), for var in iterable
is more efficient.

src/orca/speechdispatcherfactory.py
@@ -110,7 +110,7 @@ class SpeechServer(speechserver.SpeechServer):
     getSpeechServer = staticmethod(getSpeechServer)
 
     def shutdownActiveServers():
-        for server in list(SpeechServer._active_servers.values()):
+        for server in SpeechServer._active_servers.values():
             server.shutdown()
     shutdownActiveServers = staticmethod(shutdownActiveServers)
 

src/orca/acss.py
@@ -73,7 +73,7 @@ class ACSS(dict):
                     #
                     if k == ACSS.FAMILY:
                         self[k] = {}
-                        for j in list(props[k].keys()):
+                        for j in props[k].keys():
                             self[k][j] = props[k][j]
                     else:
                         self[k] = props[k]

src/orca/backends/json_backend.py
@@ -124,9 +124,9 @@ class Backend:
         self._getSettings()
         generalSettings = self.general.copy()
         profileSettings = self.profiles[profile].copy()
-        for key, value in list(profileSettings.items()):
+        for key, value in profileSettings.items():
             if key == 'voices':
-                for voiceType, voiceDef in list(value.items()):
+                for voiceType, voiceDef in value.items():
                     value[voiceType] = acss.ACSS(voiceDef)
             if key not in ['startingProfile', 'activeProfile']:
                 generalSettings[key] = value
@@ -183,7 +183,7 @@ class Backend:
         self._getSettings()
         profiles = []
 
-        for profileName in list(self.profiles.keys()):
+        for profileName in self.profiles.keys():
             profileDict = self.profiles[profileName].copy()
             profiles.append(profileDict.get('profile'))
 

src/orca/event_manager.py
@@ -80,7 +80,7 @@ class EventManager:
 
         debug.println(debug.LEVEL_INFO, 'EVENT MANAGER: Dectivating', True)
         self._active = False
-        for eventType in list(self._scriptListenerCounts.keys()):
+        for eventType in self._scriptListenerCounts.keys():
             self.registry.deregisterEventListener(self._enqueue, eventType)
         self._scriptListenerCounts = {}
         self.deregisterKeystrokeListener(self._processKeyboardEvent)
@@ -422,7 +422,7 @@ class EventManager:
         msg = 'EVENT MANAGER: registering listeners for: %s' % script
         debug.println(debug.LEVEL_INFO, msg, True)
 
-        for eventType in list(script.listeners.keys()):
+        for eventType in script.listeners.keys():
             self._registerListener(eventType)
 
     def deregisterScriptListeners(self, script):
@@ -436,19 +436,19 @@ class EventManager:
         msg = 'EVENT MANAGER: deregistering listeners for: %s' % script
         debug.println(debug.LEVEL_INFO, msg, True)
 
-        for eventType in list(script.listeners.keys()):
+        for eventType in script.listeners.keys():
             self._deregisterListener(eventType)
 
     def registerModuleListeners(self, listeners):
         """Register the listeners on behalf of the caller."""
 
-        for eventType, function in list(listeners.items()):
+        for eventType, function in listeners.items():
             self.registry.registerEventListener(function, eventType)
 
     def deregisterModuleListeners(self, listeners):
         """Deegister the listeners on behalf of the caller."""
 
-        for eventType, function in list(listeners.items()):
+        for eventType, function in listeners.items():
             self.registry.deregisterEventListener(function, eventType)
 
     def registerKeystrokeListener(self, function, mask=None, kind=None):

src/orca/formatting.py
@@ -854,7 +854,7 @@ class Formatting(dict):
         self.update(copy.deepcopy(formatting))
 
     def update(self, newDict):
-        for key, val in list(newDict.items()):
+        for key, val in newDict.items():
             if key in self:
                 if isinstance(self[key], dict) and isinstance(val, dict):
                     self[key].update(val)

src/orca/generator.py
@@ -96,7 +96,7 @@ class Generator:
         # for the results of all the legal method names.
         #
         globalsDict = {}
-        for key in list(self._methodsDict.keys()):
+        for key in self._methodsDict.keys():
             globalsDict[key] = []
         self._addGlobals(globalsDict)
 

src/orca/liveregions.py
@@ -175,7 +175,7 @@ class LiveRegionManager:
         # objects that are not registered for this page
         newpoliteness = {}
         currenturi = self._script.bookmarks.getURIKey()
-        for key, value in list(self._politenessOverrides.items()):
+        for key, value in self._politenessOverrides.items():
             if key[0] == currenturi or value != LIVE_NONE:
                 newpoliteness[key] = value
         self._politenessOverrides = newpoliteness
@@ -340,7 +340,7 @@ class LiveRegionManager:
             self._restoreOverrides = copy.copy(self._politenessOverrides)
 
             # Set all politeness overrides to LIVE_OFF.
-            for override in list(self._politenessOverrides.keys()):
+            for override in self._politenessOverrides.keys():
                 self._politenessOverrides[override] = LIVE_OFF
 
             # look through all the objects on the page and set/add to
@@ -356,7 +356,7 @@ class LiveRegionManager:
 
         # The user wants to restore politeness levels
         else:
-            for key, value in list(self._restoreOverrides.items()):
+            for key, value in self._restoreOverrides.items():
                 self._politenessOverrides[key] = value
             self._script.presentMessage(messages.LIVE_REGIONS_ALL_RESTORED)
             # Toggle our flag

src/orca/orca_gui_prefs.py
@@ -2022,7 +2022,7 @@ class OrcaSetupGUI(orca_gtkbuilder.GtkBuilderWrapper):
         self._markModified()
         iterBB = self._createNode(guilabels.KB_GROUP_BRAILLE)
         self.bbindings = self.script.getBrailleBindings()
-        for com, inputEvHand in list(self.bbindings.items()):
+        for com, inputEvHand in self.bbindings.items():
             handl = self.script.getInputEventHandlerKey(inputEvHand)
             self._insertRowBraille(handl, com, inputEvHand, iterBB)
 

commit d043a288e8d6462c3435132e72f9b4f3e9881609

Fix regression introduced by 4934a5bd58
Sometimes there actually are reasons to call list() on an iterable, such
as when the iterable is a dictionary being modified during iteration.

src/orca/speechdispatcherfactory.py
@@ -108,7 +108,8 @@ class SpeechServer(speechserver.SpeechServer):
 
     @staticmethod
     def shutdownActiveServers():
-        for server in SpeechServer._active_servers.values():
+        servers = [s for s in SpeechServer._active_servers.values()]
+        for server in servers:
             server.shutdown()
 
     # *** Instance methods ***