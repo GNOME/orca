240aa4c78a697e8cee3fed2bee6fa03f3c0f9297
Joanmarie Diggs <jdiggs@igalia.com>
2018-07-05 13:45:23 -0400

    More work on what we treat as whole objects for mouse review

--- src/orca/mouse_review.py
+++ src/orca/mouse_review.py
@@ -136,10 +136,14 @@ class _ItemContext:
         if "Text" not in interfaces:
             return True
 
-        if self._obj.getRole() == pyatspi.ROLE_LABEL:
+        roles = [pyatspi.ROLE_ENTRY,
+                 pyatspi.ROLE_LABEL,
+                 pyatspi.ROLE_PASSWORD_TEXT]
+
+        if self._obj.getRole() in roles:
             return True
 
-        if self._obj.name:
+        if self._obj.name and not "EditableText" in pyatspi.listInterfaces(self._obj):
             return True
 
         return False
