496ea5167971cb8b2a3f0af6c5f4353181d965ef
Samuel Thibault <samuel.thibault@ens-lyon.org>
2018-08-06 14:38:49 +0200

    keyboard: add support for ShiftLock
    For people using the xkb caps:shiftlock option
    
    Fixes #10

--- src/orca/orca_gui_prefs.py
+++ src/orca/orca_gui_prefs.py
@@ -2642,7 +2642,7 @@ class OrcaSetupGUI(orca_gtkbuilder.GtkBuilderWrapper):
 
         modifierKeys =  ['Alt_L', 'Alt_R', 'Control_L', 'Control_R',
                          'Shift_L', 'Shift_R', 'Meta_L', 'Meta_R',
-                         'Num_Lock', 'Caps_Lock']
+                         'Num_Lock', 'Caps_Lock', 'Shift_Lock']
         if eventString in modifierKeys:
             return False
 
@@ -2743,6 +2743,13 @@ class OrcaSetupGUI(orca_gtkbuilder.GtkBuilderWrapper):
         if mods & (1 << pyatspi.MODIFIER_SHIFTLOCK) \
            and mods & keybindings.ORCA_MODIFIER_MASK:
             mods ^= (1 << pyatspi.MODIFIER_SHIFTLOCK)
+        try:
+            shiftmask = (1 << pyatspi.MODIFIER_SHIFT)
+        except:
+            shiftmask = 0
+        if mods & shiftmask \
+           and mods & keybindings.ORCA_MODIFIER_MASK:
+            mods ^= (1 << pyatspi.MODIFIER_SHIFT)
 
         treeModel.set(myiter,
                       modMask, str(keybindings.defaultModifierMask),
