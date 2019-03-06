4da4df347fb7b6c3e0c4478294e05252304b0aa9
Joanmarie Diggs <jdiggs@igalia.com>
2019-03-06 17:24:52 +0100

    Remove unused imports

--- src/orca/scripts/apps/metacity/script.py
+++ src/orca/scripts/apps/metacity/script.py
@@ -25,7 +25,6 @@ __date__      = "$Date$"
 __copyright__ = "Copyright (c) 2005-2008 Sun Microsystems Inc."
 __license__   = "LGPL"
 
-import orca.messages as messages
 import orca.scripts.default as default
 import pyatspi
 
