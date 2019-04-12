b7c018c9c37c8ea7fc6c507e619a1a9cf95ef6e6
Joanmarie Diggs <jdiggs@igalia.com>
2019-04-12 18:28:09 -0400

    Remove a redundant call to chnames.getCharacterName()

--- src/orca/scripts/default.py
+++ src/orca/scripts/default.py
@@ -34,7 +34,6 @@ import time
 
 import pyatspi
 import orca.braille as braille
-import orca.chnames as chnames
 import orca.cmdnames as cmdnames
 import orca.debug as debug
 import orca.eventsynthesizer as eventsynthesizer
@@ -4237,8 +4236,7 @@ class Script(script.Script):
         method rather than calling speech.speakCharacter directly."""
 
         voice = self.speechGenerator.voice(string=character)
-        spokenCharacter = chnames.getCharacterName(character)
-        speech.speakCharacter(spokenCharacter, voice)
+        speech.speakCharacter(character, voice)
 
     def speakMessage(self, string, voice=None, interrupt=True, resetStyles=True):
         """Method to speak a single string. Scripts should use this
