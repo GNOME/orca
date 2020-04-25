7bcbd4400454978d8016ea8f1fca47ba4363785e
Joanmarie Diggs <jdiggs@igalia.com>
2020-04-25 15:26:09 -0400

    Revert "Braille: Ensure focused region starts the display when panToCursor"
    This reverts commit 3e934f44655b88783b5fba6f0185410f6786faa8.
    
    There seems to be a side effect that when contracted braille is being used,
    repositionCursor is messing things up causing words in editable content to
    no longer be displayed. Until I get to the bottom of that problem, reverting
    this change.

--- src/orca/braille.py
+++ src/orca/braille.py
@@ -1180,7 +1180,10 @@ def refresh(panToCursor=True,
     # right of the display if we need to pan right.
     #
     if panToCursor and (cursorOffset >= 0):
-        if targetCursorCell:
+        if len(string) <= _displaySize[0] \
+           and cursorOffset < _displaySize[0]:
+            viewport[0] = 0
+        elif targetCursorCell:
             viewport[0] = max(0, cursorOffset - targetCursorCell + 1)
         elif cursorOffset < viewport[0]:
             viewport[0] = max(0, cursorOffset)
