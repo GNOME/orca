2a3b8c6a361b9fdd996649c534bd8243d708fc49
Joanmarie Diggs <jdiggs@igalia.com>
2021-02-01 16:28:49 +0100

    Don't bother resetting capitalization style for the shutdown message
    Trying to shave time off for shutdown.

--- src/orca/orca.py
+++ src/orca/orca.py
@@ -682,7 +682,7 @@ def shutdown(script=None, inputEvent=None):
         signal.signal(signal.SIGALRM, settings.timeoutCallback)
         signal.alarm(settings.timeoutTime)
 
-    orca_state.activeScript.presentMessage(messages.STOP_ORCA)
+    orca_state.activeScript.presentMessage(messages.STOP_ORCA, resetStyles=False)
 
     _scriptManager.deactivate()
     _eventManager.deactivate()
