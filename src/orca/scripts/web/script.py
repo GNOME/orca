09d721e3d9af930760d4de0b87de96e5e38be249
Joanmarie Diggs <jdiggs@igalia.com>
2021-02-26 16:00:00 +0100

    Web: Don't generate line contents when tabbing into editable with descendants
    Certain chat web apps have an editable text object with descendant
    elements. If one gives it focus with tab, we should just present the
    entry rather than the full bar.

--- src/orca/scripts/web/script.py
+++ src/orca/scripts/web/script.py
@@ -1299,6 +1299,7 @@ class Script(default.Script):
             contents = self.utilities.getLineContentsAtOffset(newFocus, caretOffset)
             utterances = self.speechGenerator.generateContents(contents, priorObj=oldFocus)
         elif self.utilities.isContentEditableWithEmbeddedObjects(newFocus) \
+           and not self.utilities.lastInputEventWasTab() \
            and not (newFocus.getRole() == pyatspi.ROLE_TABLE_CELL and newFocus.name):
             msg = "WEB: New focus %s content editable. Generating line contents." % newFocus
             debug.println(debug.LEVEL_INFO, msg, True)
