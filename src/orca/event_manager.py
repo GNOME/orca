GNOME/orca

commit d261ce21e756ec5dec7546925a649006f7cf8b1e

Treat state-changed:active true on frames as reason to change script
The event manager was treating window:activate as a reason to switch
the active script. But it was not doing so for state-changed:active
on a frame. As a result, if an application emits the latter before
the former -- or fails to emit the former at all -- script-specific
logic might not kick in for the activation.

This event ordering was causing us to switch to browse mode incorrectly
in VSCode. Treating state-changed:active true on frames as a reason to
change the active script solves this problem.

src/orca/event_manager.py
@@ -693,6 +693,10 @@ class EventManager:
         if eType.startswith('window:activate'):
             return True, "window:activate event"
 
+        if eType.startswith('object:state-changed:active') and event.detail1 \
+           and role == pyatspi.ROLE_FRAME:
+            return True, "Window is becoming active."
+
         if eType.startswith('focus') \
            or (eType.startswith('object:state-changed:focused')
                and event.detail1):

commit 04be36dedac21fe7d8a058bfc8fd9b1e69f2bd60

Event Manager: Don't switch scripts for activate on already-active window
If we receive a window:activate and/or state-changed:active event on a
window after we've received a focus event (or figured out focus on our
own), we don't want to switch scripts. While in most cases doing so has
no impact, there are times when the script for the window is not the
same as the script for the focused object. This is true for apps which
embed WebKitGtk and terminal applications.

Therefore, only switch applications when we get window activation for
a window which is not already the active window. This solves a recent
regression introduced by d261ce21e and should also solve a similar
problem which users report has been around a while.

src/orca/event_manager.py
@@ -704,12 +704,18 @@ class EventManager:
             return True, "The script insists it should be activated for this event."
 
         eType = event.type
-        if eType.startswith('window:activate'):
-            return True, "window:activate event"
 
-        if eType.startswith('object:state-changed:active') and event.detail1 \
-           and role == pyatspi.ROLE_FRAME:
-            return True, "Window is becoming active."
+        if eType.startswith('window:activate'):
+            windowActivation = True
+        else:
+            windowActivation = eType.startswith('object:state-changed:active') \
+                and event.detail1 and role == pyatspi.ROLE_FRAME
+
+        if windowActivation:
+            if event.source != orca_state.activeWindow:
+                return True, "Window activation"
+            else:
+                return False, "Window activation for already-active window"
 
         if eType.startswith('focus') \
            or (eType.startswith('object:state-changed:focused')

src/orca/scripts/terminal/script.py
@@ -82,6 +82,8 @@ class Script(default.Script):
         """Callback for object:text-changed:insert accessibility events."""
 
         if not self.utilities.treatEventAsCommand(event):
+            msg = "TERMINAL: Passing along event to default script."
+            debug.println(debug.LEVEL_INFO, msg, True)
             super().onTextInserted(event)
             return
 