4cff4f85bafe74a3c16f699e49722f9cd274b09a
Kalev Lember <klember@redhat.com>
2021-09-10 10:12:23 +0200

    Fix compatibility with Python 3.10
    Python 3.10 removed aliases to Collections Abstract Base Classes that
    were deprecated in Python 3.3.
    
    Fix this by just using collections.abc directly without using the alias.
    
    https://docs.python.org/3.10/whatsnew/changelog.html#python-3-10-0-alpha-5

--- src/orca/generator.py
+++ src/orca/generator.py
@@ -78,7 +78,7 @@ class Generator:
         self._activeProgressBars = {}
         self._methodsDict = {}
         for method in \
-            [z for z in [getattr(self, y).__get__(self, self.__class__) for y in [x for x in dir(self) if x.startswith(METHOD_PREFIX)]] if isinstance(z, collections.Callable)]:
+            [z for z in [getattr(self, y).__get__(self, self.__class__) for y in [x for x in dir(self) if x.startswith(METHOD_PREFIX)]] if isinstance(z, collections.abc.Callable)]:
             name = method.__name__[len(METHOD_PREFIX):]
             name = name[0].lower() + name[1:]
             self._methodsDict[name] = method
