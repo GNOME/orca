61d02d0def69fec7ad83636c40aa4c9128f28d85
Joanmarie Diggs <jdiggs@igalia.com>
2022-02-16 15:19:20 +0100

    Fix nit in translator comments

--- src/orca/brltablenames.py
+++ src/orca/brltablenames.py
@@ -31,130 +31,130 @@ __license__   = "LGPL"
 
 from .orca_i18n import _
 
-# Translators: These is the name of a braille translation table. To learn more
+# Translators: This is the name of a braille translation table. To learn more
 # about braille translation tables, see http://en.wikipedia.org/wiki/Braille.
 CZ_CZ_G1   = _("Czech Grade 1")
 
-# Translators: These is the name of a braille translation table. To learn more
+# Translators: This is the name of a braille translation table. To learn more
 # about braille translation tables, see http://en.wikipedia.org/wiki/Braille.
 ES_ES_G1   = _("Spanish Grade 1")
 
-# Translators: These is the name of a braille translation table. To learn more
+# Translators: This is the name of a braille translation table. To learn more
 # about braille translation tables, see http://en.wikipedia.org/wiki/Braille.
 FR_CA_G2   = _("Canada French Grade 2")
 
-# Translators: These is the name of a braille translation table. To learn more
+# Translators: This is the name of a braille translation table. To learn more
 # about braille translation tables, see http://en.wikipedia.org/wiki/Braille.
 FR_FR_G2   = _("France French Grade 2")
 
-# Translators: These is the name of a braille translation table. To learn more
+# Translators: This is the name of a braille translation table. To learn more
 # about braille translation tables, see http://en.wikipedia.org/wiki/Braille.
 LV_LV_G1   = _("Latvian Grade 1")
 
-# Translators: These is the name of a braille translation table. To learn more
+# Translators: This is the name of a braille translation table. To learn more
 # about braille translation tables, see http://en.wikipedia.org/wiki/Braille.
 NL_NL_G1   = _("Netherlands Dutch Grade 1")
 
-# Translators: These is the name of a braille translation table. To learn more
+# Translators: This is the name of a braille translation table. To learn more
 # about braille translation tables, see http://en.wikipedia.org/wiki/Braille.
 NO_NO_G0   = _("Norwegian Grade 0")
 
-# Translators: These is the name of a braille translation table. To learn more
+# Translators: This is the name of a braille translation table. To learn more
 # about braille translation tables, see http://en.wikipedia.org/wiki/Braille.
 NO_NO_G1   = _("Norwegian Grade 1")
 
-# Translators: These is the name of a braille translation table. To learn more
+# Translators: This is the name of a braille translation table. To learn more
 # about braille translation tables, see http://en.wikipedia.org/wiki/Braille.
 NO_NO_G2   = _("Norwegian Grade 2")
 
-# Translators: These is the name of a braille translation table. To learn more
+# Translators: This is the name of a braille translation table. To learn more
 # about braille translation tables, see http://en.wikipedia.org/wiki/Braille.
 NO_NO_G3   = _("Norwegian Grade 3")
 
-# Translators: These is the name of a braille translation table. To learn more
+# Translators: This is the name of a braille translation table. To learn more
 # about braille translation tables, see http://en.wikipedia.org/wiki/Braille.
 PL_PL_G1   = _("Polish Grade 1")
 
-# Translators: These is the name of a braille translation table. To learn more
+# Translators: This is the name of a braille translation table. To learn more
 # about braille translation tables, see http://en.wikipedia.org/wiki/Braille.
 PT_PT_G1   = _("Portuguese Grade 1")
 
-# Translators: These is the name of a braille translation table. To learn more
+# Translators: This is the name of a braille translation table. To learn more
 # about braille translation tables, see http://en.wikipedia.org/wiki/Braille.
 SE_SE_G1   = _("Swedish Grade 1")
 
-# Translators: These is the name of a braille translation table. To learn more
+# Translators: This is the name of a braille translation table. To learn more
 # about braille translation tables, see http://en.wikipedia.org/wiki/Braille.
 AR_AR_G1   = _("Arabic Grade 1")
 
-# Translators: These is the name of a braille translation table. To learn more
+# Translators: This is the name of a braille translation table. To learn more
 # about braille translation tables, see http://en.wikipedia.org/wiki/Braille.
 CY_CY_G1   = _("Welsh Grade 1")
 
-# Translators: These is the name of a braille translation table. To learn more
+# Translators: This is the name of a braille translation table. To learn more
 # about braille translation tables, see http://en.wikipedia.org/wiki/Braille.
 CY_CY_G2   = _("Welsh Grade 2")
 
-# Translators: These is the name of a braille translation table. To learn more
+# Translators: This is the name of a braille translation table. To learn more
 # about braille translation tables, see http://en.wikipedia.org/wiki/Braille.
 DE_DE_G0   = _("German Grade 0")
 
-# Translators: These is the name of a braille translation table. To learn more
+# Translators: This is the name of a braille translation table. To learn more
 # about braille translation tables, see http://en.wikipedia.org/wiki/Braille.
 DE_DE_G1   = _("German Grade 1")
 
-# Translators: These is the name of a braille translation table. To learn more
+# Translators: This is the name of a braille translation table. To learn more
 # about braille translation tables, see http://en.wikipedia.org/wiki/Braille.
 DE_DE_G2   = _("German Grade 2")
 
-# Translators: These is the name of a braille translation table. To learn more
+# Translators: This is the name of a braille translation table. To learn more
 # about braille translation tables, see http://en.wikipedia.org/wiki/Braille.
 EN_GB_G2   = _("U.K. English Grade 2")
 
-# Translators: These is the name of a braille translation table. To learn more
+# Translators: This is the name of a braille translation table. To learn more
 # about braille translation tables, see http://en.wikipedia.org/wiki/Braille.
 EN_GB_G1   = _("U.K. English Grade 1")
 
-# Translators: These is the name of a braille translation table. To learn more
+# Translators: This is the name of a braille translation table. To learn more
 # about braille translation tables, see http://en.wikipedia.org/wiki/Braille.
 EN_US_G1   = _("U.S. English Grade 1")
 
-# Translators: These is the name of a braille translation table. To learn more
+# Translators: This is the name of a braille translation table. To learn more
 # about braille translation tables, see http://en.wikipedia.org/wiki/Braille.
 EN_US_G2   = _("U.S. English Grade 2")
 
-# Translators: These is the name of a braille translation table. To learn more
+# Translators: This is the name of a braille translation table. To learn more
 # about braille translation tables, see http://en.wikipedia.org/wiki/Braille.
 FR_CA_G1   = _("Canada French Grade 1")
 
-# Translators: These is the name of a braille translation table. To learn more
+# Translators: This is the name of a braille translation table. To learn more
 # about braille translation tables, see http://en.wikipedia.org/wiki/Braille.
 FR_FR_G1   = _("France French Grade 1")
 
-# Translators: These is the name of a braille translation table. To learn more
+# Translators: This is the name of a braille translation table. To learn more
 # about braille translation tables, see http://en.wikipedia.org/wiki/Braille.
 GR_GR_G1   = _("Greek Grade 1")
 
-# Translators: These is the name of a braille translation table. To learn more
+# Translators: This is the name of a braille translation table. To learn more
 # about braille translation tables, see http://en.wikipedia.org/wiki/Braille.
 HI_IN_G1   = _("Hindi Grade 1")
 
-# Translators: These is the name of a braille translation table. To learn more
+# Translators: This is the name of a braille translation table. To learn more
 # about braille translation tables, see http://en.wikipedia.org/wiki/Braille.
 HU_HU_8DOT = _("Hungarian 8 dot computer")
 
-# Translators: These is the name of a braille translation table. To learn more
+# Translators: This is the name of a braille translation table. To learn more
 # about braille translation tables, see http://en.wikipedia.org/wiki/Braille.
 HU_HU_G1   = _("Hungarian Grade 1")
 
-# Translators: These is the name of a braille translation table. To learn more
+# Translators: This is the name of a braille translation table. To learn more
 # about braille translation tables, see http://en.wikipedia.org/wiki/Braille.
 HU_HU_G2   = _("Hungarian Grade 2")
 
-# Translators: These is the name of a braille translation table. To learn more
+# Translators: This is the name of a braille translation table. To learn more
 # about braille translation tables, see http://en.wikipedia.org/wiki/Braille.
 IT_IT_G1   = _("Italian Grade 1")
 
-# Translators: These is the name of a braille translation table. To learn more
+# Translators: This is the name of a braille translation table. To learn more
 # about braille translation tables, see http://en.wikipedia.org/wiki/Braille.
 NL_BE_G1   = _("Belgium Dutch Grade 1")
