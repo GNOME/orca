31ef1f7532c74d613fbfffae1697980d2c62856d
Mike Gorse <mgorse@suse.com>
2023-05-23 11:48:41 -0500

    Filter duplicate events when checking for double presses of the orca modifier

--- src/orca/input_event.py
+++ src/orca/input_event.py
@@ -328,7 +328,7 @@ class KeyboardEvent(InputEvent):
         elif self.isModifierKey():
             self.keyType = KeyboardEvent.TYPE_MODIFIER
             self.shouldEcho = _mayEcho and settings.enableModifierKeys
-            if self.isOrcaModifier():
+            if self.isOrcaModifier() and not self.is_duplicate:
                 now = time.time()
                 if KeyboardEvent.lastOrcaModifierAlone:
                     if _isPressed:
