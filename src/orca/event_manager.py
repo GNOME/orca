a4bb2de72d09e0969fc63db1dd20e8abc40b4775
Joanmarie Diggs <jdiggs@igalia.com>
2023-07-30 15:32:19 +0200

    Event manager: Suspend more events, and do so for "busy" events
    * Add the following to suspendable events:
      - accessible name change
      - sensitive state change
      - showing state change
      - text change insert and delete
    * Add busy state change to the suspension triggers (before it was
      just events related to window activation/deactivation)
    
    This should hopefully improve performance with spammy browsers.

--- src/orca/event_manager.py
+++ src/orca/event_manager.py
@@ -65,7 +65,12 @@ class EventManager:
         # Note: These must match what the scripts registered for, otherwise
         # Atspi might segfault.
         self._suspendableEvents = ['object:children-changed:add',
-                                   'object:children-changed:remove']
+                                   'object:children-changed:remove',
+                                   'object:property-change:accessible-name',
+                                   'object:state-changed:sensitive',
+                                   'object:state-changed:showing',
+                                   'object:text-changed:insert',
+                                   'object:text-changed:delete']
         self._eventsTriggeringSuspension = []
         self._ignoredEvents = ['object:bounds-changed',
                                'object:state-changed:defunct',
@@ -136,6 +141,10 @@ class EventManager:
         app = AXObject.get_application(event.source)
 
         debug.println(debug.LEVEL_INFO, '')
+        if self._eventsSuspended:
+            msg = 'EVENT MANAGER: Suspended events: %s' % ", ".join(self._suspendableEvents)
+            debug.println(debug.LEVEL_INFO, msg, True)
+
         msg = 'EVENT MANAGER: %s for %s in %s (%s, %s, %s)' % \
               (event.type, source, app, event.detail1,event.detail2, anydata)
         debug.println(debug.LEVEL_INFO, msg, True)
@@ -472,6 +481,12 @@ class EventManager:
                 msg = "EVENT MANAGER: Should suspend events for active event on window."
                 debug.println(debug.LEVEL_INFO, msg, True)
                 return True
+        if AXUtilities.is_document(event.source):
+            if event.type.endswith("busy"):
+                msg = "EVENT MANAGER: Should suspend events for busy event on document."
+                debug.println(debug.LEVEL_INFO, msg, True)
+                return True
+
         return False
 
     def _didSuspendEventsFor(self, event):
