GNOME/orca

commit af44e7f258eabce4f9e7a9731b3d3ef9ff9274ba

spd: Make sure sayAll progress callback gets the correct offsets

src/orca/speechserver.py
@@ -90,6 +90,13 @@ class SayAllContext:
         return "SAY ALL: %s '%s' (%i-%i, current: %i)" % \
             (self.obj, self.utterance, self.startOffset, self.endOffset, self.currentOffset)
 
+    def copy(self):
+        new = SayAllContext(self.obj, self.utterance,
+                            self.startOffset, self.endOffset)
+        new.currentOffset = self.currentOffset
+        new.currentEndOffset = self.currentEndOffset
+        return new
+
 
 class SpeechServer(object):
     """Provides speech server abstraction."""

src/orca/speechdispatcherfactory.py
@@ -507,7 +507,7 @@ class SpeechServer(speechserver.SpeechServer):
                 elif t == speechserver.SayAllContext.COMPLETED:
                     context.currentOffset = context.endOffset
                     context.currentEndOffset = None
-                GLib.idle_add(orca_callback, context, t)
+                GLib.idle_add(orca_callback, context.copy(), t)
                 if t == speechserver.SayAllContext.COMPLETED:
                     GLib.idle_add(self._say_all, iterator, orca_callback)
             self._speak(context.utterance, acss, callback=callback,

commit 884e73f214025a357e6194e24a5abdc643b0133b

Fix seeking in say-all
This got broken in af44e7f258eabce4f9e7a9731b3d3ef9ff9274ba because the
implicit context comparison got broken in the process.  Fix this by
implementing comparison explicitly.

Fixes #399.

src/orca/speechserver.py
@@ -97,6 +97,12 @@ class SayAllContext:
         new.currentEndOffset = self.currentEndOffset
         return new
 
+    def __eq__(self, other):
+        return (self.startOffset == other.startOffset and
+                self.endOffset == other.endOffset and
+                self.obj == other.obj and
+                self.utterance == other.utterance)
+
 
 class SpeechServer(object):
     """Provides speech server abstraction."""