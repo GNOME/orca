GNOME/orca

commit b9a11edd12bd72af4fb44fbfb223f7c5273c494b

Web: Eliminate unnecessary keygrab refreshes
locusOfFocusChanged was unconditionally refreshing grabs if the new
locusOfFocus was not in a document. We should only do this work if
we were in a document before. Otherwise, things like arrowing in a
browser's menu will refresh grabs every time the selected item
changes.

src/orca/scripts/web/script.py
@@ -1227,6 +1227,18 @@ class Script(default.Script):
             self._madeFindAnnouncement = False
             self._inFocusMode = False
 
+            oldDocument = self.utilities.getTopLevelDocumentForObject(oldFocus)
+            if not document and self.utilities.isDocument(oldFocus):
+                oldDocument = oldFocus
+
+            if not oldDocument:
+                msg = "WEB: Not refreshing grabs because we weren't in a document before"
+                debug.printMessage(debug.LEVEL_INFO, msg, True)
+                return False
+
+            tokens = ["WEB: Refreshing grabs because we left document", oldDocument]
+            debug.printTokens(debug.LEVEL_INFO, tokens, True)
+
             reason = "locus of focus no longer in document"
             self.caretNavigation.suspend_commands(self, True, reason)
             self.structuralNavigation.suspend_commands(self, True, reason)

commit 30f592e86e486dbed9dcf336a8d9684efe2a2983

Fix another regression from b9a11edd12bd72af4fb44fbfb223f7c5273c494b
When any script is activated, default.activate() adds key grabs without
checking what should be enabled/disabled. As a result, if a web-script
based app was launched, all the navigation commands were enabled by
default and had to be toggled off before native navigation would work.

Prior to b9a11edd12bd72af4fb44fbfb223f7c5273c494b, every single time
focus changed, we refreshed keygrabs. That was extremely non-performant
and unnecessary. However, now that we no longer do all that work, we
need to ensure the initial grabs are done with knowledge of what should
be enabled.

* Override default.activate in the web script
* Suspend (or not) navigation commands based on the locus of focus
* Call default.activate so the normal activation work, including adding
  key grabs, can proceed.

src/orca/scripts/web/script.py
@@ -107,6 +107,20 @@ class Script(default.Script):
         self.attributeNamesDict["text-align"] = "justification"
         self.attributeNamesDict["text-indent"] = "indent"
 
+    def activate(self):
+        """Called when this script is activated."""
+
+        tokens = ["WEB: Activating script for", self.app]
+        debug.printTokens(debug.LEVEL_INFO, tokens, True)
+
+        in_doc = self.utilities.inDocumentContent()
+        reason = f"script activation, in document content: {in_doc}"
+        self.caretNavigation.suspend_commands(self, not in_doc, reason)
+        self.structuralNavigation.suspend_commands(self, not in_doc, reason)
+        self.liveRegionManager.suspend_commands(self, not in_doc, reason)
+        self.tableNavigator.suspend_commands(self, not in_doc, reason)
+        super().activate(self)
+
     def deactivate(self):
         """Called when this script is deactivated."""
 