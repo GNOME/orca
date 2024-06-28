24a241606af255a21c0c60704662db156d340a8b
Joanmarie Diggs <jdiggs@igalia.com>
2024-06-28 16:14:57 +0200

    Web: Fix error from f1fb74
    A page-nav check was mistakenly turned into a line-nav check.

--- src/orca/scripts/web/script.py
+++ src/orca/scripts/web/script.py
@@ -1247,7 +1247,8 @@ class Script(default.Script):
             tokens = ["WEB: New focus", new_focus, "is anchor. Generating line."]
             debug.printTokens(debug.LEVEL_INFO, tokens, True)
             contents = self.utilities.getLineContentsAtOffset(new_focus, 0)
-        elif lastCommandWasLineNav and not AXTable.get_table(new_focus) \
+        elif input_event_manager.get_manager().last_event_was_page_navigation() \
+             and not AXTable.get_table(new_focus) \
              and not AXUtilities.is_feed_article(new_focus):
             tokens = ["WEB: New focus", new_focus, "was scrolled to. Generating line."]
             debug.printTokens(debug.LEVEL_INFO, tokens, True)
