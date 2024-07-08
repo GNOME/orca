aa883d6c5904075b3a8de1be353863961d914339
Joanmarie Diggs <jdiggs@igalia.com>
2024-07-08 10:37:16 +0200

    Web: Treat toggle buttons as a form control for structural navigation

--- src/orca/ax_utilities_role.py
+++ src/orca/ax_utilities_role.py
@@ -79,7 +79,8 @@ class AXUtilitiesRole:
                  Atspi.Role.ENTRY,
                  Atspi.Role.PASSWORD_TEXT,
                  Atspi.Role.PUSH_BUTTON,
-                 Atspi.Role.SPIN_BUTTON]
+                 Atspi.Role.SPIN_BUTTON,
+                 Atspi.Role.TOGGLE_BUTTON]
         return roles
 
     @staticmethod
