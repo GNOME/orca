GNOME/orca

commit c08c49b9e2ccca0a85d40d91f592223c2cce7fa4

Eliminate use of displayedText()
This function is used quite a bit to get the text of an object which
implements the accessible text interface. But it falls back on -- or
sometimes prefers -- the accessible name. Individual callers should
ask for the text when they want text, the name when they want the name,
do any fallback work needed, etc.

See issue #528.

src/orca/generator.py
@@ -131,12 +131,7 @@ class Generator:
         Otherwise, the accessible name of the object will be used.  If
         there is no accessible name, then the description of the
         object will be used.  This method will return an empty array
-        if nothing can be found.  [[[WDW - I wonder if we should just
-        have _generateName, _generateDescription,
-        _generateDisplayedText, etc., that don't do any fallback.
-        Then, we can allow the formatting to do the fallback (e.g.,
-        'displayedText or name or description'). [[[JD to WDW - I
-        needed a _generateDescription for whereAmI. :-) See below.
+        if nothing can be found.
         """
         result = []
         self._script.point_of_reference['usedDescriptionForName'] = False
@@ -648,9 +643,9 @@ class Generator:
 
         tokens = []
         for header in headers:
-            token = self._script.utilities.displayedText(header)
-            if token and token.strip():
-                tokens.append(token.strip())
+            token = AXObject.get_name(header).strip() or AXText.get_all_text(header).strip()
+            if token:
+                tokens.append(token)
 
         if not tokens:
             return result
@@ -681,9 +676,9 @@ class Generator:
 
         tokens = []
         for header in headers:
-            token = self._script.utilities.displayedText(header)
-            if token and token.strip():
-                tokens.append(token.strip())
+            token = AXObject.get_name(header).strip() or AXText.get_all_text(header).strip()
+            if token:
+                tokens.append(token)
 
         if not tokens:
             return result
@@ -720,11 +715,11 @@ class Generator:
 
         result = []
         descendant = self._script.utilities.realActiveDescendant(obj)
-        label = self._script.utilities.displayedText(descendant)
+        label = AXObject.get_name(descendant) or AXText.get_all_text(descendant)
         if not label and self._script.utilities.hasMeaningfulToggleAction(obj):
-              headers = AXTable.get_column_headers(obj)
-              if (headers):
-                result.append(AXObject.get_name(headers[0]))
+            headers = AXTable.get_column_headers(obj)
+            if headers:
+                result.append(AXObject.get_name(headers[0]) or AXText.get_all_text(headers[0]))
         return result
 
     def _generateRealTableCell(self, obj, **args):
@@ -866,11 +861,11 @@ class Generator:
         if result:
             return result
 
-        displayedText = self._script.utilities.displayedText(obj)
-        if not displayedText:
+        text = AXText.get_all_text(obj)
+        if not text:
             return []
 
-        return [displayedText]
+        return [text]
 
     #####################################################################
     #                                                                   #
@@ -968,7 +963,7 @@ class Generator:
         if labels:
             radioGroupLabel = labels[0]
         if radioGroupLabel:
-            return [self._script.utilities.displayedText(radioGroupLabel)]
+            return [AXObject.get_name(radioGroupLabel)]
 
         parent = AXObject.get_parent_checked(obj)
         while parent:
@@ -991,8 +986,7 @@ class Generator:
         if not (AXUtilities.is_table_cell(rad) and AXObject.get_child_count(rad)):
             return self._generateDisplayedText(rad, **args)
 
-        content = set([self._script.utilities.displayedText(x).strip() \
-            for x in AXObject.iter_children(rad)])
+        content = set([AXObject.get_name(x) for x in AXObject.iter_children(rad)])
         rv = " ".join(filter(lambda x: x, content))
         if not rv:
             return self._generateDisplayedText(rad, **args)

src/orca/scripts/web/speech_generator.py
@@ -285,7 +285,8 @@ class SpeechGenerator(speech_generator.SpeechGenerator):
 
         result = []
         for o in objs:
-            string = self._script.utilities.displayedText(o) or self.getLocalizedRoleName(o)
+            string = self._script.utilities.expandEOCs(o) or AXObject.get_name(o) \
+                or self.getLocalizedRoleName(o)
             words = string.split()
             if len(words) > 5:
                 words = words[0:5] + ['...']

src/orca/scripts/apps/soffice/speech_generator.py
@@ -39,26 +39,6 @@ class SpeechGenerator(speech_generator.SpeechGenerator):
     def __init__(self, script):
         speech_generator.SpeechGenerator.__init__(self, script)
 
-    def _generateName(self, obj, **args):
-        """Returns an array of strings for use by speech and braille that
-        represent the name of the object.  If the object is directly
-        displaying any text, that text will be treated as the name.
-        Otherwise, the accessible name of the object will be used.  If
-        there is no accessible name, then the description of the
-        object will be used.  This method will return an empty array
-        if nothing can be found.
-        """
-
-        # TODO - JD: This should be the behavior by default. But the default
-        # generators call displayedText(). Once that is corrected, this method
-        # can be removed.
-        if AXObject.get_name(obj):
-            result = [AXObject.get_name(obj)]
-            result.extend(self.voice(speech_generator.DEFAULT, obj=obj, **args))
-            return result
-
-        return super()._generateName(obj, **args)
-
     def _generateAnyTextSelection(self, obj, **args):
         comboBoxEntry = self._script.utilities.getEntryForEditableComboBox(obj)
         if comboBoxEntry:

src/orca/speech_generator.py
@@ -878,7 +878,7 @@ class SpeechGenerator(generator.Generator):
             #
             result.extend(self._generateLabel(obj))
             result.extend(self._generateRoleName(obj))
-            result.append(self._script.utilities.displayedText(obj))
+            result.append(AXText.get_all_text(obj))
         else:
             link_uri_info = urllib.parse.urlparse(link_uri)
             if link_uri_info[0] in ["ftp", "ftps", "file"]:
@@ -888,7 +888,7 @@ class SpeechGenerator(generator.Generator):
                                  "file" : fileName[-1]})
             else:
                 linkOutput = messages.LINK_WITH_PROTOCOL % link_uri_info[0]
-                text = self._script.utilities.displayedText(obj)
+                text = AXText.get_all_text(obj)
                 isVisited = AXUtilities.is_visited(obj)
                 if not isVisited:
                     linkOutput = messages.LINK_WITH_PROTOCOL % link_uri_info[0]
@@ -2581,7 +2581,7 @@ class SpeechGenerator(generator.Generator):
             result = [messages.MATH_SQUARE_ROOT_OF]
         else:
             index = self._script.utilities.getMathRootIndex(obj)
-            string = self._script.utilities.displayedText(index)
+            string = AXText.get_all_text(index)
             if string == "2":
                 result = [messages.MATH_SQUARE_ROOT_OF]
             elif string == "3":

src/orca/chat.py
@@ -847,31 +847,14 @@ class Chat:
         # should override this method. :-)
         #
         def pred(x):
-            return AXUtilities.is_page_tab(x) or AXUtilities.is_frame(x)
+            if not (AXUtilities.is_page_tab(x) or AXUtilities.is_frame(x)):
+                return False
+            return bool(AXObject.get_name(x))
 
         ancestor = AXObject.find_ancestor(obj, pred)
-        name = ""
-        try:
-            text = self._script.utilities.displayedText(ancestor)
-            if text.lower().strip() != self._script.name.lower().strip():
-                name = text
-        except Exception:
-            pass
-
-        # Some applications don't trash their page tab list when there is
-        # only one active chat, but instead they remove the text or hide
-        # the item. Therefore, we'll give it one more shot.
-        #
-        if not name:
-            ancestor = AXObject.find_ancestor(obj, AXUtilities.is_frame)
-            try:
-                text = self._script.utilities.displayedText(ancestor)
-                if text.lower().strip() != self._script.name.lower().strip():
-                    name = text
-            except Exception:
-                pass
-
-        return name
+        if ancestor:
+            return AXObject.get_name(ancestor)
+        return ""
 
     def isAutoCompletedTextEvent(self, event):
         """Returns True if event is associated with text being autocompleted.

src/orca/mouse_review.py
@@ -327,9 +327,10 @@ class _ItemContext:
                 return True
             if not self._script.utilities.isEditableTextArea(self._obj):
                 return True
-            if AXUtilities.is_table_cell(self._obj) \
-               and self._string.get_string() == self._script.utilities.displayedText(self._obj):
-                return True
+            if AXUtilities.is_table_cell(self._obj):
+                text = AXText.get_all_text(self._obj) or AXObject.get_name(self._obj)
+                if self._string.get_string() == text:
+                    return True
 
         if self._string != prior._string and self._string.present():
             return True

src/orca/script_utilities.py
@@ -202,14 +202,14 @@ class Utilities:
         """
 
         labels = AXUtilities.get_is_labelled_by(obj)
-        return " ".join(map(self.displayedText, labels))
+        return " ".join(AXText.get_all_text(label) or AXObject.get_name(label) for label in labels)
 
     def preferDescriptionOverName(self, obj):
         return False
 
     def detailsContentForObject(self, obj):
         details = self.detailsForObject(obj)
-        return list(map(self.displayedText, details))
+        return list(map(AXText.get_all_text, details))
 
     def detailsForObject(self, obj, textOnly=True):
         """Return a list of objects containing details for obj."""
@@ -233,42 +233,7 @@ class Utilities:
         """Returns the text being displayed for the object describing obj."""
 
         descriptions = AXUtilities.get_is_described_by(obj)
-        return " ".join(map(self.displayedText, descriptions))
-
-    def displayedText(self, obj):
-        """Returns the text being displayed for an object.
-
-        Arguments:
-        - obj: the object
-
-        Returns the text being displayed for an object or None if there isn't
-        any text being shown.
-        """
-
-        # TODO - JD: It's finally time to consider killing this for real.
-
-        name = AXObject.get_name(obj)
-        role = AXObject.get_role(obj)
-        if role in [Atspi.Role.PUSH_BUTTON, Atspi.Role.LABEL] and name:
-            return name
-
-        displayedText = AXText.get_all_text(obj)
-        if self.EMBEDDED_OBJECT_CHARACTER in displayedText:
-            displayedText = None
-
-        if not displayedText and role not in [Atspi.Role.COMBO_BOX, Atspi.Role.SPIN_BUTTON]:
-            # TODO - JD: This should probably get nuked. But all sorts of
-            # existing code might be relying upon this bogus hack. So it
-            # will need thorough testing when removed.
-            displayedText = name
-
-        if not displayedText and role in [Atspi.Role.PUSH_BUTTON, Atspi.Role.LIST_ITEM]:
-            labels = self.unrelatedLabels(obj, minimumWords=1)
-            if not labels:
-                labels = self.unrelatedLabels(obj, onlyShowing=False, minimumWords=1)
-            displayedText = " ".join(map(self.displayedText, labels))
-
-        return displayedText
+        return " ".join(AXText.get_all_text(d) or AXObject.get_name(d) for d in descriptions)
 
     def documentFrame(self, obj=None):
         """Returns the document frame which is displaying the content.
@@ -735,7 +700,7 @@ class Utilities:
         if text and text not in tokens:
             tokens.append(text)
         else:
-            labels = " ".join(map(self.displayedText, self.unrelatedLabels(obj, False, 1)))
+            labels = " ".join(map(AXText.get_all_text, self.unrelatedLabels(obj, False, 1)))
             if labels and labels not in tokens:
                 tokens.append(labels)
 
@@ -829,7 +794,7 @@ class Utilities:
         elif self.isHidden(obj):
             layoutOnly = True
         else:
-            if not (self.displayedText(obj) or self.displayedLabel(obj)):
+            if not (AXObject.get_name(obj) or self.displayedLabel(obj) or AXText.get_all_text(obj)):
                 layoutOnly = True
 
         if layoutOnly:
@@ -1202,7 +1167,8 @@ class Utilities:
             return obj
 
         def pred(x):
-            return x and not self.isStaticTextLeaf(x) and self.displayedText(x).strip()
+            return x and not self.isStaticTextLeaf(x) \
+                and (AXObject.get_name(x) or AXText.get_all_text(x))
 
         child = AXObject.find_descendant(obj, pred)
         if child is not None:
@@ -1406,7 +1372,7 @@ class Utilities:
         # Eliminate things suspected to be labels for widgets
         labels_filtered = []
         for label in labels:
-            name = AXObject.get_name(label) or self.displayedText(label)
+            name = AXObject.get_name(label) or AXText.get_all_text(label)
             if name and name in [rootName, AXObject.get_name(AXObject.get_parent(label))]:
                 continue
             if len(name.split()) < minimumWords:
@@ -2301,18 +2267,18 @@ class Utilities:
 
     def getComboBoxValue(self, obj):
         if not AXObject.get_child_count(obj):
-            return self.displayedText(obj)
+            return AXObject.get_name(obj) or AXText.get_all_text(obj)
 
         entry = self.getEntryForEditableComboBox(obj)
         if entry:
-            return self.displayedText(entry)
+            return AXText.get_all_text(entry)
 
         selected = self._script.utilities.selectedChildren(obj)
         selected = selected or self._script.utilities.selectedChildren(AXObject.get_child(obj, 0))
         if len(selected) == 1:
-            return selected[0].name or self.displayedText(selected[0])
+            return AXObject.get_name(selected[0]) or AXText.get_all_text(selected[0])
 
-        return self.displayedText(obj)
+        return AXObject.get_name(obj) or AXText.get_all_text(obj)
 
     def isNonModalPopOver(self, obj):
         if not AXUtilities.get_is_popup_for(obj):

src/orca/scripts/apps/gedit/script.py
@@ -29,6 +29,7 @@ from orca import focus_manager
 from orca import input_event_manager
 from orca.scripts.toolkits import gtk
 from orca.ax_object import AXObject
+from orca.ax_text import AXText
 from orca.ax_utilities import AXUtilities
 from .spellcheck import SpellCheck
 
@@ -113,7 +114,7 @@ class Script(gtk.Script):
             return
 
         entry = self.spellcheck.getChangeToEntry()
-        if name != self.utilities.displayedText(entry):
+        if name != AXText.get_all_text(entry):
             return
 
         # If we're here, the locusOfFocus was in the selection list when

commit 285b74afcfcb20b8c095c244ffa942d44eeb4584

Web: Fix link regression better
The removal of displayedText() and all of its associated messiness
in c08c49b9e2c regressed link presentation when the link had one or
more children. 25224a6f6a fixed that for links with text children
but not for children of other types, such as image links.

Looking further into it, it turns out that the web script's
_generateName() was returning nothing for links which were not
given an explicit name. Then displayedText() ignored the link text
due to the presence of "U+FFFC" and returned the name as the
displayed text. Thus the real fix is to have _generateName() do
what it claims to do.

src/orca/generator.py
@@ -866,7 +866,8 @@ class Generator:
             return []
 
         if "\ufffc" in text:
-            return self._generateExpandedEOCs(obj, **args)
+            return []
+
         return [text]
 
     #####################################################################

src/orca/scripts/web/speech_generator.py
@@ -347,9 +347,6 @@ class SpeechGenerator(speech_generator.SpeechGenerator):
         if AXObject.get_name(obj):
             if self._script.utilities.preferDescriptionOverName(obj):
                 result = [AXObject.get_description(obj)]
-            elif self._script.utilities.isLink(obj) \
-                 and not self._script.utilities.hasExplicitName(obj):
-                return []
             else:
                 name = AXObject.get_name(obj)
                 if not self._script.utilities.hasExplicitName(obj):