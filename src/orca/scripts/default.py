GNOME/orca

commit 164ce522c044d37085d8633cbb76b9e3885c626e

Web: Always handle sayCharacter/Word/Line() for document content
We were deferring to the default script's sayCharacter/Word/Line()
if the element was not editable and our caret navigation was not
being used. As a result, if one used the browser's native caret
navigation (i.e. F7 + switching to focus mode), any embedded object
in the character/word/line would not be expanded and the user would
hear however their synthesizer handles U+FFFC (often it's not spoken).

We should do our best to present the same character/word/line regardless
of whether we're in browse mode or focus mode, therefore only defer to
the default script if we're not in document content.

Note: There is an additional issue not addressed by this commit, namely
if native browser navigation in focus mode causes a link to become
focused, we only present that object rather than the unit of text
associated with the navigation. That will be addressed in a follow-up
commit.

src/orca/scripts/web/script.py
@@ -809,13 +809,19 @@ class Script(default.Script):
     def sayCharacter(self, obj):
         """Speaks the character at the current caret position."""
 
-        if not self.caret_navigation.last_input_event_was_navigation_command() \
-           and not self.utilities.isContentEditableWithEmbeddedObjects(obj):
+        tokens = ["WEB: Say character for", obj]
+        debug.printTokens(debug.LEVEL_INFO, tokens, True)
+        if not self.utilities.inDocumentContent(obj):
+            msg = "WEB: Object is not in document content."
+            debug.printMessage(debug.LEVEL_INFO, msg, True)
             super().sayCharacter(obj)
             return
 
         document = self.utilities.getTopLevelDocumentForObject(obj)
         obj, offset = self.utilities.getCaretContext(documentFrame=document)
+        tokens = ["WEB: Adjusted object and offset for say character to", obj, offset]
+        debug.printTokens(debug.LEVEL_INFO, tokens, True)
+
         if not obj:
             return
 
@@ -846,8 +852,11 @@ class Script(default.Script):
     def sayWord(self, obj):
         """Speaks the word at the current caret position."""
 
-        isEditable = self.utilities.isContentEditableWithEmbeddedObjects(obj)
-        if not self.caret_navigation.last_input_event_was_navigation_command() and not isEditable:
+        tokens = ["WEB: Say word for", obj]
+        debug.printTokens(debug.LEVEL_INFO, tokens, True)
+        if not self.utilities.inDocumentContent(obj):
+            msg = "WEB: Object is not in document content."
+            debug.printMessage(debug.LEVEL_INFO, msg, True)
             super().sayWord(obj)
             return
 
@@ -855,6 +864,9 @@ class Script(default.Script):
         obj, offset = self.utilities.getCaretContext(documentFrame=document)
         if input_event_manager.get_manager().last_event_was_right():
             offset -= 1
+
+        tokens = ["WEB: Adjusted object and offset for say word to", obj, offset]
+        debug.printTokens(debug.LEVEL_INFO, tokens, True)
 
         wordContents = self.utilities.getWordContentsAtOffset(obj, offset, useCache=True)
         textObj, startOffset, endOffset, word = wordContents[0]
@@ -865,22 +877,21 @@ class Script(default.Script):
     def sayLine(self, obj):
         """Speaks the line at the current caret position."""
 
-        lastCommandWasCaretNav = self.caret_navigation.last_input_event_was_navigation_command()
-        lastCommandWasStructNav = \
-            self.structural_navigation.last_input_event_was_navigation_command() \
-            or self.get_table_navigator().last_input_event_was_navigation_command()
-
-        isEditable = self.utilities.isContentEditableWithEmbeddedObjects(obj)
-        if not (lastCommandWasCaretNav or lastCommandWasStructNav) and not isEditable:
+        tokens = ["WEB: Say line for", obj]
+        debug.printTokens(debug.LEVEL_INFO, tokens, True)
+        if not self.utilities.inDocumentContent(obj):
+            msg = "WEB: Object is not in document content."
+            debug.printMessage(debug.LEVEL_INFO, msg, True)
             super().sayLine(obj)
             return
 
         document = self.utilities.getTopLevelDocumentForObject(obj)
-        priorObj = None
-        if lastCommandWasCaretNav or isEditable:
-            priorObj, priorOffset = self.utilities.getPriorContext(documentFrame=document)
+        priorObj, _priorOffset = self.utilities.getPriorContext(documentFrame=document)
 
         obj, offset = self.utilities.getCaretContext(documentFrame=document)
+        tokens = ["WEB: Adjusted object and offset for say line to", obj, offset]
+        debug.printTokens(debug.LEVEL_INFO, tokens, True)
+
         contents = self.utilities.getLineContentsAtOffset(obj, offset, useCache=True)
         self.speakContents(contents, priorObj=priorObj)
         self.point_of_reference["lastTextUnitSpoken"] = "line"

commit 39d70da5d5fe0a1e99d9f3d7cd7e07afeccc3037

Temporarily special-case editable content in web sayLine()
Commit 164ce522c caused the web script to always handle speaking the
character/word/line for document content. As a side effect, Orca stopped
speaking indentation in VSCode. The reason why is that the adjustments
to the string to be spoken are not currently being made in the generators
as they should; instead they're being made on the spot in the default
script's sayLine().

Given that doing the work where it belongs may be a non-trivial change,
let the default script's sayLine() handle presentation of a line of text
which lacks U+FFFC and is in an editable object for now.

src/orca/scripts/default.py
@@ -2037,6 +2037,7 @@ class Script(script.Script):
 
         line, startOffset = AXText.get_line_at_offset(obj)[0:2]
         if len(line) and line != "\n":
+            # TODO - JD: This needs to be done in the generators.
             indentationDescription = self.utilities.indentationDescription(line)
             if indentationDescription:
                 self.speakMessage(indentationDescription)
@@ -2051,6 +2052,7 @@ class Script(script.Script):
                 if not string:
                     continue
 
+                # TODO - JD: This needs to be done in the generators.
                 voice = self.speech_generator.voice(
                     obj=obj, string=string, language=language, dialect=dialect)
                 string = self.utilities.adjustForLinks(obj, string, start)

src/orca/scripts/web/script.py
@@ -885,6 +885,15 @@ class Script(default.Script):
             super().sayLine(obj)
             return
 
+        # TODO - JD: We're making an exception here because the default script's sayLine()
+        # handles verbalized punctuation, indentation, repeats, etc. That adjustment belongs
+        # in the generators, but that's another potentially non-trivial change.
+        if AXUtilities.is_editable(obj) and "\ufffc" not in AXText.get_line_at_offset(obj)[0]:
+            msg = "WEB: Object is editable and line has no EOCs."
+            debug.printMessage(debug.LEVEL_INFO, msg, True)
+            super().sayLine(obj)
+            return
+
         document = self.utilities.getTopLevelDocumentForObject(obj)
         priorObj, _priorOffset = self.utilities.getPriorContext(documentFrame=document)
 