GNOME/orca

commit a6116995bff6dbc2fd6d74b62cf1d60441dc5786

Address several issues related to presentation of sets in UI
* Eliminate redundant logic between getting status bar items and
  getting list/listbox UI items
* Handle UI list items not being immediate children of the list/
  listbox when calculating set size.
* Improve presentation of descendants of UI lists (child widgets
  might be buried deeper in the tree)

src/orca/generator.py
@@ -43,6 +43,7 @@ from gi.repository import Atspi
 
 from . import braille
 from . import debug
+from . import focus_manager
 from . import object_properties
 from . import settings
 from . import settings_manager
@@ -298,10 +299,16 @@ class Generator:
 
         return []
 
+    def _generate_result_separator(self, _obj, **_args):
+        return []
+
     ################################# BASIC DETAILS #################################
 
     @log_generator_output
-    def _generate_accessible_description(self, obj, **_args):
+    def _generate_accessible_description(self, obj, **args):
+        if args.get("omitDescription"):
+            return []
+
         if hash(obj) in Generator.CACHED_DESCRIPTION:
             return Generator.CACHED_DESCRIPTION.get(hash(obj))
 
@@ -345,11 +352,11 @@ class Generator:
 
     @log_generator_output
     def _generate_accessible_label_and_name(self, obj, **args):
-        if AXUtilities.is_menu(obj, args.get("role")) \
-           and self._script.utilities.isPopupMenuForCurrentItem(obj):
-            tokens = ["GENERATOR:", obj, "is popup menu for current item."]
-            debug.printTokens(debug.LEVEL_INFO, tokens, True)
-            return []
+        focus = focus_manager.get_manager().get_locus_of_focus()
+        if focus and obj != focus:
+            name = AXObject.get_name(obj)
+            if name and name in [AXObject.get_name(focus), AXObject.get_description(focus)]:
+                return []
 
         result = []
         label = self._generate_accessible_label(obj, **args)
@@ -503,6 +510,33 @@ class Generator:
             return Atspi.Role.IMAGE
 
         return role
+
+    @log_generator_output
+    def _generate_descendants(self, obj, **args):
+        result = []
+        obj_name = AXObject.get_name(obj)
+        descendants = self._script.utilities.getOnScreenObjects(obj)
+        for child in descendants:
+            if child == obj:
+                continue
+            if AXUtilities.is_image(child):
+                continue
+            if AXUtilities.is_label(child):
+                if not AXText.has_presentable_text(child):
+                    continue
+                if AXObject.get_name(child) == obj_name:
+                    continue
+                if child in AXUtilities.get_is_labelled_by(obj):
+                    continue
+                if child in AXUtilities.get_is_described_by(obj):
+                    continue
+
+            child_result = self.generate(child, includeContext=False, omitDescription=True)
+            if child_result:
+                result.extend(child_result)
+                result.extend(self._generate_result_separator(child, **args))
+
+        return result
 
     @log_generator_output
     def _generate_focused_item(self, obj, **args):

src/orca/ax_utilities.py
@@ -417,6 +417,9 @@ class AXUtilities:
             if len(selected_children) == 1:
                 obj = selected_children[0]
 
+        if AXUtilitiesRole.is_list(obj) or AXUtilitiesRole.is_list_box(obj):
+            obj = AXObject.find_descendant(obj, AXUtilitiesRole.is_list_item)
+
         members = AXUtilities.get_set_members(obj)
         return len(members)
 

src/orca/braille_generator.py
@@ -170,6 +170,9 @@ class BrailleGenerator(generator.Generator):
                         combined, prior, delimiter)
         return combined
 
+    def _generate_result_separator(self, _obj, **_args):
+        return [braille.Region(" ")]
+
     ################################# BASIC DETAILS #################################
 
     @log_generator_output
@@ -242,35 +245,6 @@ class BrailleGenerator(generator.Generator):
         if count < 0:
             return []
         return [f"({messages.valueCountForTerm(count)})"]
-
-    @log_generator_output
-    def _generate_status_bar_items(self, obj, **_args):
-        if not AXUtilities.is_status_bar(obj):
-            return []
-
-        items = self._script.utilities.statusBarItems(obj)
-        if not items or items == [obj]:
-            return []
-
-        result = []
-        for child in items:
-            child_result = self.generate(child, includeContext=False)
-            if child_result:
-                result.extend(child_result)
-                result.append(braille.Region(" "))
-
-        return result
-
-    @log_generator_output
-    def _generate_list_box_item_widgets(self, obj, **_args):
-        if not AXUtilities.is_list_box(AXObject.get_parent(obj)):
-            return []
-
-        result = []
-        for widget in AXUtilities.get_all_widgets(obj):
-            result.extend(self.generate(widget, includeContext=False))
-            result.append(braille.Region(" "))
-        return result
 
     ################################### KEYBOARD ###################################
 
@@ -959,10 +933,7 @@ class BrailleGenerator(generator.Generator):
         if level:
             result += [braille.Region(" " + self._as_string(level))]
 
-        widgets = self._generate_list_box_item_widgets(obj, **args)
-        if widgets:
-            result += [braille.Region(" " + self._as_string(widgets))]
-
+        result += self._generate_descendants(obj, **args)
         return result
 
     def _generate_log(self, obj, **args):
@@ -1294,7 +1265,7 @@ class BrailleGenerator(generator.Generator):
                 self._generate_accessible_label_and_name(obj, **args) +
                 self._generate_accessible_role(obj, **args)))]
         result += [braille.Region(" ")]
-        result += self._generate_status_bar_items(obj, **args)
+        result += self._generate_descendants(obj, **args)
         result += self._generate_default_suffix(obj, **args)
         return result
 

src/orca/script_utilities.py
@@ -1170,32 +1170,6 @@ class Utilities:
 
         return obj
 
-    def isStatusBarDescendant(self, obj):
-        if obj is None:
-            return False
-
-        return AXObject.find_ancestor(obj, AXUtilities.is_status_bar) is not None
-
-    def statusBarItems(self, obj):
-        if not AXUtilities.is_status_bar(obj):
-            return []
-
-        start = time.time()
-        items = self._script.point_of_reference.get('statusBarItems')
-        if not items:
-
-            def include(x):
-                return not AXUtilities.is_status_bar(x)
-
-            items = list(filter(include, self.getOnScreenObjects(obj)))
-            self._script.point_of_reference['statusBarItems'] = items
-
-        end = time.time()
-        msg = f"SCRIPT UTILITIES: Time getting status bar items: {end - start:.4f}"
-        debug.printMessage(debug.LEVEL_INFO, msg, True)
-
-        return items
-
     def infoBar(self, root):
         return None
 

src/orca/scripts/default.py
@@ -1345,6 +1345,11 @@ class Script(script.Script):
                 msg = "DEFAULT: Event is redundant notification for the locusOfFocus"
                 debug.printMessage(debug.LEVEL_INFO, msg, True)
                 return
+            if AXObject.get_name(focus) == event.any_data:
+                msg = "DEFAULT: Event is redundant notification for the locusOfFocus"
+                debug.printMessage(debug.LEVEL_INFO, msg, True)
+                return
+
         elif event.source != focus_manager.get_manager().get_locus_of_focus():
             msg = "DEFAULT: Event is for object other than the locusOfFocus"
             debug.printMessage(debug.LEVEL_INFO, msg, True)

src/orca/speech_generator.py
@@ -164,6 +164,9 @@ class SpeechGenerator(generator.Generator):
             result.append(dialogs)
         return result
 
+    def _generate_result_separator(self, _obj, **_args):
+        return PAUSE
+
     def _generate_pause(self, _obj, **args):
         if not settings_manager.get_manager().get_setting('enablePauseBreaks') \
            or args.get('eliminatePauses', False):
@@ -495,9 +498,6 @@ class SpeechGenerator(generator.Generator):
         role = args.get("role")
         if AXUtilities.is_list(obj, role) or AXUtilities.is_list_box(obj, role):
             set_size = AXUtilities.get_set_size(obj)
-            if set_size is None:
-                children = list(AXObject.iter_children(obj, AXUtilities.is_list_item))
-                set_size = len(children)
             if set_size:
                 result = [messages.listItemCount(set_size)]
                 result.extend(self.voice(SYSTEM, obj=obj, **args))
@@ -1003,39 +1003,6 @@ class SpeechGenerator(generator.Generator):
         position += 1
         result.append(string % {"index": position, "total": total})
         result.extend(self.voice(SYSTEM, obj=obj, **args))
-        return result
-
-    @log_generator_output
-    def _generate_status_bar_items(self, obj, **args):
-        if not AXUtilities.is_status_bar(obj):
-            return []
-
-        items = self._script.utilities.statusBarItems(obj)
-        if not items or items == [obj]:
-            return []
-
-        result = []
-        for child in items:
-            if child == obj:
-                continue
-
-            child_result = self.generate(child, includeContext=False)
-            if child_result:
-                result.extend(child_result)
-                if not isinstance(child_result[-1], Pause):
-                    result.extend(self._generate_pause(child, **args))
-
-        return result
-
-    @log_generator_output
-    def _generate_list_box_item_widgets(self, obj, **_args):
-        if not AXUtilities.is_list_box(AXObject.get_parent(obj)):
-            return []
-
-        result = []
-        widgets = AXUtilities.get_all_widgets(obj)
-        for widget in widgets:
-            result.append(self.generate(widget, includeContext=False))
         return result
 
     ################################### KEYBOARD ###################################
@@ -3100,10 +3067,11 @@ class SpeechGenerator(generator.Generator):
         result += self._generate_state_unselected(obj, **args)
         result += self._generate_pause(obj, **args)
         result += self._generate_state_expanded(obj, **args)
+        if AXUtilities.is_focusable(obj):
+            result += self._generate_descendants(obj, **args)
         result += self._generate_pause(obj, **args)
         result += self._generate_position_in_list(obj, **args)
         result += self._generate_pause(obj, **args)
-        result += self._generate_list_box_item_widgets(obj, **args)
         result += self._generate_default_suffix(obj, **args)
         return result
 
@@ -3681,8 +3649,7 @@ class SpeechGenerator(generator.Generator):
             return result
 
         result += self._generate_pause(obj, **args)
-        # TODO - JD: Move this logic here.
-        result += self._generate_status_bar_items(obj, **args)
+        result += self._generate_descendants(obj, **args)
         result += self._generate_default_suffix(obj, **args)
         return result
 

src/orca/where_am_i_presenter.py
@@ -375,9 +375,7 @@ class WhereAmIPresenter:
         if frame:
             statusbar = AXUtilities.get_status_bar(frame)
             if statusbar:
-                script.point_of_reference['statusBarItems'] = None
                 script.presentObject(statusbar, interrupt=True)
-                script.point_of_reference['statusBarItems'] = None
             else:
                 full = messages.STATUS_BAR_NOT_FOUND_FULL
                 brief = messages.STATUS_BAR_NOT_FOUND_BRIEF

commit 5217167a58e63cbbcfb80a276cc692f9bab39ebf

Hack around Qt giving us a new cell every time we ask for a child
a6116995bff6 broke presentation of Qt cells in QTreeView. That change
eliminated some chattiness by not presenting the name of an object
that was not the focused object if the name was identical. It turns
out that when we go to generate the name for one of these cells, the
equality check fails. We get a new copy of the object every time we
ask for the child via index.

As a safe hackaround in time for the release candidate, only do
the chattiness reduction if obj is not focus and obj and focus have
different roles. That should solve chattiness in the case of popups
with the same name as the focused object, as well as the redundant
labelling we're seeing in GTK4 (libadwaita) apps.

See issue #535.

src/orca/generator.py
@@ -353,7 +353,10 @@ class Generator:
     @log_generator_output
     def _generate_accessible_label_and_name(self, obj, **args):
         focus = focus_manager.get_manager().get_locus_of_focus()
-        if focus and obj != focus:
+        # TODO - JD: The role check is a quick workaround for issue #535 in which we stopped
+        # presenting Qt table cells because Qt keeps giving us a different object each and
+        # every time we ask for the cell. File a bug against Qt to get them to stop that.
+        if focus and obj != focus and AXObject.get_role(obj) != AXObject.get_role(focus):
             name = AXObject.get_name(obj)
             if name and name in [AXObject.get_name(focus), AXObject.get_description(focus)]:
                 return []