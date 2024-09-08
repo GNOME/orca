6293348bb8fd61955050135e8c9a12ed14640a2a
Joanmarie Diggs <jdiggs@igalia.com>
2024-09-08 16:39:13 +0200

    Don't say "grayed" for Calc cells
    Now that we support aria-disabled on grid cells, some users have
    reported Orca saying "grayed" for table cells. That means that in
    some versions of Calc, the cells lack the sensitive state. Work
    around that by not generating the sensitive state for spreadsheet
    cells.

--- src/orca/speech_generator.py
+++ src/orca/speech_generator.py
@@ -1735,11 +1735,9 @@ class SpeechGenerator(generator.Generator):
         if settings_manager.get_manager().get_setting("onlySpeakDisplayedText"):
             return []
 
-        if AXUtilities.is_sensitive(obj):
-            return []
-
-        result = [object_properties.STATE_INSENSITIVE_SPEECH]
-        result.extend(self.voice(SYSTEM, obj=obj, **args))
+        result = super()._generate_state_sensitive(obj, **args)
+        if result:
+            result.extend(self.voice(STATE, obj=obj, **args))
         return result
 
     @log_generator_output
