b8f3596442eb5b707cf39089523b760213cdce24
Joanmarie Diggs <jdiggs@igalia.com>
2025-06-10 11:55:49 +0200

    Structural Navigation: Interrupt speech in goObject() if we lack an input event
    If goObject() is called due to an input event such as "H" to go to the
    next heading, speech will be interrupted due to the press of "H". If
    navigation was not triggered by an input event, we still want to stop
    speaking the previous location before presenting the new location.
    Note that we cannot count on locus_of_focus_changed() in this case
    because the structural navigation code "silently" updates the locus of
    focus in order to prevent double-presentation (i.e. should the navigation
    trigger the object to claim focus).

--- src/orca/structural_navigation.py
+++ src/orca/structural_navigation.py
@@ -820,6 +820,9 @@ class StructuralNavigation:
         """
 
         self._last_input_event = event
+        if event is None:
+            self._script.presentationInterrupt()
+
         matches = self._getAll(structuralNavigationObject, arg)
         if not matches:
             structuralNavigationObject.present(None, arg)
