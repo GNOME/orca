e5095fa99093b030f07a3232f1760b94ef54ec7d
Federico Mena Quintero <federico@gnome.org>
2025-06-18 11:50:20 -0600

    ci/install-python-tools.sh: Explain the distinction between system and environment packages
    Part-of: <https://gitlab.gnome.org/GNOME/orca/-/merge_requests/248>

--- ci/install-python-tools.sh
+++ ci/install-python-tools.sh
@@ -3,6 +3,11 @@
 # Creates a Python virtual environment in /usr/local/python and installs
 # the modules from requirements.txt in it.  These modules are required
 # by various jobs in the CI pipeline.
+#
+# Note that Orca's run-time dependencies are *not* in this
+# ci/requirements.txt - those come from system packages.  The packages
+# that this script installs are Python tools for use during the CI
+# only, to check the code, rather than to run it.
 
 set -eux -o pipefail
 
