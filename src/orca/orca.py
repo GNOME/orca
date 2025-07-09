e3a704cfd855ff338405916c67577d8309c3abfd
Adrian Vovk <adrianvovk@gmail.com>
2025-07-08 23:47:59 -0400

    Don't shutdown on screen-reader-enabled -> false
    With systemd startup, this will no longer work: the only way to shut
    down Orca correctly would be to stop and disable the systemd service.
    This prevents weird edge-cases where the screen reader is supposed to be
    running but isn't.
    
    In GNOME, gnome-settings-daemon is responsible for managing the systemd
    service in tandem with the gsetting.
    
    MATE, Cinnamon, and Unity all use old versions of gnome-session or forks
    of gnome-session. Either way, gnome-session's builtin service manager
    will kill orca-autostart.desktop whenever the gsetting is false, so Orca
    doesn't need to do this on its own.
    
    KDE is currently using the gsetting to stop Orca, but I spoke to the KDE
    developers and they were interested to use the systemd service directly,
    like GNOME will be doing. This presents an opportunity to do some
    cleanup for KDE, which is currently forking off `gsettings set` commands
    to kill Orca

--- src/orca/orca.py
+++ src/orca/orca.py
@@ -41,7 +41,6 @@ gi.require_version("Atspi", "2.0")
 gi.require_version("Gdk", "3.0")
 from gi.repository import Atspi
 from gi.repository import Gdk
-from gi.repository import Gio
 from gi.repository import GLib
 
 from . import braille
@@ -165,13 +164,6 @@ def shutdown(script=None, _event=None, _signum=None):
 def main():
     """The main entry point for Orca."""
 
-    def _on_enabled_changed(gsetting, key):
-        enabled = gsetting.get_boolean(key)
-        msg = f"ORCA: {key} changed to {enabled}."
-        debug.print_message(debug.LEVEL_INFO, msg, True)
-        if key == "screen-reader-enabled" and not enabled:
-            shutdown()
-
     def _reload_on_signal(signum, frame):
         signal_string = f'({signal.strsignal(signum)})'
         tokens = [f"ORCA: Reloading due to signal={signum} {signal_string}", frame]
@@ -204,18 +196,6 @@ def main():
         settings_manager.get_manager().set_accessibility(True)
 
     load_user_settings(is_reload=False)
-
-    try:
-        _a11y_applications_gsetting = Gio.Settings(schema_id="org.gnome.desktop.a11y.applications")
-        connection = _a11y_applications_gsetting.connect("changed", _on_enabled_changed)
-        msg = f"ORCA: Connected to a11y applications gsetting: {bool(connection)}"
-        debug.print_message(debug.LEVEL_INFO, msg, True)
-    except GLib.Error as error:
-        msg = f"ORCA: EXCEPTION connecting to a11y applications (schema may be missing): {error}"
-        debug.print_message(debug.LEVEL_SEVERE, msg, True)
-    except (AttributeError, TypeError) as error:
-        msg = f"ORCA: EXCEPTION connecting to a11y applications (version incompatibility): {error}"
-        debug.print_message(debug.LEVEL_SEVERE, msg, True)
 
     dbus_service.get_remote_controller().start()
     script = script_manager.get_manager().get_default_script()
