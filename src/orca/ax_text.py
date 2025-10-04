2128311f71827fc7df888bf140aa75d5e8e755e8
Joanmarie Diggs <jdiggs@igalia.com>
2025-10-04 13:28:58 +0200

    Fix false positive identifying grammar errors as spelling
    Check that invalid is not set to grammar before concluding that an
    underline type of error means spelling.

--- src/orca/ax_text.py
+++ src/orca/ax_text.py
@@ -1458,6 +1458,8 @@ class AXText:
         attributes = AXText.get_text_attributes_at_offset(obj, offset)[0]
         if attributes.get("invalid") == "spelling":
             return True
+        if attributes.get("invalid") == "grammar":
+            return False
         if attributes.get("text-spelling") == "misspelled":
             return True
         if attributes.get("underline") in ["error", "spelling"]:
@@ -1471,7 +1473,7 @@ class AXText:
         attributes = AXText.get_text_attributes_at_offset(obj, offset)[0]
         if attributes.get("invalid") == "grammar":
             return True
-        if attributes.get("underline") in ["error", "grammar"]:
+        if attributes.get("underline") == "grammar":
             return True
         return False
 
