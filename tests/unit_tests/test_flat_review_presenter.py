5330882e3d04bf1696a7b35a91df3858940a2a98
Joanmarie Diggs <jdiggs@igalia.com>
2026-04-24 12:03:51 +0200

    Update test

--- tests/unit_tests/test_flat_review_presenter.py
+++ tests/unit_tests/test_flat_review_presenter.py
@@ -86,10 +86,10 @@ class TestFlatReviewPresenter:
         atspi_mock.EventListener.new = test_context.Mock(return_value=event_listener_mock)
         gi_repository_mock.Atspi = atspi_mock
 
-        from orca.ax_utilities_application import AXUtilitiesApplication
+        from orca.ax_utilities import AXUtilities
 
         test_context.patch_object(
-            AXUtilitiesApplication,
+            AXUtilities,
             "get_application",
             return_value=None,
         )
